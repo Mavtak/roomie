5ce7390c5850f0499d59b0c884a393d68d2935b1
David McGrath <github.com@davidmcgrath.com>
2012-09-25 00:39:30 -0400

    removed GoDaddy-specific hacks

--- Web/Website/content/Scripts/PageReady.js
+++ Web/Website/content/Scripts/PageReady.js
@@ -6,27 +6,11 @@ function debug(message) {
     $('#navigation').append('<div class="debugMessage">' + message + '</div>');
 }
 
-function fixLinks() {
-    //http://stackoverflow.com/questions/179713/how-to-change-the-href-for-a-hyperlink-using-jquery
-    // curse you, GoDaddy!
-    $("a").each(function () {
-       this.href = this.href.replace(/roomiebot.com\/root\//, '');
-    });
-}
-
-function fixUrl() {
-    if (location.href.indexOf('/roomiebot.com/root') != -1) {
-        location.href = location.href.replace(/\/roomiebot.com\/root/, '');
-    }
-}
-
 $(window).resize(function () {
     
 });
 
 $(document).ready(function () {
-    fixLinks();
-    fixUrl();
 
     /*
     $('.button').mouseup(function () {
