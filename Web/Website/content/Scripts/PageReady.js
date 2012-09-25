363fade2129033babf50bb9de3dce3723f340437
David McGrath <github.com@davidmcgrath.com>
2012-09-25 00:35:39 -0400

    removed sidebar entirely

--- Web/Website/content/Scripts/PageReady.js
+++ Web/Website/content/Scripts/PageReady.js
@@ -6,26 +6,6 @@ function debug(message) {
     $('#navigation').append('<div class="debugMessage">' + message + '</div>');
 }
 
-function adjustLayout() {
-
-    if (!navigationVisible) {
-        $('.horizontalCrossSection').css('padding-right', "0px");
-        $('.horizontalCrossSection').css('margin-left', "auto");
-        return;
-    }
-
-    gap = $("#page").width() - 1000;
-    if (gap >=125) {
-        $('.horizontalCrossSection').css('padding-right', "125px");
-        $('.horizontalCrossSection').css('margin-left', "auto");
-    }
-    else
-    {
-        $('.horizontalCrossSection').css('padding-right', "0px");
-        $('.horizontalCrossSection').css('margin-left', "0px");
-    }
-}
-
 function fixLinks() {
     //http://stackoverflow.com/questions/179713/how-to-change-the-href-for-a-hyperlink-using-jquery
     // curse you, GoDaddy!
@@ -46,7 +26,6 @@ $(window).resize(function () {
 });
 
 $(document).ready(function () {
-    adjustLayout();
     fixLinks();
     fixUrl();
 
