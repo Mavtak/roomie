c6ef12c80da5a4ebb8f754d84eca237ab277582c
David McGrath <github.com@davidmcgrath.com>
2012-10-21 10:17:55 -0400

    moved route configuration to App_Start/RouteConfig.cs, in the MVC4 pattern

---
+++ Web/Website/App_Start/RouteConfig.cs
@@ -0,0 +1,97 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Roomie.Web.Website
+{
+    public static class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Sign In Shortcut",
+                url: "SignIn",
+                defaults: new
+                {
+                    controller = "User",
+                    action = "SignIn"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Sign Out Shortcut",
+                url: "SignOut",
+                defaults: new
+                {
+                    controller = "User",
+                    action = "SignOut"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Authenticate Shortcut",
+                url: "Authenticate",
+                defaults: new
+                {
+                    controller = "User",
+                    action = "Authenticate"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Account Settings Shortcut",
+                url: "Account",
+                defaults: new
+                {
+                    controller = "User",
+                    action = "Edit"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Source code redirect shortcut",
+                url: "Source",
+                defaults: new
+                {
+                    controller = "Home",
+                    action = "Source"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Index shortcut",
+                url: "{controller}s",
+                defaults: new
+                {
+                    action = "Index"
+                },
+                constraints: new
+                {
+                    controller = "(Network|Device|Computer|Script|Task|ControlPanel)"
+                }
+            );
+
+            routes.MapRoute(
+                name: "Details Shortcut",
+                url: "{controller}/{id}/{name}",
+                defaults: new
+                {
+                    action = "Details",
+                    name = UrlParameter.Optional
+                },
+                constraints: new
+                {
+                    id = "[0-9]*"
+                }
+                );
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+
+        }
+    }
+}
--- Web/Website/Global.asax.cs
+++ Web/Website/Global.asax.cs
@@ -18,102 +18,13 @@ namespace Roomie.Web.Website
             filters.Add(new HandleErrorAttribute());
         }
 
-        public static void RegisterRoutes(RouteCollection routes)
-        {
-            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
-
-            routes.MapRoute(
-                name: "Sign In Shortcut",
-                url: "SignIn",
-                defaults: new
-                {
-                    controller = "User",
-                    action = "SignIn"
-                }
-            );
-
-            routes.MapRoute(
-                name: "Sign Out Shortcut",
-                url: "SignOut",
-                defaults: new
-                {
-                    controller = "User",
-                    action = "SignOut"
-                }
-            );
-
-            routes.MapRoute(
-                name: "Authenticate Shortcut",
-                url: "Authenticate",
-                defaults: new
-                {
-                    controller = "User",
-                    action = "Authenticate"
-                }
-            );
-
-            routes.MapRoute(
-                name: "Account Settings Shortcut",
-                url: "Account",
-                defaults: new
-                {
-                    controller = "User",
-                    action = "Edit"
-                }
-            );
-
-            routes.MapRoute(
-                name: "Source code redirect shortcut",
-                url: "Source",
-                defaults: new
-                {
-                    controller = "Home",
-                    action = "Source"
-                }
-            );  
-
-            routes.MapRoute(
-                name: "Index shortcut",
-                url: "{controller}s",
-                defaults: new
-                    {
-                        action = "Index"
-                    },
-                constraints: new
-                    {
-                        controller = "(Network|Device|Computer|Script|Task|ControlPanel)"
-                    }
-            );
-
-            routes.MapRoute(
-                name: "Details Shortcut",
-                url: "{controller}/{id}/{name}",
-                defaults: new
-                    {
-                        action = "Details",
-                        name = UrlParameter.Optional
-                    },
-                constraints: new
-                    {
-                        id = "[0-9]*"
-                    }
-                );
-
-            routes.MapRoute(
-                "Default", // Route name
-                "{controller}/{action}/{id}", // URL with parameters
-                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
-            );
-
-        }
-
         protected void Application_Start()
         {
             Database.SetInitializer<RoomieDatabaseContext>(new RoomieDatabaseInitializer());
             AreaRegistration.RegisterAllAreas();
 
             RegisterGlobalFilters(GlobalFilters.Filters);
-            RegisterRoutes(RouteTable.Routes);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
 
             //var database = new RoomieDatabaseContext();
             //database.CreateTables();
