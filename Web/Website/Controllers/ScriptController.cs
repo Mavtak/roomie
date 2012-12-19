49ca9ce9d59572e771abd837fff59430de7d9da2
David McGrath <github.com@davidmcgrath.com>
2012-12-19 09:04:26 -0500

    a little Saved Scripts update

--- Web/Website/Controllers/ScriptController.cs
+++ Web/Website/Controllers/ScriptController.cs
@@ -2,6 +2,8 @@
 using System.Web.Mvc;
 using Roomie.Web.Models;
 using Roomie.Web.Website.Helpers;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Roomie.Web.Website.Controllers
 {
@@ -10,7 +12,9 @@ namespace Roomie.Web.Website.Controllers
     {
         public ActionResult Index()
         {
-            return View(User.SavedScripts);
+            var scripts = User.SavedScripts;
+
+            return View(scripts);
         }
 
         public ActionResult Details(int id)
@@ -35,10 +39,26 @@ namespace Roomie.Web.Website.Controllers
         }
 
         [HttpPost]
-        public ActionResult New(SavedScriptModel model, string returnUrl)
+        public ActionResult New(string name, string text, string returnUrl)
         {
-            model.Owner = User;
-            Database.SavedScripts.Add(model);
+            var now = DateTime.UtcNow;
+
+            var savedScript = new SavedScriptModel
+            {
+                ModificationTimestamp = null,
+                Name = name,
+                Owner = User,
+                Script = new ScriptModel
+                {
+                    CreationTimestamp = now,
+                    LastRunTimestamp = null,
+                    Mutable = true,
+                    RunCount = 0,
+                    Text = text
+                }
+            };
+
+            Database.SavedScripts.Add(savedScript);
             Database.SaveChanges();
 
             if (Request.IsAjaxRequest())
@@ -56,14 +76,82 @@ namespace Roomie.Web.Website.Controllers
             }
         }
 
-        public ActionResult Edit()
+        public ActionResult Edit(int id)
         {
-            throw new NotImplementedException();
+            var savedScript = this.SelectSavedScript(id);
+
+            return View(savedScript);
         }
 
-        public ActionResult Run()
+        [HttpPost]
+        public ActionResult Edit(int id, string name, string text, string returnUrl)
         {
-            throw new NotImplementedException();
+            var savedScript = this.SelectSavedScript(id);
+
+            if (name != null)
+            {
+                savedScript.Name = name;
+            }
+
+            if (text != null)
+            {
+                savedScript.Script.Text = text;
+            }
+
+            savedScript.ModificationTimestamp = DateTime.UtcNow;
+
+            Database.SaveChanges();
+
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+
+            return Json(new
+                {
+                    Id = savedScript.Id,
+                    Name = name,
+                    Text = text
+                });
+
+        }
+
+        [HttpPost]
+        public ActionResult Delete(int id)
+        {
+            var savedScript = this.SelectSavedScript(id);
+            Database.SavedScripts.Remove(savedScript);
+            Database.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Run(int id, int computerId, string returnUrl)
+        {
+            var savedScript = this.SelectSavedScript(id);
+            var computer = this.SelectComputer(computerId);
+
+            addTask(
+                computer: computer,
+                origin: "Scripts Menu",
+                scriptText: savedScript.Script.Text
+                );
+
+            savedScript.Script.LastRunTimestamp = DateTime.UtcNow;
+            savedScript.Script.RunCount++;
+
+            Database.SaveChanges();
+
+            if (!String.IsNullOrEmpty(returnUrl))
+            {
+                return Redirect(returnUrl);
+            }
+
+            return Json(new
+            {
+                success = true
+            });
         }
 
     }
