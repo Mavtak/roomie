20faf085114b88ca8a346a0c52342085162e0ef9
David McGrath <github.com@davidmcgrath.com>
2013-02-23 14:09:10 -0500

    added retries to single-device home automation commands

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Attributes/RetriesParameterAttribute.cs
@@ -0,0 +1,14 @@
+﻿using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Attributes
+{
+    public class RetriesParameterAttribute : ParameterAttribute
+    {
+        public const string Key = "Retries";
+
+        public RetriesParameterAttribute()
+            : base(Key, "Integer", "3")
+        {
+        }
+    }
+}
