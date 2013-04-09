ecd93ddd3d1582c4f550f0242646ec822df10fb9
David McGrath <github.com@davidmcgrath.com>
2013-04-09 14:27:46 -0400

    eep!  Updated csproj with renames Also added motion-sensor-related IEventTypes

---
+++ Common/HomeAutomation/Events/StillnessDetected.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Roomie.Common.HomeAutomation.Events
+{
+    public class StillnessDetected : DevicePowerChanged
+    {
+        public new const string Key = "Stillness Detected";
+
+        public override string Name
+        {
+            get
+            {
+                return Key;
+            }
+        } 
+    }
+}
