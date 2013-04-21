8006766d80523a1a5a8d1fe6389cccc269a4524d
David McGrath <github.com@davidmcgrath.com>
2013-04-21 11:56:55 -0400

    improved display of event listing Used TextTable helper class, and HomeAutomation.ListDeviceEvent's logic calls HomeAutomation.ListEvents. Also, times are now displayed in local time rather than UTC.

--- CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/ListDeviceEvents.cs
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/ListDeviceEvents.cs
@@ -9,30 +9,11 @@ namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomati
         {
             var network = context.Network;
             var device = context.Device;
-            context.Interpreter.WriteEvent("network: " + network);
-            context.Interpreter.WriteEvent("network.Context: " + network.Context);
-            context.Interpreter.WriteEvent("network.Context.History: " + network.Context.History);
             var history = network.Context.History;
             var interpreter = context.Interpreter;
+            var deviceHistory = history.DeviceEvents.FilterEntity(device);
 
-            foreach (var @event in history.DeviceEvents.FilterEntity(device))
-            {
-                //TODO print prettier & DRY
-                var message = new StringBuilder();
-                message.Append(@event.TimeStamp);
-                message.Append(" ");
-                message.Append(@event.Type.Name);
-                message.Append(" ");
-                message.Append(@event.Entity.Name);
-
-                if (@event.Type is DevicePowerChanged)
-                {
-                    message.Append(" Power=");
-                    message.Append(@event.Power);
-                }
-
-                interpreter.WriteEvent(message.ToString());
-            }
+            ListEvents.WriteHistory(deviceHistory, interpreter);
         }
     }
 }
--- CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/ListEvents.cs
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/ListEvents.cs
@@ -1,5 +1,10 @@
-﻿using Roomie.Common.HomeAutomation.Events;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Roomie.Common.HomeAutomation.Events;
+using Roomie.Common.TextUtilities;
+using System;
 using System.Text;
+using Roomie.Desktop.Engine;
 
 namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
 {
@@ -9,28 +14,50 @@ namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomati
         {
             var network = context.Network;
             var interpreter = context.Interpreter;
+            var history = network.Context.History;
 
-            foreach (var @event in network.Context.History)
+            WriteHistory(history, interpreter);
+        }
+
+        internal static void WriteHistory(IEnumerable<IEvent> history, RoomieCommandInterpreter interpreter)
+        {
+            var headers = new[] {"Time Stamp", "Entity", "Type", "Source", ""};
+            var tableBuilder = new TextTable(new int[]
+                {
+                    Math.Max(history.Max(x => x.TimeStamp.ToLocalTime().ToString().Length), headers[0].Length),
+                    Math.Max(history.Max(x => ((x.Entity == null) ? string.Empty : x.Entity.Name).Length), headers[1].Length),
+                    Math.Max(history.Max(x => ((x.Type == null) ? string.Empty : x.Type.Name).Length), headers[2].Length),
+                    Math.Max(history.Max(x => ((x.Source == null) ? string.Empty : x.Source.Name).Length), headers[3].Length),
+                    10
+                });
+
+            interpreter.WriteEvent(tableBuilder.StartOfTable(headers));
+
+            foreach (var @event in history)
             {
-                var message = new StringBuilder();
-                message.Append(@event.TimeStamp);
-                message.Append(" ");
-                message.Append(@event.Type.Name);
-                message.Append(" ");
-                message.Append(@event.Entity.Name);
+                var extra = string.Empty;
 
                 var deviceEvent = @event as IDeviceEvent;
                 if (deviceEvent != null)
                 {
                     if (deviceEvent.Type is DevicePowerChanged)
                     {
-                        message.Append(" Power=");
-                        message.Append(deviceEvent.Power);
+                        extra = "Power=" + deviceEvent.Power;
                     }
                 }
 
-                interpreter.WriteEvent(message.ToString());
+                var line = tableBuilder.ContentLine(
+                    @event.TimeStamp.ToLocalTime().ToString(),
+                    (@event.Entity == null) ? string.Empty : @event.Entity.Name,
+                    (@event.Type == null) ? string.Empty : @event.Type.Name,
+                    (@event.Source == null) ? string.Empty : @event.Source.Name,
+                    extra
+                    );
+
+                interpreter.WriteEvent(line);
             }
+
+            interpreter.WriteEvent(tableBuilder.EndOfTable());
         }
     }
 }
