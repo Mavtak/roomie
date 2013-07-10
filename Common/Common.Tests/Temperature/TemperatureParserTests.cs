edfb2515cfc004cd5eee72d45159000f26decf51
David McGrath <github.com@davidmcgrath.com>
2013-07-09 23:20:30 -0400

    taught Roomie how to parse temperature strings TDD, like a boss

---
+++ Common/Common.Tests/Temperature/TemperatureParserTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using NUnit.Framework;
+using Roomie.Common.Temperature;
+
+namespace Roomie.Common.Tests.Temperature
+{
+    public class TemperatureParserTests
+    {
+        public readonly string[] ValidTemperatures = new[] {"0C", "0 C", "0  C", "0.1C", "0.1 C"};
+        public readonly string[] InvalidTemperatures = new[] {"", " ", "0", "kelvin", " 0C", "0\tC", "0.C", ".0C"};
+
+        [TestCase("0C")]
+        [TestCase("0 C")]
+        [TestCase("0  C")]
+        [TestCase("0.1C")]
+        public void ItAcceptsWellFormedTemperatures(string input)
+        {
+            var result = TemperatureParser.IsTemperature(input);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCaseSource("InvalidTemperatures")]
+        public void ItRejectsPoorlyFormedTemperatures(string input)
+        {
+            var result = TemperatureParser.IsTemperature(input);
+
+            Assert.That(result, Is.False);
+        }
+
+        [TestCaseSource("ValidTemperatures")]
+        public void ItDoesNotThrowAnExceptionWhenParsingValidTemperatures(string input)
+        {
+            TemperatureParser.Parse(input);
+        }
+
+        [TestCaseSource("InvalidTemperatures")]
+        [ExpectedException]
+        public void ItThrowsAnExceptionWhenParsingInvalidTemperatures(string input)
+        {
+            TemperatureParser.Parse(input);
+        }
+
+        [TestCase("0C", 0)]
+        [TestCase("10C", 10)]
+        [TestCase("123123123C", 123123123)]
+        [TestCase("123.456C", 123.456)]
+        public void ItParsesTheValueProperly(string input, double expected)
+        {
+            var result = TemperatureParser.Parse(input);
+
+            Assert.That(result.Value, Is.EqualTo(expected));
+        }
+
+        [TestCase("0C", typeof (CelsiusTemperature))]
+        [TestCase("0c", typeof (CelsiusTemperature))]
+        [TestCase("0Celsius", typeof (CelsiusTemperature))]
+        [TestCase("0celsius", typeof (CelsiusTemperature))]
+        [TestCase("0F", typeof (FahrenheitTemperature))]
+        [TestCase("0f", typeof (FahrenheitTemperature))]
+        [TestCase("0Fahrenheit", typeof (FahrenheitTemperature))]
+        [TestCase("0fahrenheit", typeof (FahrenheitTemperature))]
+        [TestCase("0K", typeof (KelvinTemperature))]
+        [TestCase("0k", typeof (KelvinTemperature))]
+        [TestCase("0Kelvin", typeof (KelvinTemperature))]
+        [TestCase("0kelvin", typeof (KelvinTemperature))]
+        public void ItParsesTheTypeProperly(string input, Type type)
+        {
+            var result = TemperatureParser.Parse(input);
+
+            Assert.That(result.GetType(), Is.EqualTo(type));
+        }
+    }
+}
---
+++ Common/Common/Temperature/TemperatureParser.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace Roomie.Common.Temperature
+{
+    public static class TemperatureParser
+    {
+        private const string Pattern = @"^(?<value>\d+([.]\d+)?) [ ]* (?<type>(c|f|k|celsius|fahrenheit|kelvin))$";
+
+        private static readonly Regex PatternRegex = new Regex(Pattern, RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace | RegexOptions.Compiled);
+
+        public static bool IsTemperature(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            return PatternRegex.IsMatch(input);
+        }
+
+        public static ITemperature Parse(string input)
+        {
+            var match = PatternRegex.Match(input);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException("The input does not represent a valid temperature", "input");
+            }
+
+            var value = Convert.ToDouble(match.Groups["value"].Value);
+
+            ITemperature result;
+
+            var type = match.Groups["type"].Value;
+            switch (type.ToLower())
+            {
+                case "c":
+                case "celsius":
+                    result = new CelsiusTemperature(value);
+                    break;
+
+                case "f":
+                case "fahrenheit":
+                    result = new FahrenheitTemperature(value);
+                    break;
+
+                case "k":
+                case "kelvin":
+                    result = new KelvinTemperature(value);
+                    break;
+
+                default:
+                    throw new Exception("Could not determine type " + type);
+            }
+
+            return result;
+        }
+    }
+}
