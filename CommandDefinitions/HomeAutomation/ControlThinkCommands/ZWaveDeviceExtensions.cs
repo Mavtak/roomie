ea1e60b7afe136091785235960d1fae6ad6b81dd
David McGrath <github.com@davidmcgrath.com>
2013-07-13 01:15:40 -0400

    implemented Device Lost and Device Found events for Z-Wave devices

--- CommandDefinitions/HomeAutomation/ControlThinkCommands/ZWaveDeviceExtensions.cs
+++ CommandDefinitions/HomeAutomation/ControlThinkCommands/ZWaveDeviceExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Roomie.CommandDefinitions.HomeAutomationCommands.Exceptions;
+using Roomie.Common.HomeAutomation.Events;
 using Roomie.Common.HomeAutomation.Exceptions;
 
 namespace Roomie.CommandDefinitions.ControlThinkCommands
@@ -11,17 +12,31 @@ namespace Roomie.CommandDefinitions.ControlThinkCommands
     {
         internal static TResult DoDeviceOperation<TResult>(this ZWaveDevice device, Func<TResult> operation)
         {
+            var wasConnected = device.IsConnected;
+
             try
             {
                 var result = operation();
+
                 device.IsConnected = true;
+
+                if (wasConnected != true)
+                {
+                    var @event = DeviceEvent.Found(device, null);
+                    device.AddEvent(@event);
+                }
 
                 return result;
             }
             catch (ControlThink.ZWave.ZWaveException exception)
             {
-                //TODO: device disconnected event
                 device.IsConnected = false;
+
+                if (wasConnected == true)
+                {
+                    var @event = DeviceEvent.Lost(device, null);
+                    device.AddEvent(@event);
+                }
 
                 if (exception is ControlThink.ZWave.DeviceNotRespondingException)
                 {
