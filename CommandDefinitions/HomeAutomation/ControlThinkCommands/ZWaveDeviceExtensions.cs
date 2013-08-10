d1b00edf566884b1675a161d0eb2cd6622686a90
David McGrath <github.com@davidmcgrath.com>
2013-08-10 11:39:29 -0400

    automatically retry a failed Z-Wave device action if the device was connected before the failure. This will increase reliability of commands, especially polling on thermostats, which do several pings in a row.  This way, each ping gets a second chance before the whole sequence fails.

--- CommandDefinitions/HomeAutomation/ControlThinkCommands/ZWaveDeviceExtensions.cs
+++ CommandDefinitions/HomeAutomation/ControlThinkCommands/ZWaveDeviceExtensions.cs
@@ -11,41 +11,63 @@ namespace Roomie.CommandDefinitions.ControlThinkCommands
         {
             var wasConnected = device.IsConnected;
 
+            Func<TResult> attempt = () =>
+                {
+                    var result = operation();
+
+                    device.IsConnected = true;
+
+                    if (wasConnected != true)
+                    {
+                        var @event = DeviceEvent.Found(device, null);
+                        device.AddEvent(@event);
+                    }
+
+                    return result;
+                };
+
+            Func<Exception, Exception> fail = (exception) =>
+                {
+                    device.IsConnected = false;
+
+                    if (wasConnected == true)
+                    {
+                        var @event = DeviceEvent.Lost(device, null);
+                        device.AddEvent(@event);
+                    }
+
+                    if (exception is ControlThink.ZWave.DeviceNotRespondingException)
+                    {
+                        return new DeviceNotRespondingException(device, exception);
+                    }
+
+                    if (exception is ControlThink.ZWave.CommandTimeoutException)
+                    {
+                        return new CommandTimedOutException(device, exception);
+                    }
+
+                    return new HomeAutomationException("Unexpected Z-Wave error: " + exception.Message, exception);
+                };
+
             try
             {
-                var result = operation();
-
-                device.IsConnected = true;
-
-                if (wasConnected != true)
-                {
-                    var @event = DeviceEvent.Found(device, null);
-                    device.AddEvent(@event);
-                }
-
-                return result;
+                return attempt();
             }
-            catch (ControlThink.ZWave.ZWaveException exception)
+            catch (ControlThink.ZWave.ZWaveException exception1)
             {
-                device.IsConnected = false;
-
                 if (wasConnected == true)
                 {
-                    var @event = DeviceEvent.Lost(device, null);
-                    device.AddEvent(@event);
+                    try
+                    {
+                        return attempt();
+                    }
+                    catch(ControlThink.ZWave.ZWaveException exception2)
+                    {
+                        throw fail(exception2);
+                    }
                 }
 
-                if (exception is ControlThink.ZWave.DeviceNotRespondingException)
-                {
-                    throw new DeviceNotRespondingException(device, exception);
-                }
-
-                if (exception is ControlThink.ZWave.CommandTimeoutException)
-                {
-                    throw new CommandTimedOutException(device, exception);
-                }
-
-                throw new HomeAutomationException("Unexpected Z-Wave error: " + exception.Message, exception);
+                throw fail(exception1);
             }
         }
 
