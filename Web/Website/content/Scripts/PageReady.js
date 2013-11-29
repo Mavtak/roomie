5c0134e97b6eacb1ec9630b405f5b1a30859acfc
David McGrath <github.com@davidmcgrath.com>
2013-11-29 14:51:25 -0500

    detach and float the header

--- Web/Website/content/Scripts/PageReady.js
+++ Web/Website/content/Scripts/PageReady.js
@@ -4,7 +4,7 @@
 }
 
 $(window).resize(function () {
-    
+    sizeGhostHeader();
 });
 
 $(document).ready(function () {
@@ -29,3 +29,28 @@ var botZoom = function () {
 };
 
 $('.roomieBot').click(botZoom);
+
+var $page = $('#page');
+var $headerRow = $('#headerRow');
+var $ghostHeaderRow;
+
+var detatchHeader = function() {
+    $headerRow.css('position', 'fixed');
+    $headerRow.css('z-index', '50');
+    $headerRow.css('top', '0');
+    $headerRow.css('left', '0');
+    $headerRow.css('right', '0');
+    $headerRow.css('background-color', $page.css('background-color'));
+
+    if (!$ghostHeaderRow) {
+        $ghostHeaderRow = $('<div />');
+        $page.prepend($ghostHeaderRow);
+    }
+};
+
+var sizeGhostHeader = function() {
+    $ghostHeaderRow.css('height', $headerRow.height());
+};
+
+detatchHeader();
+sizeGhostHeader();
