d2631fd14a87ad71b4d37abab6cbf7338cb5060b
David McGrath <github.com@davidmcgrath.com>
2014-01-11 20:41:20 -0500

    added trigger action for running scripts

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/RunScriptTriggerAction.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Roomie.Common.ScriptingLanguage;
+using Roomie.Common.Triggers;
+using Roomie.Desktop.Engine;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands
+{
+    public class RunScriptTriggerAction : ITriggerAction
+    {
+        private ThreadPool _threadPool;
+        private IEnumerable<ScriptCommandList> _scripts;
+
+        public RunScriptTriggerAction(ThreadPool threadPool, params ScriptCommandList[] scripts)
+        {
+            _threadPool = threadPool;
+            _scripts = scripts.ToArray();
+        }
+
+        public void Action()
+        {
+            foreach (var script in _scripts)
+            {
+                _threadPool.AddCommands(script);
+            }
+        }
+    }
+}
