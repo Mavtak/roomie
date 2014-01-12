3da88d88bb288704f3718470c8d830edf6013c1a
David McGrath <github.com@davidmcgrath.com>
2014-01-11 20:34:08 -0500

    added helper method for ITriggerCollection

---
+++ Common/Triggers/TriggerCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace Roomie.Common.Triggers
+{
+    public static class TriggerCollectionExtensions
+    {
+        public static void CheckAndAct(this ITriggerCollection triggers)
+        {
+            var bundles = triggers.Check();
+            var actions = bundles.SelectMany(x => x.Actions);
+
+            foreach (var action in actions)
+            {
+                action.Action();
+            }
+        }
+    }
+}
