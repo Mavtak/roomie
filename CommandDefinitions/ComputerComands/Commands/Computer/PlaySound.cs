4cd49d449e174cde5cb7b03fa3fca9d79f513956
David McGrath <github.com@davidmcgrath.com>
2014-01-12 23:46:21 -0500

    added Computer.PlaySound command

---
+++ CommandDefinitions/ComputerComands/Commands/Computer/PlaySound.cs
@@ -0,0 +1,18 @@
+﻿using System.Media;
+using Roomie.Desktop.Engine;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.ComputerCommands.Commands.Computer
+{
+    [StringParameter("Path")]
+    public class PlaySound : RoomieCommand
+    {
+        protected override void Execute_Definition(RoomieCommandContext context)
+        {
+            var scope = context.Scope;
+            var path = scope.GetValue("Path");
+            var player = new SoundPlayer(path);
+            player.Play();
+        }
+    }
+}
