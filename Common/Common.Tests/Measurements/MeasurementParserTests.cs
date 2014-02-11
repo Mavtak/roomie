e9061234591f490982ea09a9a1f5ef49d0c57544
David McGrath <github.com@davidmcgrath.com>
2014-02-10 21:52:39 -0500

    added generic measurement parser

---
+++ Common/Common.Tests/Measurements/MeasurementParserTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using NUnit.Framework;
+using Roomie.Common.Measurements;
+using Roomie.Common.Measurements.Energy;
+using Roomie.Common.Measurements.Ratio;
+using Roomie.Common.Measurements.Temperature;
+
+namespace Roomie.Common.Tests.Measurements
+{
+    public class MeasurementParserTests
+    {
+        [TestCase("1J", 1, typeof(JoulesEnergy))]
+        public void ItParsesEnergyMeasurementsCorrectly(string input, double value, Type type)
+        {
+            var result = MeasurementParser.Parse<IEnergy>(input);
+
+            Assert.That(result.Value, Is.EqualTo(value));
+            Assert.That(result.GetType(), Is.EqualTo(type));
+        }
+
+        [TestCase("1%", 1, typeof(PercentageRatio))]
+        public void ItParsesRatioMeasurementsCorrectly(string input, double value, Type type)
+        {
+            var result = MeasurementParser.Parse<IRatio>(input);
+
+            Assert.That(result.Value, Is.EqualTo(value));
+            Assert.That(result.GetType(), Is.EqualTo(type));
+        }
+
+        [TestCase("1K", 1, typeof(KelvinTemperature))]
+        public void ItParsesTemperatureMeasurementsCorrectly(string input, double value, Type type)
+        {
+            var result = MeasurementParser.Parse<ITemperature>(input);
+
+            Assert.That(result.Value, Is.EqualTo(value));
+            Assert.That(result.GetType(), Is.EqualTo(type));
+        }
+
+        [TestCase("1Unit", 1, typeof(ReadOnlyMeasurement))]
+        public void ItParsesGeneralMeasurementsCorrectly(string input, double value, Type type)
+        {
+            var result = MeasurementParser.Parse<IMeasurement>(input);
+
+            Assert.That(result.Value, Is.EqualTo(value));
+            Assert.That(result.GetType(), Is.EqualTo(type));
+        }
+    }
+}
---
+++ Common/Common/Measurements/MeasurementParser.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Roomie.Common.Measurements.Energy;
+using Roomie.Common.Measurements.Ratio;
+using Roomie.Common.Measurements.Temperature;
+
+namespace Roomie.Common.Measurements
+{
+    public static class MeasurementParser
+    {
+        private const string Pattern = @"^(?<value>\d+([.]\d+)?) [ ]* (?<type>(.+))$";
+
+        private static readonly Regex PatternRegex = new Regex(Pattern,
+                                                               RegexOptions.IgnoreCase |
+                                                               RegexOptions.IgnorePatternWhitespace |
+                                                               RegexOptions.Compiled);
+
+        public static bool IsEnergy(string input)
+        {
+            if (String.IsNullOrWhiteSpace(input))
+            {
+                return false;
+            }
+
+            return PatternRegex.IsMatch(input);
+        }
+
+        public static TMeasurement Parse<TMeasurement>(string input)
+            where TMeasurement : IMeasurement
+        {
+            var match = PatternRegex.Match(input);
+
+            if (!match.Success)
+            {
+                throw new ArgumentException("The input does not represent a valid Energy", "input");
+            }
+
+            var value = Convert.ToDouble(match.Groups["value"].Value);
+            var type = match.Groups["type"].Value;
+            var result = Parse<TMeasurement>(value, type);
+
+            return result;
+        }
+
+        public static TMeasurement Parse<TMeasurement>(double value, string type)
+            where TMeasurement : IMeasurement
+        {
+            var runtimeType = typeof (TMeasurement);
+
+            if (runtimeType == typeof (IEnergy))
+            {
+                return (TMeasurement) EnergyParser.Parse(value, type);
+            }
+
+            if (runtimeType == typeof (IRatio))
+            {
+                return (TMeasurement) RatioParser.Parse(value, type);
+            }
+
+            if (runtimeType == typeof (ITemperature))
+            {
+                return (TMeasurement) TemperatureParser.Parse(value, type);
+            }
+
+            if (runtimeType == typeof (IMeasurement))
+            {
+                return (TMeasurement) (IMeasurement) new ReadOnlyMeasurement(value, type);
+            }
+
+            throw new Exception("Could not determine type " + runtimeType.Name);
+        }
+
+        public static TMeasurement ToMeasurement<TMeasurement>(this string input)
+            where TMeasurement : IMeasurement
+        {
+            return Parse<TMeasurement>(input);
+        }
+    }
+}
