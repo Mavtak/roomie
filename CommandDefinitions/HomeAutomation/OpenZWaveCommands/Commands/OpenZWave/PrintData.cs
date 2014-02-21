02cf7e28b53f8fdfdc95fea75e8efde08791797e
David McGrath <github.com@davidmcgrath.com>
2014-02-20 19:12:45 -0500

    improved printing of Open Z-Wave device info

--- CommandDefinitions/HomeAutomation/OpenZWaveCommands/Commands/OpenZWave/PrintData.cs
+++ CommandDefinitions/HomeAutomation/OpenZWaveCommands/Commands/OpenZWave/PrintData.cs
@@ -1,4 +1,6 @@
-﻿using Roomie.CommandDefinitions.HomeAutomationCommands;
+﻿using System.Linq;
+using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Common.TextUtilities;
 
 namespace Roomie.CommandDefinitions.OpenZWaveCommands.Commands.OpenZWave
 {
@@ -9,12 +11,61 @@ namespace Roomie.CommandDefinitions.OpenZWaveCommands.Commands.OpenZWave
             var interpreter = context.Interpreter;
             var device = context.Device as OpenZWaveDevice;
 
-            foreach (var value in device.Values)
+            var headers = new[]
             {
-                var text = value.FormatData();
+                "Command Class",
+                "Index",
+                "Instance",
+                "Value",
+                "Units",
+                "Type",
+                "Label",
+                "Help"
+            };
 
-                interpreter.WriteEvent(text);
+            var values = device.Values
+                               .OrderBy(x => x.Index)
+                               .OrderBy(x => x.Instance)
+                               .OrderBy(x => x.CommandClass.ToString());
+
+            var rows = values.Select(TransformValue).ToArray();
+
+            var rowsAndHeaders = new[] {headers}.Union(rows).ToArray();
+
+            var columnWidths = new int[headers.Length];
+
+            for (var i = 0; i < columnWidths.Length; i++)
+            {
+                columnWidths[i] = rowsAndHeaders.Max(x => x[i].Length);
             }
+
+
+            var tableBuilder = new TextTable(columnWidths);
+
+            interpreter.WriteEvent(tableBuilder.StartOfTable(headers));
+
+            foreach (var row in rows)
+            {
+                interpreter.WriteEvent(tableBuilder.ContentLine(row));
+            }
+
+            interpreter.WriteEvent(tableBuilder.EndOfTable());
+        }
+
+        private static string[] TransformValue(OpenZWaveDeviceValue deviceValue)
+        {
+            var commandClass = deviceValue.CommandClass.ToString();
+            var index = deviceValue.Index.ToString();
+            var instance = deviceValue.Instance.ToString();
+            var value = deviceValue.GetValue();
+            var units = deviceValue.Units;
+            var type = deviceValue.Type.ToString();
+            var label = deviceValue.Label;
+            var help = deviceValue.Help;
+
+            var result = new[] {commandClass, index, instance, value, units, type, label, help};
+
+            return result;
         }
     }
 }
