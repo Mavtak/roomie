915b93fe182fc426d71305411ebfd03ce142832b
David McGrath <github.com@davidmcgrath.com>
2014-04-20 12:28:00 -0400

    added UnitsMatcher and LabelMatcher

---
+++ CommandDefinitions/HomeAutomation/OpenZWaveCommands/OpenZWaveDeviceValueMatchers/LabelMatcher.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace Roomie.CommandDefinitions.OpenZWaveCommands.OpenZWaveDeviceValueMatchers
+{
+    public class LabelMatcher : IOpenZWaveDeviceValueMatcher
+    {
+        private readonly string[] _label;
+
+        public LabelMatcher(params string[] units)
+        {
+            _label = units;
+        }
+
+        public bool Matches(OpenZWaveDeviceValue entry)
+        {
+            var result = _label.Any(x => string.Equals(x, entry.Label, StringComparison.InvariantCultureIgnoreCase));
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Temperature()
+        {
+            var result = new UnitsMatcher("Temperature");
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Luminance()
+        {
+            var result = new UnitsMatcher("Luminance");
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Humidity()
+        {
+            var result = new UnitsMatcher("Relative Humidity");
+
+            return result;
+        }
+    }
+}
---
+++ CommandDefinitions/HomeAutomation/OpenZWaveCommands/OpenZWaveDeviceValueMatchers/UnitsMatcher.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace Roomie.CommandDefinitions.OpenZWaveCommands.OpenZWaveDeviceValueMatchers
+{
+    public class UnitsMatcher : IOpenZWaveDeviceValueMatcher
+    {
+        private readonly string[] _units;
+
+        public UnitsMatcher(params string[] units)
+        {
+            _units = units;
+        }
+
+        public bool Matches(OpenZWaveDeviceValue entry)
+        {
+            var result = _units.Any(x => string.Equals(x, entry.Units, StringComparison.InvariantCultureIgnoreCase));
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Time()
+        {
+            var result = new UnitsMatcher("hours", "minutes", "seconds", "milliseconds");
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Temperature()
+        {
+            var result = new UnitsMatcher("C", "F");
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Luminance()
+        {
+            var result = new UnitsMatcher("lux");
+
+            return result;
+        }
+
+        public static IOpenZWaveDeviceValueMatcher Humidity()
+        {
+            var result = new UnitsMatcher("%");
+
+            return result;
+        }
+    }
+}
