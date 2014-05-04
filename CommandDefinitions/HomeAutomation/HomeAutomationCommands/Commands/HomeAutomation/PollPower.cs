d5d4f743e22aaab970bee1a9965663dc56b1dc6f
David McGrath <github.com@davidmcgrath.com>
2014-05-04 11:14:55 -0400

    added commands to poll humidity and power

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/PollPower.cs
@@ -0,0 +1,15 @@
+﻿using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [NotFinished]
+    public class PollPower : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var device = context.Device;
+
+            device.PowerSensor.Poll();
+        }
+    }
+}
