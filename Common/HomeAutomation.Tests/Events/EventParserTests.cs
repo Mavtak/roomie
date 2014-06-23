719a60f728935aba29f234885a19a2e5bda2ef59
David McGrath <github.com@davidmcgrath.com>
2014-06-22 22:16:50 -0400

    added an IEventType parser

---
+++ Common/HomeAutomation.Tests/Events/EventParserTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using Roomie.Common.HomeAutomation.Events;
+
+namespace Roomie.Common.HomeAutomation.Tests.Events
+{
+    public class EventParserTests
+    {
+        public string[] ValidInputs = new[] { "Powered Off", "Device Power Changed", "Device State Changed", "Network Connected" };
+        public string[] InvalidInputs = new[] { "whatever", "", null };
+
+        [TestCaseSource("ValidInputs")]
+        public void ItAcceptsWellFormedInput(string input)
+        {
+            var result = EventTypeParser.IsValid(input);
+
+            Assert.That(result, Is.True);
+        }
+
+        [TestCaseSource("InvalidInputs")]
+        public void ItRejectsMalformedInput(string input)
+        {
+            var result = EventTypeParser.IsValid(input);
+
+            Assert.That(result, Is.False);
+        }
+
+        [TestCaseSource("ValidInputs")]
+        public void ItDoesNotThrowExceptionsWhenParsingValidInput(string input)
+        {
+            EventTypeParser.Parse(input);
+        }
+
+        [TestCaseSource("InvalidInputs")]
+        [ExpectedException]
+        public void ItThrowsExceptionsWhenParsingValidInput(string input)
+        {
+            EventTypeParser.Parse(input);
+        }
+
+        [Test]
+        public void ItParsesValuesProperly()
+        {
+            var expected = new DevicePowerChanged();
+            var actual = EventTypeParser.Parse("Device Power Changed");
+
+            Assert.That(actual.GetType(), Is.EqualTo(expected.GetType()));
+        }
+    }
+}
---
+++ Common/HomeAutomation/Events/EventTypeParser.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Roomie.Common.HomeAutomation.Events
+{
+    public static class EventTypeParser
+    {
+        public static bool IsValid(string input)
+        {
+            var @event = TryParse(input);
+            var result = @event != null;
+
+            return result;
+        }
+
+        public static IEventType TryParse(string input)
+        {
+            var eventTypes = GetEventTypes();
+            var result = eventTypes.FirstOrDefault(x => string.Equals(x.Name, input));
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        public static IEventType Parse(string input)
+        {
+            var result = TryParse(input);
+
+            if (result == null)
+            {
+                throw new ArgumentException("can not parse Event Type \"" + input + "\".");
+            }
+
+            return result;
+        }
+
+        public static IEventType ToEventType(this string input)
+        {
+            return Parse(input);
+        }
+
+        private static IEnumerable<IEventType> GetEventTypes()
+        {
+            var result = GetImplementations<IEventType>()
+                .Select(Activator.CreateInstance)
+                .Cast<IEventType>();
+
+            return result;
+        }
+
+        private static IEnumerable<Type> GetImplementations(Type type)
+        {
+            var assemblies = AppDomain.CurrentDomain.GetAssemblies();
+            var types = assemblies.SelectMany(x => x.GetTypes());
+            var result = types.Where(x => type.IsAssignableFrom(x));
+            result = result.Where(x => x != type);
+
+            return result;
+        }
+ 
+        public static IEnumerable<Type> GetImplementations<TType>()
+        {
+            var type = typeof (TType);
+
+            return GetImplementations(type);
+        }
+    }
+}
