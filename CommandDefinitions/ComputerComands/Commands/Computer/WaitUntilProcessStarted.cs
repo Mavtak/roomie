8fa4d4f31adadf3c2f6edd1ead5033f045129c9e
David McGrath <david.mcgrath@rackspace.com>
2014-06-27 19:53:14 -0400

    added Computer.WaitUntilProcessStarted

---
+++ CommandDefinitions/ComputerComands/Commands/Computer/WaitUntilProcessStarted.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using Roomie.Common;
+using Roomie.Desktop.Engine;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.ComputerCommands.Commands.Computer
+{
+    [StringParameter("Name")]
+    [BooleanParameter("Running", true)]
+    [TimeSpanParameter("PollInterval", "500ms")]
+    [Description("Wait until a process is runnning or not running.")]
+    public class WaitUntilProcessRunning : RoomieCommand
+    {
+        protected override void Execute_Definition(RoomieCommandContext context)
+        {
+            var scope = context.Scope;
+            var name = scope.GetValue("Name");
+            var expectedRunning = scope.GetValue("Running").ToBoolean();
+            var pollInterval = scope.GetValue("PollInterval").ToTimeSpan();
+
+            while (true)
+            {
+                var processes = Process.GetProcesses();
+                var actualRunning = processes.Any(x => string.Equals(x.ProcessName, name));
+
+                if (actualRunning == expectedRunning)
+                {
+                    return;
+                }
+
+                Thread.Sleep(pollInterval);
+            }
+        }
+    }
+}
