8b3ae9b22de7cb2545072b8c25fa8ca478651c00
David McGrath <github.com@davidmcgrath.com>
2014-06-30 20:12:24 -0400

    added some paging input sanitation

--- Web/Persistence/Repositories/EntityFrameworkRepositories/Helpers.cs
+++ Web/Persistence/Repositories/EntityFrameworkRepositories/Helpers.cs
@@ -4,8 +4,25 @@ namespace Roomie.Web.Persistence.Repositories.EntityFrameworkRepositories
 {
     public static class Helpers
     {
+        public const int MaxPageSize = 50;
+
         public static IQueryable<T> Page<T>(this IQueryable<T> items, int? page, int? count)
         {
+            if (page < 1)
+            {
+                page = 1;
+            }
+
+            if (count < 1)
+            {
+                count = 1;
+            }
+
+            if (count > MaxPageSize)
+            {
+                count = MaxPageSize;
+            }
+
             var result = items.Skip((page ?? 1 - 1)*count ?? 0)
                               .Take(count ?? 0);
 
