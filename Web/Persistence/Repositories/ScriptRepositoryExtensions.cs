897ed6feb8a4b52a5199ffe47c2c35ff627823e0
David McGrath <github.com@davidmcgrath.com>
2014-07-06 14:59:30 -0400

    added extension method to clean up unused scripts

---
+++ Web/Persistence/Repositories/ScriptRepositoryExtensions.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+
+namespace Roomie.Web.Persistence.Repositories
+{
+    public static class ScriptRepositoryExtensions
+    {
+        public static int Clean(this IScriptRepository repository, ITaskRepository taskRepository, ISavedScriptRepository savedScriptRepository)
+        {
+            var scripts = repository.List(new ListFilter
+            {
+                SortDirection = SortDirection.Ascending
+            });
+
+            scripts = scripts.Where(x =>
+                {
+                    var taskOwners = taskRepository.Get(x);
+
+                    if (taskOwners.Any())
+                    {
+                        return false;
+                    }
+
+                    var savedScriptOwners = savedScriptRepository.Get(x);
+
+                    if (savedScriptOwners.Any())
+                    {
+                        return false;
+                    }
+
+                    return true;
+                }).ToArray();
+
+            if (!scripts.Any())
+            {
+                return 0;
+            }
+
+            var count = 0;
+
+            foreach (var task in scripts)
+            {
+                repository.Remove(task);
+
+                count++;
+            }
+
+            return count;
+        }
+    }
+}
