3e228d40ef8e978244c22140259c42416205e913
David McGrath <github.com@davidmcgrath.com>
2014-07-11 19:56:27 -0400

    add extension method for formatting time

---
+++ Common/Common/StringBuilderExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text;
+
+namespace Roomie.Common
+{
+    public static class StringBuilderExtensions
+    {
+        public static void AppendDateTime(this StringBuilder builder, DateTime? dateTime, TimeZoneInfo timeZone = null)
+        {
+            if (dateTime != null && timeZone != null)
+            {
+                dateTime = TimeZoneInfo.ConvertTimeFromUtc(dateTime.Value, timeZone);
+            }
+
+            builder.Append(dateTime);
+        }
+    }
+}
