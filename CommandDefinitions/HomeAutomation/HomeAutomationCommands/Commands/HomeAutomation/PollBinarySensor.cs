feb1e8c6a183013e88fc71bce3697366e20bd9d5
David McGrath <github.com@davidmcgrath.com>
2014-07-11 19:27:23 -0400

    add HomeAutomation.PollBinarySensor command

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/PollBinarySensor.cs
@@ -0,0 +1,15 @@
+﻿using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [NotFinished]
+    public class PollBinarySensor : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var device = context.Device;
+
+            device.BinarySensor.Poll();
+        }
+    }
+}
