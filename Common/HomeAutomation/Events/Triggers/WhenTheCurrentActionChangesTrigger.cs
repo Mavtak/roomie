93402ab7289e958a134462690fa10fc5c7f1a88b
David McGrath <github.com@davidmcgrath.com>
2014-07-26 23:20:04 -0400

    add trigger for when the current action changes

---
+++ Common/HomeAutomation/Events/Triggers/WhenTheCurrentActionChangesTrigger.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+
+namespace Roomie.Common.HomeAutomation.Events.Triggers
+{
+    public class WhenTheCurrentActionChangesTrigger : WhenDeviceEventHappensTrigger
+    {
+        private readonly string _desiredCurrentAction;
+
+        public WhenTheCurrentActionChangesTrigger(IDevice device, string currentAction, IDeviceHistory history)
+            : base(device, new CurrentActionChanged(), history)
+        {
+            _desiredCurrentAction = currentAction;
+        }
+
+        protected override System.Collections.Generic.IEnumerable<IDeviceEvent> GetMatches(IDeviceHistory history, IDevice device, IEventType eventType, System.DateTime since)
+        {
+            var matches = base.GetMatches(history, device, eventType, since);
+
+            matches = matches.Where(x => string.Equals(x.State.CurrentAction, _desiredCurrentAction));
+
+            return matches;
+        }
+    }
+}
