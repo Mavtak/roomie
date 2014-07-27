658c98540e2fd10b5807559a45b5a4e3b70f44bb
David McGrath <github.com@davidmcgrath.com>
2014-07-26 23:13:26 -0400

    added CurrentStateGenerator This can be configured with information about what different power ranges map to what states.

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands.Tests/CurrentStateGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using NUnit.Framework;
+using Roomie.Common.HomeAutomation.BinarySwitches;
+using Roomie.Common.Measurements.Power;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Tests
+{
+    public class CurrentStateGeneratorTests
+    {
+        [TestCase("", BinarySwitchPower.On, 0, null)]
+        [TestCase("", BinarySwitchPower.On, 5, null)]
+        [TestCase("", BinarySwitchPower.Off, 0, null)]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.Off, 0, null)]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 0, "off")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 1, "off")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 5, "idle")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 10, "idle")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 50, "running")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 100, "running")]
+        [TestCase("1,off;10,idle;100,running", BinarySwitchPower.On, 101, null)]
+        [TestCase("100,running;1,off", BinarySwitchPower.On, 0, "off")]
+        public void Works(string steps, BinarySwitchPower? binaryPower, double wattsPower, string expected)
+        {
+            var generator = new CurrentStateGenerator();
+
+            if (!string.IsNullOrEmpty(steps))
+            {
+                foreach (var step in steps.Split(';').Select(x => x.Split(',')))
+                {
+                    var stepValue = double.Parse(step[0]);
+                    var stepName = step[1];
+                    generator.AddStep(new WattsPower(stepValue), stepName);
+                }
+            }
+
+            var actual = generator.Generate(binaryPower, new WattsPower(wattsPower));
+
+            Assert.That(actual, Is.EqualTo(expected));
+        }
+    }
+}
---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/CurrentStateGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using Roomie.Common.HomeAutomation.BinarySwitches;
+using Roomie.Common.Measurements.Power;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands
+{
+    public class CurrentStateGenerator
+    {
+        private readonly List<Tuple<IPower, string>> _steps;
+
+        public CurrentStateGenerator()
+        {
+            _steps = new List<Tuple<IPower, string>>();
+        }
+
+        public string Generate(BinarySwitchPower? power, IPower value)
+        {
+            if (power != BinarySwitchPower.On || value == null)
+            {
+                return null;
+            }
+
+            foreach (var step in _steps)
+            {
+                //TODO: add comparison to IPower interface
+                if (value.Value <= step.Item1.Value)
+                {
+                    return step.Item2;
+                }
+            }
+
+            return null;
+        }
+
+        public void AddStep(IPower value, string name)
+        {
+            _steps.Add(new Tuple<IPower, string>(value, name));
+
+            _steps.Sort((x, y) => x.Item1.Value.CompareTo(y.Item1.Value));
+        }
+    }
+}
