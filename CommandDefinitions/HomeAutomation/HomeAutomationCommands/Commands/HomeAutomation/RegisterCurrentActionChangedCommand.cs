fa54b31492bf6195c00f3ab2d75e21163eb2c071
David McGrath <github.com@davidmcgrath.com>
2014-07-26 23:20:30 -0400

    add command to register CurrentAction triggers

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/RegisterCurrentActionChangedCommand.cs
@@ -0,0 +1,29 @@
+﻿using Roomie.Common.HomeAutomation.Events.Triggers;
+using Roomie.Common.Triggers;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [StringParameter("CurrentAction")]
+    public class RegisterCurrentActionChangedCommand : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var currentAction = context.Scope.GetValue("CurrentAction");
+            var originalCommand = context.OriginalCommand;
+            var commands = originalCommand.InnerCommands;
+            var device = context.Device;
+            var network = device.Network;
+            var triggers = network.Context.Triggers;
+            var history = network.Context.History;
+            var threadPool = network.Context.ThreadPool;
+
+            var trigger = new WhenTheCurrentActionChangesTrigger(device, currentAction, history.DeviceEvents);
+            var action = new RunScriptTriggerAction(threadPool, commands);
+
+            var triggerBundle = new TriggerBundle(trigger, action);
+
+            triggers.Add(triggerBundle);
+        }
+    }
+}
