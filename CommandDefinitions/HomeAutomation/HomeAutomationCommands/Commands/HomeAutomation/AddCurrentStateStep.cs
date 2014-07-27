fbb0aad57191228b39af6b39a586917d55f378bc
David McGrath <github.com@davidmcgrath.com>
2014-07-26 23:21:17 -0400

    added command to set CurrentAction generation

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/AddCurrentStateStep.cs
@@ -0,0 +1,21 @@
+﻿using Roomie.Common.Measurements.Power;
+using Roomie.Desktop.Engine;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [IntegerParameter("Power")]
+    [StringParameter("Name")]
+    public class AddCurrentStateStep : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var scope = context.Scope;
+            var power = scope.GetValue("Power").ToInteger();
+            var name = scope.GetValue("Name");
+            var device = context.Device;
+
+            device.CurrentStateGenerator.AddStep(new WattsPower(power), name);
+        }
+    }
+}
