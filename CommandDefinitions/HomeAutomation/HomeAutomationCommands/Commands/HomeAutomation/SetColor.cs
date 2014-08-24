a8265a85a2f6b81b420f9ca6ca06da3a152025b4
David McGrath <github.com@davidmcgrath.com>
2014-08-24 17:03:06 -0400

    added HomeAutomation.SetColor

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/SetColor.cs
@@ -0,0 +1,18 @@
+﻿using Roomie.Common.Color;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [ColorParameter("Color")]
+    public class SetColor : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var scope = context.Scope;
+            var device = context.Device;
+            var color = scope.GetValue("Color").ToColor();
+
+            device.ColorSwitch.SetValue(color);
+        }
+    }
+}
