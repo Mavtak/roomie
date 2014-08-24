5e079e334a033d793401a2862a05904be0f89b64
David McGrath <github.com@davidmcgrath.com>
2014-08-24 17:00:11 -0400

    added ColorParser

---
+++ Common/Common/Color/ColorParser.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Roomie.Common.Color
+{
+    public static class ColorParser
+    {
+        //TODO: add tests
+        public static bool IsValid(string input)
+        {
+            var result = TryParse(input) != null;
+
+            return result;
+        }
+
+        public static IColor Parse(string input)
+        {
+            var result = TryParse(input);
+
+            if (input == null)
+            {
+                throw new Exception("Invalid color \"" + input + "\"");
+            }
+
+            return result;
+        }
+
+        public static IColor TryParse(string input)
+        {
+            var result = ColorExtensions.FromHexString(input);
+            
+            if (result != null)
+            {
+                return result;
+            }
+
+            result = Utilities.NameDictionary.Find(input);
+
+            return result;
+        }
+
+        public static IColor ToColor(this string input)
+        {
+            return Parse(input);
+        }
+    }
+}
