255c7853adb8f1b8794ab2a425955878b7ba92a4
David McGrath <github.com@davidmcgrath.com>
2014-08-24 17:02:28 -0400

    Added color parameter type for commands

---
+++ Desktop/Engine/Commands/ColorParameterAttribute.cs
@@ -0,0 +1,17 @@
+﻿using Roomie.Desktop.Engine.RoomieCommandArgumentTypes;
+
+namespace Roomie.Desktop.Engine.Commands
+{
+    public class ColorParameterAttribute : ParameterAttribute
+    {
+        public ColorParameterAttribute(string name)
+            : base(name, new ColorParameterType())
+        {
+        }
+
+        public ColorParameterAttribute(string name, string @default)
+            : base(name, new ColorParameterType(), @default)
+        {
+        }
+    }
+}
---
+++ Desktop/Engine/RoomieCommandArgumentTypes/ColorParameterType.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using Roomie.Common.Color;
+
+namespace Roomie.Desktop.Engine.RoomieCommandArgumentTypes
+{
+    public class ColorParameterType : IRoomieCommandArgumentType
+    {
+        public const string Key = "Color";
+
+        public string Name
+        {
+            get
+            {
+                return Key;
+            }
+        }
+
+        public bool Validate(string value)
+        {
+            return ColorParser.IsValid(value);
+        }
+
+        public string ValidationMessage(string parameterName = null)
+        {
+            var builder = new StringBuilder();
+
+            using (new ParameterValidationMessageHelper(builder, parameterName))
+            {
+                builder.Append("a value that represents a color, like #FF0000 or Red");
+            }
+
+            return builder.ToString();
+        }
+    }
+}
