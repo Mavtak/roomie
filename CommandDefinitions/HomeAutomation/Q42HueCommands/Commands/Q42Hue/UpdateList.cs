67e3be7b62a3a882751284739a2c40e98a4b6edc
David McGrath <github.com@davidmcgrath.com>
2014-08-26 19:46:34 -0400

    Added Q42Hue.UpdateList

---
+++ CommandDefinitions/HomeAutomation/Q42HueCommands/Commands/Q42Hue/UpdateList.cs
@@ -0,0 +1,16 @@
+﻿using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Q42HueCommands.Commands.Q42Hue
+{
+    [Description("This command polls all lights on the specified network, including ones that were recently added to the Hue bridge.")]
+    public class UpdateList : HomeAutomationNetworkCommand
+    {
+        protected override void Execute_HomeAutomationNetworkDefinition(HomeAutomationCommandContext context)
+        {
+            var network = context.Network as Q42HueNetwork;
+
+            network.UpdateList();
+        }
+    }
+}
