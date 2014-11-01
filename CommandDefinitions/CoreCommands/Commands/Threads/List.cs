143014e468c065e05ba0d2af54b0364a09858cc1
David McGrath <github.com@davidmcgrath.com>
2014-11-01 11:54:09 -0400

    include the thread id in Threads.List command

--- CommandDefinitions/CoreCommands/Commands/Threads/List.cs
+++ CommandDefinitions/CoreCommands/Commands/Threads/List.cs
@@ -11,24 +11,28 @@ namespace Roomie.CommandDefinitions.CoreCommands.Commands.Threads
         {
             //TODO: make this include plugins' private threadpools.
             var interpreter = context.Interpreter;
+            var engine = context.Engine;
             var threads = context.Threads;
 
-            var headers = new string [] { "Thread Name", "Busy"};
+            var headers = new [] { "ID", "Name", "Busy"};
 
+            var maxThreadIdLength = threads.Max(x => x.Id.Length);
             var maxThreadNameLength = threads.Max(thread => (thread.Name ?? string.Empty).Length);
 
             var tableBuilder = new TextTable(new int []
                 {
-                    Math.Max(maxThreadNameLength, headers[0].Length),
-                    headers[1].Length
+                    Math.Max(maxThreadIdLength, headers[0].Length),
+                    Math.Max(maxThreadNameLength, headers[1].Length),
+                    headers[2].Length
                 });
 
             interpreter.WriteEvent(tableBuilder.StartOfTable(headers));
 
             foreach (var thread in threads)
             {
-                interpreter.WriteEvent(tableBuilder.ContentLine(new string []
+                interpreter.WriteEvent(tableBuilder.ContentLine(new []
                     {
+                        thread.Id,
                         thread.Name,
                         thread.IsBusy ? "yes" : " - "
                     }));
