0c3b850e0dd26de9b4738f913478a5519fce8aa4
David McGrath <github.com@davidmcgrath.com>
2014-11-02 17:14:09 -0500

    use more generic interface

--- Common/HomeAutomation/Events/ExtensionsToIHistory.cs
+++ Common/HomeAutomation/Events/ExtensionsToIHistory.cs
@@ -5,8 +5,8 @@ namespace Roomie.Common.HomeAutomation.Events
 {
     public static class ExtensionsToIHistory
     {
-        public static IEnumerable<TEvent> FilterEntity<TEvent>(this IHistory<TEvent> history, IHasName entity)
-            where TEvent: class, IEvent
+        public static IEnumerable<TEvent> FilterEntity<TEvent>(this IEnumerable<TEvent> history, IHasName entity)
+            where TEvent : class, IEvent
         {
             var results = history.Where(x => ReferenceEquals(x.Entity, entity));
 
