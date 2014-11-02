db9a610efa9bcb43418d028ce460ef7f57fca022
David McGrath <github.com@davidmcgrath.com>
2014-11-02 17:16:26 -0500

    added SaveDevicePowerEvents This writes the power events for a device to a CSV file.  Graphs!

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/SaveDevicePowerEvents.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using Roomie.Common;
+using Roomie.Common.HomeAutomation;
+using Roomie.Common.HomeAutomation.Events;
+using Roomie.Common.Measurements;
+using Roomie.Desktop.Engine;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [StringParameter("Filename")]
+    [BooleanParameter("IncludeHeaders", true)]
+    public class SaveDevicePowerEvents : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var scope = context.Scope;
+            var filename = scope.GetValue("Filename");
+            var includeHeaders = scope.GetValue("IncludeHeaders").ToBoolean();
+
+            var device = context.Device;
+            var network = device.Network;
+            var networkContext = network.Context;
+            var history = networkContext.History;
+
+            var deviceHistory = history.DeviceEvents
+                .FilterEntity(device)
+                .FilterType(new DevicePowerSensorValueChanged());
+
+            var converter = new Converter();
+            var writer = new CsvWriter<IDeviceEvent>(converter);
+
+            writer.Write(filename, deviceHistory, includeHeaders);
+        }
+
+        private class Converter : KeyValuePairConverter<IDeviceEvent>
+        {
+            public Converter()
+                : base(Conversions())
+            {
+            }
+
+            private static KeyValuePair<string, Func<IDeviceEvent, string>>[] Conversions()
+            {
+                var result = new[]
+                    {
+                        Conversion("Time", x => x.TimeStamp.ToString()),
+                        Conversion("Value", x => x.State.PowerSensorState.Value.Format())
+                    };
+
+                return result;
+            }
+        }
+    }
+}
