de339af3495ddb7bf027555b3d22726d40af56ef
David McGrath <github.com@davidmcgrath.com>
2014-11-15 13:02:58 -0500

    command to reconnect to a PiEngineering device

---
+++ CommandDefinitions/HomeAutomation/PiEngineeringCommands/Commands/ReconnectDevice.cs
@@ -0,0 +1,17 @@
+﻿using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.PiEngineeringCommands.Commands
+{
+    [Description("Reestablish a connection to P. I. Engineering device.")]
+    [Group("PiEngineering")]
+    public class ReconnectDevice : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var device = context.Device as PiEngineeringDevice;
+
+            device.Reconnect();
+        }
+    }
+}
