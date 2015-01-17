782b26a66a0c7087ff1d6be1619a56b3ec5f07dc
David McGrath <github.com@davidmcgrath.com>
2015-01-17 01:03:06 -0500

    expose tasks via Web API

---
+++ Web/Website/Controllers/Api/TaskController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using Roomie.Web.Persistence.Models;
+using Roomie.Web.Persistence.Repositories;
+using Roomie.Web.Website.Helpers;
+
+namespace Roomie.Web.Website.Controllers.Api
+{
+    [ApiRestrictedAccessAttribute]
+    [AutoSave]
+    public class TaskController : RoomieBaseApiController
+    {
+        public ShallowTaskModel Get(int id)
+        {
+            var task = Database.Tasks.Get(id);
+            var result = ShallowTaskModel.FromTaskModel(task);
+
+            return result;
+        }
+
+        public Page<ShallowTaskModel> Get([FromUri] ListFilter filter)
+        {
+            var result = Database.Tasks.List(User, filter)
+                .Transform(ShallowTaskModel.FromTaskModel);
+
+            return result;
+        }
+    }
+}
