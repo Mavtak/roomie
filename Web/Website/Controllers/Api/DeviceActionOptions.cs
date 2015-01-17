a3f72993cf4296e201226ed37acab04ccd41a9c7
David McGrath <github.com@davidmcgrath.com>
2015-01-16 23:01:52 -0500

    implement device actions in Web API controller I had to do some hackery to accept values from both the path and the body.

---
+++ Web/Website/Controllers/Api/DeviceActionOptions.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Roomie.Web.Website.Controllers.Api
+{
+    public class DeviceActionOptions
+    {
+        public string Color { get; set; }
+        public int? Power { get; set; }
+        public string Mode { get; set; }
+        public string Type { get; set; }
+        public string Temperature { get; set; }
+    }
+}
--- Web/Website/Controllers/Api/DeviceController.cs
+++ Web/Website/Controllers/Api/DeviceController.cs
@@ -1,6 +1,14 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
+using System.Web.Http;
+using Roomie.Common.Color;
 using Roomie.Common.HomeAutomation;
+using Roomie.Common.HomeAutomation.BinarySwitches;
+using Roomie.Common.HomeAutomation.Thermostats;
+using Roomie.Common.HomeAutomation.Thermostats.Fans;
+using Roomie.Common.HomeAutomation.Thermostats.SetpointCollections;
+using Roomie.Common.Measurements.Temperature;
 using Roomie.Web.Persistence.Database;
 using Roomie.Web.Website.Helpers;
 
@@ -32,6 +40,82 @@ namespace Roomie.Web.Website.Controllers.Api
                 .NewWithNetwork(null);
 
             return result;
+        }
+
+        public void Post(int id, string action, [FromBody] DeviceActionOptions options,
+            [FromUri] string color = null,
+            [FromUri] string mode = null,
+            [FromUri] int? power = null,
+            [FromUri] string temperature = null,
+            [FromUri] string type = null
+            )
+        {
+            options = options ?? new DeviceActionOptions();
+            options.Color = options.Color ?? color;
+            options.Mode = options.Mode ?? mode;
+            options.Power = options.Power ?? power;
+            options.Temperature = options.Temperature ?? temperature;
+            options.Type = options.Type ?? type;
+
+            IDevice device = this.SelectDevice(id);
+
+            switch (action)
+            {
+                case "Dim":
+                    device.MultilevelSwitch.SetPower(options.Power.Value);
+                    break;
+
+                case "PollBinarySensor":
+                    device.BinarySensor.Poll();
+                    break;
+
+                case "PollDevice":
+                    device.Poll();
+                    break;
+
+                case "PollHumiditySensor":
+                    device.HumiditySensor.Poll();
+                    break;
+
+                case "PollIlluminanceSensor":
+                    device.IlluminanceSensor.Poll();
+                    break;
+
+                case "PollPowerSensor":
+                    device.PowerSensor.Poll();
+                    break;
+
+                case "PollTemperatureSensor":
+                    device.TemperatureSensor.Poll();
+                    break;
+
+                case "PowerOn":
+                    device.BinarySwitch.SetPower(BinarySwitchPower.On);
+                    break;
+
+                case "PowerOff":
+                    device.BinarySwitch.SetPower(BinarySwitchPower.Off);
+                    break;
+
+                case "SetColor":
+                    device.ColorSwitch.SetValue(options.Color.ToColor());
+                    break;
+
+                case "SetThermostatFanMode":
+                    device.Thermostat.Fan.SetMode(options.Mode.ToThermostatFanMode());
+                    break;
+
+                case "SetThermostatMode":
+                    device.Thermostat.Core.SetMode(options.Mode.ToThermostatMode());
+                    break;
+
+                case "SetThermostatSetpoint":
+                    device.Thermostat.Setpoints.SetSetpoint(options.Type.ToSetpointType(), options.Temperature.ToTemperature());
+                    break;
+
+                default:
+                    throw new NotImplementedException();
+            }
         }
     }
 }
