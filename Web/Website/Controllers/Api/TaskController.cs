888b2c652236fb23f45f80d9b84938b52d95b955
David McGrath <github.com@davidmcgrath.com>
2015-01-19 22:28:57 -0500

    follow pattern from Computer controller for Tasks

--- Web/Website/Controllers/Api/TaskController.cs
+++ Web/Website/Controllers/Api/TaskController.cs
@@ -9,20 +9,50 @@ namespace Roomie.Web.Website.Controllers.Api
     [AutoSave]
     public class TaskController : RoomieBaseApiController
     {
-        public ShallowTaskModel Get(int id)
+        public TaskModel Get(int id)
         {
             var task = Database.Tasks.Get(id);
-            var result = ShallowTaskModel.FromTaskModel(task);
+            var result = GetSerializableVersion(task);
 
             return result;
         }
 
-        public Page<ShallowTaskModel> Get([FromUri] ListFilter filter)
+        public Page<TaskModel> Get([FromUri] ListFilter filter)
         {
             var result = Database.Tasks.List(User, filter)
-                .Transform(ShallowTaskModel.FromTaskModel);
+                .Transform(GetSerializableVersion);
 
             return result;
+        }
+
+        private TaskModel GetSerializableVersion(TaskModel task)
+        {
+            var result = new TaskModel
+            {
+                Expiration = task.Expiration,
+                Id = task.Id,
+                Origin = task.Origin,
+                ReceivedTimestamp = task.ReceivedTimestamp,
+                Script = task.Script
+            };
+
+            if (task.Owner != null)
+            {
+                result.Owner = new UserModel
+                {
+                    Id = task.Owner.Id
+                };
+            }
+
+            if (task.Target != null)
+            {
+                result.Target = new ComputerModel
+                {
+                    Id = task.Target.Id
+                };
+            }
+
+            return task;
         }
     }
 }
