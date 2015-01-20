ef49b30e73ac1711ee0495db6911e34451f9dfa2
David McGrath <github.com@davidmcgrath.com>
2015-01-19 22:23:52 -0500

    expose computers via Web API

---
+++ Web/Website/Controllers/Api/ComputerController.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Roomie.Common.HomeAutomation;
+using Roomie.Web.Persistence.Models;
+using Roomie.Web.Persistence.Repositories;
+using Roomie.Web.Website.Helpers;
+
+namespace Roomie.Web.Website.Controllers.Api
+{
+    [ApiRestrictedAccessAttribute]
+    [AutoSave]
+    public class ComputerController : RoomieBaseApiController
+    {
+        public ComputerModel[] Get()
+        {
+            var computers = Database.Computers.Get(User);
+            var result = computers.Select(GetSerializableVersion)
+                .ToArray();
+
+            return result;
+        }
+
+        public ComputerModel Get(int id)
+        {
+            var computer = this.SelectComputer(id);
+            var result = GetSerializableVersion(computer);
+
+            return result;
+        }
+
+        public ComputerModel Get(string accessKey)
+        {
+            var computer = Database.Computers.Get(accessKey);
+            var result = GetSerializableVersion(computer);
+            return result;
+        }
+
+        public void Post(ComputerModel model)
+        {
+            Database.Computers.Add(new ComputerModel
+            {
+                Owner = User,
+                Name = model.Name
+            });
+        }
+
+        private static ComputerModel GetSerializableVersion(ComputerModel device)
+        {
+            var result = new ComputerModel
+            {
+                AccessKey = device.AccessKey,
+                Address = device.Address,
+                EncryptionKey = device.EncryptionKey,
+                Id = device.Id,
+                LastPing = device.LastPing,
+                LastScript = device.LastScript,
+                Name = device.Name,
+                Owner = new UserModel
+                {
+                    Id = device.Owner.Id
+                }
+            };
+
+            return result;
+        }
+    }
+}
