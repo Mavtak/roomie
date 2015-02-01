00de09a59ff66b85bf712b2ac194b43c161bef35
David McGrath <github.com@davidmcgrath.com>
2015-02-01 14:55:59 -0500

    added command Q42Hue.ColorLoop

---
+++ CommandDefinitions/HomeAutomation/Q42HueCommands/Commands/ColorLoop.cs
@@ -0,0 +1,22 @@
+﻿using Q42.HueApi;
+using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Q42HueCommands.Commands
+{
+    [Description("Set the device to loop through the rainbow, starting at its current color.")]
+    [Group("Q42Hue")]
+    public class ColorLoop : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var device = context.Device as Q42HueDevice;
+            var command = new LightCommand
+            {
+                Effect = Effect.ColorLoop,
+            };
+
+            device.SendCommand(command);
+        }
+    }
+}
