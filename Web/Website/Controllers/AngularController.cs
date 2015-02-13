6f9cc8bab7ac87404f79964895c8b914724f73e6
David McGrath <github.com@davidmcgrath.com>
2015-02-12 22:33:39 -0500

    add angular page

---
+++ Web/Website/Controllers/AngularController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace Roomie.Web.Website.Controllers
+{
+    public class AngularController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View("App");
+        }
+    }
+}
