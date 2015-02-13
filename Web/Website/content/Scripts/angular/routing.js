e0cfa978a41507a1f5ae75c9c0359b99477d1d1c
David McGrath <github.com@davidmcgrath.com>
2015-02-12 22:27:50 -0500

    configure routing for tasks list

--- Web/Website/content/Scripts/angular/routing.js
+++ Web/Website/content/Scripts/angular/routing.js
@@ -5,4 +5,9 @@ module.run(['$rootScope', '$state', function ($rootScope, $state) {
 }]);
 
 module.config(['$stateProvider', function ($stateProvider) {
+  $stateProvider.state('tasks', {
+    url: '/tasks?start&count',
+    controller: 'TasksController',
+    template: '<task-widget task="task" ng-repeat="task in page.items"></task-widget>'
+  });
 }]);
