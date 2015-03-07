c492b22a6ef442910fd3db08b54ec5eb946aaab0
David McGrath <github.com@davidmcgrath.com>
2015-03-07 10:38:37 -0500

    list dependencies on separate lines

--- Web/Website/content/Scripts/angular/dependencies.js
+++ Web/Website/content/Scripts/angular/dependencies.js
@@ -1,4 +1,15 @@
-﻿angular.module('roomie.app', ['ui.router', 'roomie.devices', 'roomie.tasks']);
-angular.module('roomie.common', []);
-angular.module('roomie.devices', ['roomie.common']);
-angular.module('roomie.tasks', ['roomie.common']);
+﻿angular.module('roomie.app', [
+  'ui.router',
+  'roomie.devices',
+  'roomie.tasks'
+]);
+
+angular.module('roomie.common', [
+]);
+
+angular.module('roomie.devices', [
+  'roomie.common'
+]);
+angular.module('roomie.tasks', [
+  'roomie.common'
+]);
