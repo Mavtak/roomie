0c37a5c0578e71e0fb5f131d984bca4550eac6de
David McGrath <github.com@davidmcgrath.com>
2015-03-07 10:35:01 -0500

    add roomie.devices module

--- Web/Website/content/Scripts/angular/dependencies.js
+++ Web/Website/content/Scripts/angular/dependencies.js
@@ -1,3 +1,4 @@
-﻿angular.module('roomie.app', ['ui.router', 'roomie.tasks']);
+﻿angular.module('roomie.app', ['ui.router', 'roomie.devices', 'roomie.tasks']);
 angular.module('roomie.common', []);
+angular.module('roomie.devices', ['roomie.common']);
 angular.module('roomie.tasks', ['roomie.common']);
