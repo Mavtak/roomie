640f086c5223e978411603d9b75d449cdd7670c9
David McGrath <github.com@davidmcgrath.com>
2015-03-07 10:36:41 -0500

    add minimal DevicesController

---
+++ Web/Website/content/Scripts/angular/devices/DevicesController.js
@@ -0,0 +1,11 @@
+﻿var module = angular.module('roomie.devices');
+
+module.controller('DevicesController', ['$http', '$scope', function($http, $scope) {
+
+  $http.get('/api/device').success(function (devices) {
+    $scope.page = {
+      items: devices
+    };
+  });
+
+}]);
