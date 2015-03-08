ff390610e2300102e769e1b9d86c43228e31006c
David McGrath <github.com@davidmcgrath.com>
2015-03-08 05:47:51 -0400

    extract data polling code to it's own component Sheesh!  factory vs service (which is a factory) I just wanted to be able to new up new instances in the controller. T_T  Are you newing the class for me?  Once?  Every time it's injected?  I got it though...  Angular, I still love you.
    Also, I'm putting off unit tests some more lala laaa!

---
+++ Web/Website/content/Scripts/angular/data/AutomaticPollingUpdater.js
@@ -0,0 +1,74 @@
+﻿var module = angular.module('roomie.data');
+
+module.factory('AutomaticPollingUpdater', ['$http', '$timeout', function($http, $timeout) {
+
+  return function AutomaticPollingUpdater(options) {
+    var self = this;
+
+    self.url = options.url;
+    self.originals = options.originals;
+    self.setFunctions = options.setFunctions;
+
+    self.start = function() {
+      forever(function() {
+        return getData()
+          .success(applyUpdates)
+          .then(wait);
+      });
+    };
+
+    self.stop = function() {
+      //TODO: write me!
+    };
+
+    function forever(promiseFactory) {
+      return promiseFactory().then(function() {
+        forever(promiseFactory);
+      });
+    }
+
+    function getData() {
+      return $http.get(self.url);
+    }
+
+    function applyUpdates(updates) {
+      var originals = self.originals;
+
+      for (var i = 0; i < updates.length; i++) {
+        var update = updates[i];
+        var original = getOriginal(originals, update);
+
+        if (original === update) {
+          addOriginal(originals, original);
+        } else {
+          applyUpdate(original, update);
+        }
+      }
+    }
+
+    function wait() {
+      return $timeout(function() {
+      }, 500);
+    }
+
+    function getOriginal(originals, update) {
+      return _.find(originals, { id: update.id }) || update;
+    }
+
+    function addOriginal(originals, item) {
+      self.setFunctions(item);
+      originals.push(item);
+    }
+
+    function applyUpdate(original, update) {
+      //TODO: delete data from original when it makes sense (functions on original should be preserved)
+      //TODO: decide array behavior (which might be context dependent)
+
+      _.merge(original, update, function(a, b) {
+        if (typeof a === 'object' && typeof b === 'object') {
+          return _.merge(a, b);
+        }
+      });
+    }
+  }
+}]);
--- Web/Website/content/Scripts/angular/devices/DevicesController.js
+++ Web/Website/content/Scripts/angular/devices/DevicesController.js
@@ -1,75 +1,30 @@
 ﻿var module = angular.module('roomie.devices');
 
-module.controller('DevicesController', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
+module.controller('DevicesController', ['$http', '$scope', 'AutomaticPollingUpdater', function ($http, $scope, AutomaticPollingUpdater) {
 
-  initScope();
-
-  forever(function() {
-    return getData()
-      .success(applyUpdates)
-      .then(wait);
-  });
-
-  function initScope() {
+  initializeScope();
+  connectData();
+  
+  function initializeScope() {
     $scope.page = {
       items: []
     };
   }
 
-  function forever(promiseFactory) {
-    return promiseFactory().then(function() {
-      forever(promiseFactory);
+  function connectData() {
+    var data = new AutomaticPollingUpdater({
+      url: '/api/device',
+      originals: $scope.page.items,
+      setFunctions: setFunctions
     });
+
+    data.start();
   }
-
-  function getData() {
-    return $http.get('/api/device');
-  }
-
-  function applyUpdates(updates) {
-    var originals = $scope.page.items;
-
-    for (var i = 0; i < updates.length; i++) {
-      var update = updates[i];
-      var original = getOriginal(originals, update);
-
-      if (original === update) {
-        addOriginal(originals, original);
-      } else {
-        applyUpdate(original, update);
-      }
-    }
-  }
-
-  function wait() {
-    return $timeout(function() {
-    }, 500);
-  }
-
-  function getOriginal(originals, update) {
-    return _.find(originals, { id: update.id }) || update;
-  }
-
-  function addOriginal(originals, item) {
-    setFunctions(item);
-    originals.push(item);
-  }
-
+  
   function setFunctions(device) {
-    device.binarySwitch.setPower = function(power) {
+    device.binarySwitch.setPower = function (power) {
       $http.post('/api/device/' + device.id + '?action=Power' + power);
     };
-  }
-
-  function applyUpdate(original, update) {
-    //TODO: delete data from original when it makes sense (functions on original should be preserved)
-    //TODO: decide array behavior (which might be context dependent)
-
-    _.merge(original, update, function (a, b) {
-      if (typeof a === 'object' && typeof b === 'object') {
-        return _.merge(a, b);
-      }
-    });
   }
 
 }]);
