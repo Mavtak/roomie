fee567edf3d122c41d64cea23baa4371e76487ae
David McGrath <github.com@davidmcgrath.com>
2015-03-08 04:17:12 -0400

    support device polling rather retrieve the data once in the beginning, this code will retreive the new data continuously. Currently it will add new items (which is the same for the first request as the nth). Next up: apply the new data to the original data.  (hint: I already have this code written :P) Yes, yes... I'll write unit tests as soon as I pull this logic into a service.

--- Web/Website/content/Scripts/angular/devices/DevicesController.js
+++ Web/Website/content/Scripts/angular/devices/DevicesController.js
@@ -1,21 +1,76 @@
 ﻿var module = angular.module('roomie.devices');
 
-module.controller('DevicesController', ['$http', '$scope', function($http, $scope) {
+module.controller('DevicesController', ['$http', '$scope', '$timeout', function($http, $scope, $timeout) {
 
-  $http.get('/api/device').success(function (devices) {
-    $scope.page = {
-      items: devices
-    };
+  initScope();
 
-    for (var i = 0; i < $scope.page.items.length; i++) {
-      processDevices($scope.page.items[i]);
-    }
+  forever(function() {
+    return getData()
+      .success(applyUpdates)
+      .then(wait);
   });
 
-  function processDevices(device) {
+  function initScope() {
+    $scope.page = {
+      items: []
+    };
+  }
+
+  function forever(promiseFactory) {
+    return promiseFactory().then(function() {
+      forever(promiseFactory);
+    });
+  }
+
+  function getData() {
+    return $http.get('/api/device');
+  }
+
+  function applyUpdates(updates) {
+    var originals = $scope.page.items;
+
+    for (var i = 0; i < updates.length; i++) {
+      var update = updates[i];
+      var original = getOriginal(originals, update);
+
+      if (original === update) {
+        addOriginal(originals, original);
+      } else {
+        applyUpdate(original, update);
+      }
+    }
+  }
+
+  function wait() {
+    return $timeout(function() {
+    }, 500);
+  }
+
+  function getOriginal(originals, update) {
+    for (var i = 0; i < originals.length; i++) {
+      var original = originals[i];
+
+      if (original.id === update.id) {
+        return original;
+      }
+    }
+
+    return update;
+  }
+
+  function addOriginal(originals, item) {
+    setFunctions(item);
+    originals.push(item);
+  }
+
+  function setFunctions(device) {
     device.binarySwitch.setPower = function(power) {
       $http.post('/api/device/' + device.id + '?action=Power' + power);
     };
+  }
+
+  function applyUpdate(original, update) {
+    //todo: copy updated data from update to original
   }
 
 }]);
