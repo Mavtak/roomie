2e3cdd2b6599f0e31a0d2659fecff8a3f16dcc13
David McGrath <github.com@davidmcgrath.com>
2015-03-09 20:50:13 -0400

    update style

--- Web/Website/content/Scripts/angular/tasks/taskWidget.js
+++ Web/Website/content/Scripts/angular/tasks/taskWidget.js
@@ -8,14 +8,39 @@ module.directive('taskWidget', function () {
     },
     template: '' +
       '<widget>' +
-        '<widget-header title="Task"></widget-header>' +
+        '<widget-header ' +
+          'title="Task"' +
+          '>' +
+        '</widget-header>' +
         '<widget-data-section>' +
-          '<key-value key="Origin" value="{{task.origin}}"></key-value>' +
-          '<key-value key="Created" value="{{task.script.creationTimestamp.toLocaleString()}}"></key-value>' +
-          '<key-value key="Target" value="{{task.target.name}}" href="/computer/{{task.target.id}}/{{task.target.name}}"></key-value>' +
-          '<key-value key="Received" value="{{task | received}}"></key-value>' +
+          '<key-value ' +
+            'key="Origin" ' +
+            'value="{{task.origin}}"' +
+            '>' +
+          '</key-value>' +
+          '<key-value ' +
+            'key="Created" ' +
+            'value="{{task.script.creationTimestamp.toLocaleString()}}"' +
+            '>' +
+          '</key-value>' +
+          '<key-value ' +
+            'key="Target" ' +
+            'value="{{task.target.name}}" ' +
+            'href="/computer/{{task.target.id}}/{{task.target.name}}"' +
+            '>' +
+          '</key-value>' +
+          '<key-value ' +
+            'key="Received" ' +
+            'value="{{task | received}}"' +
+            '>' +
+          '</key-value>' +
         '</widget-data-section>' +
-        '<textarea class="code" readonly>{{task.script.text}}</textarea>' +
+        '<textarea ' +
+          'class="code" ' +
+          'readonly' +
+          '>' +
+          '{{task.script.text}}' +
+        '</textarea>' +
       '</widget>'
   };
 });
