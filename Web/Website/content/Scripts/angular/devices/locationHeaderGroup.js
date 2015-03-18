53389164c246ce82b62e2aec6298cf2456ff8ff2
David McGrath <github.com@davidmcgrath.com>
2015-03-17 21:55:00 -0400

    added location-header-group directive for displaying locations above device widgets in a device list

---
+++ Web/Website/content/Scripts/angular/devices/locationHeaderGroup.js
@@ -0,0 +1,28 @@
+﻿var module = angular.module('roomie.devices');
+
+module.directive('locationHeaderGroup', ['LocationHeaderLabelGenerator', function(LocationHeaderLabelGenerator) {
+
+  return {
+    restrict: 'E',
+    scope: {
+      currentLocation: '=currentLocation',
+      previousLocation: '=previousLocation'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'ng-repeat="part in parts"' +
+        '>' +
+        '<h2 ng-if="part.depth === 0">{{part.label}}</h2>' +
+        '<h3 ng-if="part.depth === 1">{{part.label}}</h3>' +
+        '<h4 ng-if="part.depth === 2">{{part.label}}</h4>' +
+        '<h5 ng-if="part.depth === 3">{{part.label}}</h5>' +
+        '<h6 ng-if="part.depth > 3">{{part.label}}</h6>' +
+      '</div>'
+  };
+
+  function link(scope) {
+    var labelGenerator = new LocationHeaderLabelGenerator(scope.previousLocation, scope.currentLocation);
+    scope.parts = labelGenerator.getParts();
+  }
+}]);
---
+++ Web/Website/content/scriptsTests/angular/devices/locationHeaderGroup.spec.js
@@ -0,0 +1,181 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/common/widgetHeader.js"/>
+/// <reference path="../../../Scripts/angular/devices/locationHeaderGroup.js"/>
+
+describe('roomie.devices.locationHeaderGroup', function() {
+  var $compile;
+  var $rootScope;
+  var givenCurrentLocation;
+  var givenPreviousLocation;
+  var parts;
+
+  beforeEach(angular.mock.module('roomie.devices', function($provide) {
+    $provide.factory('LocationHeaderLabelGenerator', function() {
+      return MockLocationHeaderLabelGenerator;
+    });
+  }));
+
+  beforeEach(angular.mock.inject(function(_$compile_, _$rootScope_) {
+    $compile = _$compile_;
+    $rootScope = _$rootScope_;
+  }));
+
+  beforeEach(function() {
+    givenCurrentLocation = null;
+    givenPreviousLocation = null;
+    parts = null;
+  });
+
+  it('gives the previous and current locations to the LocationHeaderLabelGenerator', function() {
+    $rootScope.previous = { name: 'herp' };
+    $rootScope.current = { name: 'derp' };
+
+    $compile('<location-header-group previous-location="previous" current-location="current"></location-header-group>')($rootScope);
+
+    expect(givenPreviousLocation).toBe($rootScope.previous);
+    expect(givenCurrentLocation).toBe($rootScope.current);
+  });
+
+  describe('when getParts returns an empty array', function() {
+
+    it('renders no labels', function() {
+      parts = [];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeaders(element).length).toEqual(0);
+    });
+
+  });
+
+  describe('when getParts returns a single entry', function() {
+
+    it('renders the label', function() {
+      parts = [{
+        label: 'Derp',
+        depth: 0
+      }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeaders(element).length).toEqual(1);
+    });
+
+    it('renders depth = 0 as an H2', function() {
+      parts = [{ depth: 0 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H2');
+    });
+
+    it('renders depth = 1 as an H3', function() {
+      parts = [{ depth: 1 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H3');
+    });
+
+    it('renders depth = 2 as an H4', function() {
+      parts = [{ depth: 2 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H4');
+    });
+
+    it('renders depth = 3 as an H5', function() {
+      parts = [{ depth: 3 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H5');
+    });
+
+    it('renders depth = 4 as an H6', function() {
+      parts = [{ depth: 4 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H6');
+    });
+
+    it('renders depth = 5 as an H6', function() {
+      parts = [{ depth: 5 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H6');
+    });
+
+    it('renders a higher depth as an H6', function() {
+      parts = [{ depth: 123 }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H6');
+    });
+
+  });
+
+  describe('when getParts returns an multiple entries', function() {
+
+    it('renders them', function() {
+      parts = [{
+          label: 'Derp',
+          depth: 2
+        }, {
+          label: 'Merp',
+          depth: 3
+        }, {
+          label: 'Berp',
+          depth: 4
+        }];
+
+      var element = $compile('<location-header-group></location-header-group>')($rootScope);
+      $rootScope.$digest();
+
+      expect(getHeaders(element).length).toEqual(3);
+
+      expect(getHeader(element, 0).text()).toEqual('Derp');
+      expect(getHeader(element, 0).get(0).tagName).toEqual('H4');
+
+      expect(getHeader(element, 1).text()).toEqual('Merp');
+      expect(getHeader(element, 1).get(0).tagName).toEqual('H5');
+
+      expect(getHeader(element, 2).text()).toEqual('Berp');
+      expect(getHeader(element, 2).get(0).tagName).toEqual('H6');
+    });
+
+  });
+
+  function getHeaders(element) {
+    return $(element).children();
+  }
+
+  function getHeader(element, index) {
+    return getHeaders(element).eq(index).children();
+  }
+
+  function MockLocationHeaderLabelGenerator(previous, current) {
+    givenPreviousLocation = previous;
+    givenCurrentLocation = current;
+
+    this.getParts = function() {
+      return parts;
+    };
+  }
+});
