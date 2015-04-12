132ba4f4a3f94c1d733ea2cae8d4b5536dbf7bb9
David McGrath <github.com@davidmcgrath.com>
2015-04-12 16:16:18 -0400

    add site-menu-button directive

---
+++ Web/Website/content/Scripts/angular/common/sideMenuButton.js
@@ -0,0 +1,38 @@
+﻿var module = angular.module('roomie.common');
+
+module.directive('sideMenuButton', function() {
+
+  return {
+    restrict: 'E',
+    scope: {
+      'close': '&close',
+      'isOpen': '=isOpen',
+      'open': '&open'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'class="menuButton" ' +
+        'ng-click="toggle()"' +
+        '>' +
+        '<div class="bar"></div>' +
+        '<div class="bar"></div>' +
+        '<div class="bar"></div>' +
+      '</div>'
+  };
+  
+  function link(scope) {
+    scope.isOpen = false;
+    scope.toggle = toggle;
+    
+    function toggle() {
+      var action = scope.isOpen ? scope.close : scope.open;
+      scope.isOpen = !scope.isOpen;
+
+      if (typeof action === 'function') {
+        action();
+      }
+    }
+  }
+
+});
