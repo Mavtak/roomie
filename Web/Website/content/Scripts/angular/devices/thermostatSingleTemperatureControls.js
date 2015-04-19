5dc57302de68cb765257e953fc2cfb18749bd238
David McGrath <github.com@davidmcgrath.com>
2015-04-19 09:17:20 -0400

    add thermostat-single-temperature-controls for displaying a temperature, a label for the temperature and (optionally) + and - buttons to increase and decrease the temperature.

---
+++ Web/Website/content/Scripts/angular/devices/thermostatSingleTemperatureControls.js
@@ -0,0 +1,97 @@
+﻿var module = angular.module('roomie.devices');
+
+module.directive('thermostatSingleTemperatureControls', function() {
+
+  return {
+    restrict: 'E',
+    scope: {
+      'label': '@label',
+      'set': '=set',
+      'temperature': '=temperature'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'class="temperature"' +
+        '>' +
+        '<button ' +
+          'class="button setpoint-button"' +
+          'ng-click="colder()" ' +
+          'ng-if="canSet()" ' +
+          '>' +
+          '-' +
+        '</button>' +
+        '<div ' +
+          'class="value"' +
+          '>' +
+          '{{format(temperature)}}' +
+        '</div>' +
+        '<button ' +
+          'class="button setpoint-button"' +
+          'ng-click="hotter()" ' +
+          'ng-if="canSet()" ' +
+          '>' +
+          '+' +
+        '</button>' +
+        '<div ' +
+          'class="description"' +
+          '>' +
+          '{{label}}' +
+        '</div>' +
+      '</div>'
+  };
+
+  function link(scope) {
+    scope.canSet = canSet;
+    scope.colder = colder;
+    scope.format = format;
+    scope.hotter = hotter;
+
+    function canSet() {
+      if (typeof scope.set !== 'function') {
+        return false;
+      }
+
+      if (typeof scope.temperature === 'undefined') {
+        return false;
+      }
+
+      if (typeof scope.temperature.value === 'undefined') {
+        return false;
+      }
+
+      return true;
+    }
+
+    function colder() {
+      var newTemperature = add(scope.temperature, -1);
+      scope.set(newTemperature);
+    }
+
+    function hotter() {
+      var newTemperature = add(scope.temperature, 1);
+      scope.set(newTemperature);
+    }
+  }
+
+  function add(temperature, amount) {
+    return {
+      value: temperature.value + amount,
+      units: temperature.units
+    };
+  }
+
+  function format(temperature) {
+    if (typeof temperature === 'undefined' || typeof temperature.value === 'undefined') {
+      return '';
+    }
+
+    var result = temperature.value;
+
+    if (typeof temperature.units === 'string') {
+      result += '°' + temperature.units.substr(0, 1);
+    }
+
+    return result;
+  }
+});
---
+++ Web/Website/content/scriptsTests/angular/devices/thermostatSingleTemperatureControls.spec.js
@@ -0,0 +1,208 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/common/widgetButton.js"/>
+/// <reference path="../../../Scripts/angular/devices/thermostatSingleTemperatureControls.js"/>
+
+describe('roomie.devices.thermostatSingleTemperatureControls', function() {
+  var $compile;
+  var $rootScope;
+  var attributes;
+  var element;
+
+  beforeEach(angular.mock.module('roomie.devices'));
+
+  beforeEach(angular.mock.inject(function($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  beforeEach(function() {
+    element = $compile('<thermostat-single-temperature-controls label="thingy" set="attributes.set" temperature="attributes.temperature"></thermostat-single-temperature-controls>')($rootScope);
+
+    attributes = {
+      set: jasmine.createSpy(),
+      temperature: {
+        value: 12.55,
+        units: 'McDerps'
+      }
+    };
+
+    $rootScope.attributes = attributes;
+
+    $rootScope.$digest();
+  });
+
+  describe('the cooler button', function() {
+
+    describe('existence criteria', function() {
+
+      it('the "set" attribute is a function and "temperature" attribute exists and has a "value" property', function() {
+        var button = selectButton();
+
+        expect(button.length).toEqual(1);
+      });
+
+      it('even if the "temperature" attribute does not have a "units" property', function() {
+        delete attributes.temperature.units;
+        var button = selectButton();
+
+        expect(button.length).toEqual(1);
+      });
+
+    });
+
+    describe('nonexistence criteria', function() {
+
+      it('the "set" attribute is not a function', function() {
+        attributes.set = 'nope!';
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+      it('the "temperature" attribute does not exist', function() {
+        delete attributes.temperature;
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+      it('the "temperature" attribute does not have a "value" attribute', function() {
+        delete attributes.temperature.value;
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+    });
+
+    describe('clicking behavior', function() {
+
+      it('calls the function specified by the "set" callback', function() {
+        var button = selectButton();
+
+        expect(attributes.set).not.toHaveBeenCalled();
+
+        button.click();
+
+        expect(attributes.set).toHaveBeenCalledWith({
+          value: 11.55,
+          units: 'McDerps'
+        });
+      });
+
+    });
+
+    function selectButton() {
+      $rootScope.$digest();
+
+      return $(element).find('.temperature .button.setpoint-button').filter(function() {
+        return $(this).text() === '-';
+      });
+    }
+
+  });
+
+  describe('the temperature display', function() {
+
+    it('exists', function() {
+      var display = selectDisplay();
+
+      expect(display.length).toEqual(1);
+    });
+
+    it('formats the temperature', function() {
+      var display = selectDisplay();
+
+      expect(display.text()).toEqual('12.55°M')
+    });
+
+    function selectDisplay() {
+      return $(element).find('.temperature .value');
+    }
+  });
+
+  describe('the hotter button', function() {
+
+    describe('existence criteria', function() {
+
+      it('the "set" attribute is a function and "temperature" attribute exists and has a "value" property', function() {
+        var button = selectButton();
+
+        expect(button.length).toEqual(1);
+      });
+
+      it('even if the "temperature" attribute does not have a "units" property', function() {
+        delete attributes.temperature.units;
+        var button = selectButton();
+
+        expect(button.length).toEqual(1);
+      });
+
+    });
+
+    describe('nonexistence criteria', function() {
+
+      it('the "set" attribute is not a function', function() {
+        attributes.set = 'nope!';
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+      it('the "temperature" attribute does not exist', function() {
+        delete attributes.temperature;
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+      it('the "temperature" attribute does not have a "value" attribute', function() {
+        delete attributes.temperature.value;
+        var button = selectButton();
+
+        expect(button.length).toEqual(0);
+      });
+
+    });
+
+    describe('clicking behavior', function() {
+
+      it('calls the function specified by the "set" callback', function() {
+        var button = selectButton();
+
+        expect(attributes.set).not.toHaveBeenCalled();
+
+        button.click();
+
+        expect(attributes.set).toHaveBeenCalledWith({
+          value: 13.55,
+          units: 'McDerps'
+        });
+      });
+
+    });
+
+    function selectButton() {
+      $rootScope.$digest();
+
+      return $(element).find('.temperature .button.setpoint-button').filter(function() {
+        return $(this).text() === '+';
+      });
+    }
+
+  });
+
+  describe('the label', function() {
+
+    it('exists', function() {
+      var actual = $(element).find('.temperature .description').text();
+
+      expect(actual).toEqual('thingy');
+    });
+
+  });
+
+});
