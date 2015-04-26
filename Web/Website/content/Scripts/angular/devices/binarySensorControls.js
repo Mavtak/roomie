5065c4336657fc40447e67c8890e582324c9c0c1
David McGrath <github.com@davidmcgrath.com>
2015-04-26 14:20:46 -0400

    add binary-sensor-controls directive for showing things like motion, window, and door sensors

---
+++ Web/Website/content/Scripts/angular/devices/binarySensorControls.js
@@ -0,0 +1,44 @@
+﻿var module = angular.module('roomie.devices');
+
+module.directive('binarySensorControls', function () {
+
+  return {
+    restrict: 'E',
+    scope: {
+      label: '@label',
+      sensor: '=sensor'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'class="group" ' +
+        '>' +
+        '<button ' +
+          'ng-click="sensor.poll()"' +
+          '>' +
+          '{{translateLabel(sensor)}}: {{translateValue(sensor)}} (at {{sensor.timeStamp.toLocaleString()}})' +
+        '</button>' +
+      '</div>'
+  };
+
+  function link(scope) {
+    scope.translateLabel = translateLabel;
+    scope.translateValue = translateValue;
+  }
+
+  function translateLabel(sensor) {
+    //TODO: account for different values of sensor.type
+    return 'Binary Sensor';
+  }
+
+  function translateValue(sensor) {
+    if (typeof sensor.value === 'undefined') {
+      return 'Unknown';
+    }
+
+    //TODO: account for different values of sensor.type
+
+    return sensor.value ? 'True' : 'False';
+  }
+
+});
---
+++ Web/Website/content/scriptsTests/angular/devices/binarySensorControls.spec.js
@@ -0,0 +1,117 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/devices/binarySensorControls.js"/>
+
+describe('roomie.devices.binarySensorControls', function() {
+  var $compile;
+  var $rootScope;
+  var attributes;
+  var element;
+
+  beforeEach(angular.mock.module('roomie.devices'));
+
+  beforeEach(angular.mock.inject(function($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  beforeEach(function() {
+    element = $compile('<binary-sensor-controls sensor="attributes.sensor"></binary-sensor-controls>')($rootScope);
+
+    attributes = {
+      sensor: {
+        poll: jasmine.createSpy(),
+        value: true,
+        timeStamp: new Date(2015, 3, 26, 13, 47, 23)
+      }
+    };
+
+    $rootScope.attributes = attributes;
+    $rootScope.$digest();
+  });
+
+  describe('the text', function() {
+    var parts;
+    var text;
+
+    beforeEach(readText);
+
+    describe('the label', function() {
+
+      it('is "Binary Sensor" when sensor.type is undefined', function() {
+        expect(text).toMatch(/^Binary Sensor: /);
+      });
+
+      //TODO: account for different values of sensor.type
+
+    });
+
+    describe('the measurement', function() {
+
+      it('is "Unknown" when device.value is undefined', function() {
+        delete attributes.sensor.value;
+        readText();
+
+        expect(text).toMatch(/: Unknown \(at/);
+      });
+
+      it('is "True" when device.value == true', function() {
+        attributes.sensor.value = true;
+        readText();
+
+        expect(text).toMatch(/: True \(at/);
+
+      });
+
+      it('is "False" when device.value == false', function() {
+        attributes.sensor.value = false;
+        readText();
+
+        expect(text).toMatch(/: False \(at/);
+      });
+
+
+      //TODO: account for different values of sensor.type
+
+    });
+
+    it('has the timestamp last', function() {
+      expect(text).toMatch(/\(at 4\/26\/2015, 1:47:23 PM\)$/);
+    });
+
+    function readText() {
+      $rootScope.$digest();
+      text = $(element).text();
+      parts = text.split(' ');
+    }
+  });
+
+  describe('the button', function() {
+    var button;
+
+    beforeEach(function() {
+      button = $(element).find('button');
+    });
+
+    it('exists', function() {
+      expect(button.length).toEqual(1);
+    });
+
+    describe('clicking', function() {
+
+      it('calls the sensor.poll()', function() {
+        expect(attributes.sensor.poll).not.toHaveBeenCalled();
+
+        button.click();
+
+        expect(attributes.sensor.poll).toHaveBeenCalled();
+
+      });
+
+    });
+
+  });
+
+});
