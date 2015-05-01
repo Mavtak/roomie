b6307863fcdbfa35cb206e273cbeea65e330e585
David McGrath <github.com@davidmcgrath.com>
2015-04-30 20:50:01 -0400

    add a dock directive for docking content to the top or bottom of the page

---
+++ Web/Website/content/Scripts/angular/common/dock.js
@@ -0,0 +1,40 @@
+﻿var module = angular.module('roomie.common');
+
+module.directive('dock', function() {
+
+  return {
+    transclude: true,
+    restrict: 'E',
+    scope: {
+      area: '@area'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'class="dock {{area}}" ' +
+        'ng-transclude ' +
+        '>' +
+      '</div>' +
+      '<div ' +
+        'ng-style="fillerStyle" ' +
+        '>' +
+      '</div>'
+  };
+  
+  function link(scope, element) {
+    var content = element.contents()[0];
+    
+    scope.fillerStyle = {};
+
+    scope.$watch(calculateHeight, updateHeight);
+
+    function calculateHeight() {
+      return content.offsetHeight + 'px';
+    }
+    
+    function updateHeight(newValue) {
+      scope.fillerStyle.height = newValue;
+    }
+  }
+
+});
---
+++ Web/Website/content/scriptsTests/angular/common/dock.spec.js
@@ -0,0 +1,57 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/common/dock.js"/>
+
+describe('roomie.common.dock', function () {
+  var $compile;
+  var $rootScope;
+  var element;
+
+  beforeEach(angular.mock.module('roomie.common'));
+
+  beforeEach(angular.mock.inject(function ($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+    element = $compile('<dock area="blam"><div class="boop">beep</div></dock>')($rootScope);
+    $rootScope.$digest();
+  }));
+
+  describe('dock styling', function() {
+
+    it('exists', function() {
+      expect(selectDock().length).toEqual(1);
+    });
+
+    it('has a "dock" class', function () {
+      expect(selectDock().hasClass('dock')).toEqual(true);
+    });
+    
+    it('has a class that matches the "area" attribute', function () {
+      expect(selectDock().hasClass('blam')).toEqual(true);
+    });
+
+  });
+
+  describe('the filler', function () {
+
+    it('exists', function () {
+      expect(selectFiller().length).toEqual(1);
+    });
+
+    it('has a custom style that matches its height to the dock height', function() {
+      expect(selectFiller().attr('style')).toMatch(/height\: [0-9]+px/);
+    });
+
+  });
+  
+  function selectDock() {
+    return $(element).children().eq(0);
+  }
+  
+  function selectFiller() {
+    return $(element).children().eq(1);
+  }
+
+});
