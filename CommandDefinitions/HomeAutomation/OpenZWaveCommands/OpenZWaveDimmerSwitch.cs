0adee14d55619fbd797c3df202a0cc0170f58c92
David McGrath <github.com@davidmcgrath.com>
2015-05-30 21:00:52 -0400

    fix typo

--- CommandDefinitions/HomeAutomation/OpenZWaveCommands/OpenZWaveDimmerSwitch.cs
+++ CommandDefinitions/HomeAutomation/OpenZWaveCommands/OpenZWaveDimmerSwitch.cs
@@ -38,7 +38,7 @@ namespace Roomie.CommandDefinitions.OpenZWaveCommands
         {
             get
             {
-                if (_device.Type == null || !_device.Type.Equals(DeviceType.MultilevelSensor))
+                if (_device.Type == null || !_device.Type.Equals(DeviceType.MultilevelSwitch))
                 {
                     return null;
                 }
