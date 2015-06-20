09731c1513a38bbe89626a29e163826fc0a7590b
David McGrath <github.com@davidmcgrath.com>
2015-06-20 18:38:44 -0400

    add color-switch-controls directive for controling color switches

---
+++ Web/Website/content/Scripts/angular/devices/colorSwitchControls.js
@@ -0,0 +1,34 @@
+﻿var module = angular.module('roomie.devices');
+
+module.directive('colorSwitchControls', ['ColorSwitchButtonGenerator', function (ColorSwitchButtonGenerator) {
+  return {
+    restrict: 'E',
+    scope: {
+      colorSwitch: '=colorSwitch'
+    },
+    link: link,
+    template: '' +
+      '<widget-button-group>' +
+        '<widget-button ' +
+          'ng-repeat="button in buttons" ' +
+          'label="{{button.label}}" ' +
+          'activate="colorSwitch.setValue(button.color)" ' +
+          'activated="button.activated" ' +
+          'color="{{button.color}}"' +
+          '>' +
+          '</widget-button>' +
+      '</widget-button-group>'
+    };
+  
+  function link(scope) {
+    var buttonGenerator = new ColorSwitchButtonGenerator(scope.colorSwitch);
+
+    updateButtons();
+
+    scope.$watch('colorSwitch.color.value', updateButtons);
+    
+    function updateButtons() {
+      scope.buttons = buttonGenerator.generate();
+    }
+  }
+}]);
---
+++ Web/Website/content/scriptsTests/angular/devices/colorSwitchControls.spec.js
@@ -0,0 +1,143 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/common/widget.js"/>
+/// <reference path="../../../Scripts/angular/common/widgetButton.js"/>
+/// <reference path="../../../Scripts/angular/common/widgetButtonGroup.js"/>
+/// <reference path="../../../Scripts/angular/devices/colorSwitchControls.js"/>
+
+describe('roomie.devices.colorSwitchControls', function() {
+  var $compile;
+  var $rootScope;
+  var element;
+  var buttons;
+  var givenColorSwitch;
+
+  beforeEach(angular.mock.module('roomie.devices', function($provide) {
+    $provide.factory('ColorSwitchButtonGenerator', function() {
+      return MockColorSwitchButtonGenerator;
+    });
+  }));
+
+  beforeEach(angular.mock.inject(function ($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
+  }));
+
+  beforeEach(function() {
+    buttons = null;
+    givenColorSwitch = null;
+  });
+
+  beforeEach(function () {
+    $rootScope.colorSwitch = {};
+    element = $compile('<color-switch-controls color-switch="colorSwitch"></color-switch-controls>')($rootScope);
+  });
+
+  it('gives the colorSwitch to the ColorSwitchButtonGenerator', function () {
+    expect(givenColorSwitch).toBe($rootScope.colorSwitch);
+  });
+
+  describe('the button group', function() {
+
+    it('has one', function() {
+      $rootScope.$digest();
+
+      expect($(element).find('.buttonGroup').length).toEqual(1);
+    });
+
+    it('has as many buttons as the button generator returned', function () {
+      buttons = [{}, {}, {}, {}, {}];
+      $rootScope.$digest();
+
+      expect(selectButtons().length).toEqual(5);
+    });
+
+    it('pulls labels from the button generator', function() {
+      buttons = [{
+          label: 'herp'
+        }, {
+          label: 'derp'
+        }, {
+          label: ''
+        }];
+
+      $rootScope.$digest();
+
+      expect(selectButton(0).text()).toEqual('herp');
+      expect(selectButton(1).text()).toEqual('derp');
+      expect(selectButton(2).text()).toEqual('');
+    });
+
+    it('pulls activated from the button generator', function() {
+      buttons = [{
+          activated: true
+        }, {
+          activated: false
+        }, {
+          activated: true
+        }];
+
+      $rootScope.$digest();
+
+      expect(selectButton(0).hasClass('activated')).toEqual(true);
+      expect(selectButton(1).hasClass('activated')).toEqual(false);
+      expect(selectButton(2).hasClass('activated')).toEqual(true);
+    });
+
+    it('pulls target color from the button generator (for rendering)', function () {
+      buttons = [{
+        color: 'orange'
+      }, {
+        color: 'green'
+      }, {
+        color: 'purple'
+      }];
+      
+      $rootScope.$digest();
+
+      expect(selectButton(0).css('background-color')).toEqual('orange');
+      expect(selectButton(1).css('background-color')).toEqual('green');
+      expect(selectButton(2).css('background-color')).toEqual('purple');
+
+      console.log(element.html());
+    });
+
+    it('pulls target color from the button generator', function() {
+      buttons = [{
+          color: 'orange'
+        }, {
+          color: 'green'
+        }, {
+          color: 'purple'
+        }];
+
+      $rootScope.colorSwitch.setValue = jasmine.createSpy('setValue');
+
+      $rootScope.$digest();
+
+      expect($rootScope.colorSwitch.setValue).not.toHaveBeenCalled();
+
+      selectButton(1).click();
+
+      expect($rootScope.colorSwitch.setValue).toHaveBeenCalledWith('green');
+    });
+
+    function selectButtons() {
+      return $(element).find('.buttonGroup .button').filter(':not(.button .button)');
+    }
+
+    function selectButton(index) {
+      return selectButtons().eq(index).find('.button');
+    }
+  });
+
+  function MockColorSwitchButtonGenerator(colorSwitch) {
+    givenColorSwitch = colorSwitch;
+
+    this.generate = function() {
+      return buttons;
+    };
+  }
+});
