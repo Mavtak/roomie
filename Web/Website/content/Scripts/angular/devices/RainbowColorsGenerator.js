ea22483beeb82042a446e4b8c8fbb663e33e7acd
David McGrath <github.com@davidmcgrath.com>
2015-06-20 18:33:37 -0400

    add RainbowColorsGenerator This mirrors the functionality used in the ColorSwitchButtons.cshtml partial view.  I added this line to the partial view to generate the hex values:
    var derp = String.Join("," + Environment.NewLine, colors.Select(x => x.ToHexString()).Select(x => "'" + x + "'"));

---
+++ Web/Website/content/Scripts/angular/devices/RainbowColorsGenerator.js
@@ -0,0 +1,39 @@
+﻿var module = angular.module('roomie.devices');
+
+module.factory('RainbowColorsGenerator', function () {
+
+  return function RainbowColorsGenerator() {
+
+    this.generate = function() {
+      var result = [
+        '#FF0000',
+        '#FF2900',
+        '#FF5200',
+        '#FF7B00',
+        '#FFA500',
+        '#FFBB00',
+        '#FFD200',
+        '#FFE800',
+        '#FFFF00',
+        '#BFDF00',
+        '#7FBF00',
+        '#3F9F00',
+        '#008000',
+        '#00603F',
+        '#00407F',
+        '#0020BF',
+        '#0000FF',
+        '#2000DF',
+        '#4000BF',
+        '#60009F',
+        '#800080',
+        '#9F0060',
+        '#BF0040',
+        '#DF0020'
+      ];
+
+      return result;
+    };
+  };
+
+});
---
+++ Web/Website/content/scriptsTests/angular/devices/RainbowColorsGenerator.spec.js
@@ -0,0 +1,52 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/devices/RainbowColorsGenerator.js"/>
+
+describe('roomie.devices.RainbowColorsGenerator', function () {
+  var RainbowColorsGenerator;
+  
+  beforeEach(angular.mock.module('roomie.devices'));
+
+  beforeEach(angular.mock.inject(function ($injector) {
+    RainbowColorsGenerator = $injector.get('RainbowColorsGenerator');
+  }));
+
+  describe('generate', function() {
+
+    it('returns a specific array of hex colors', function() {
+      var generator = new RainbowColorsGenerator({});
+
+      var result = generator.generate();
+
+      expect(result).toEqual([
+        '#FF0000',
+        '#FF2900',
+        '#FF5200',
+        '#FF7B00',
+        '#FFA500',
+        '#FFBB00',
+        '#FFD200',
+        '#FFE800',
+        '#FFFF00',
+        '#BFDF00',
+        '#7FBF00',
+        '#3F9F00',
+        '#008000',
+        '#00603F',
+        '#00407F',
+        '#0020BF',
+        '#0000FF',
+        '#2000DF',
+        '#4000BF',
+        '#60009F',
+        '#800080',
+        '#9F0060',
+        '#BF0040',
+        '#DF0020'
+      ]);
+    });
+  });
+
+});
