71146f01295412ed944e27717e1a054daa92f7d0
David McGrath <github.com@davidmcgrath.com>
2015-06-20 18:35:45 -0400

    added ColorSwitchButtonGenerator This is just like the MultilevelSwitchButtonGenerator, but for color switches.  It computes all of the data needed for rendering buttons for color switch controls.  It currently doesn't set the activated button, but the using code will respect the values, so that a change here will be the only thing needed for that to work.

---
+++ Web/Website/content/Scripts/angular/devices/ColorSwitchButtonGenerator.js
@@ -0,0 +1,28 @@
+﻿var module = angular.module('roomie.devices');
+
+module.factory('ColorSwitchButtonGenerator', ["RainbowColorsGenerator", function (RainbowColorsGenerator) {
+
+  return function ColorSwitchButtonGenerator() {
+    var rainbowColorsGenerator = new RainbowColorsGenerator();
+
+    this.generate = function() {
+      var result = [];
+      var colors = rainbowColorsGenerator.generate();
+
+      for (var i = 0; i < colors.length; i++) {
+        var label = '\xA0';
+        var color = colors[i];
+        var activated = false;
+
+        result.push({
+          label: label,
+          color: color,
+          activated: activated
+        });
+      }
+
+      return result;
+    };
+  };
+
+}]);
---
+++ Web/Website/content/scriptsTests/angular/devices/ColorSwitchButtonGenerator.spec.js
@@ -0,0 +1,86 @@
+﻿/// <reference path="../../../Scripts/Libraries/jquery-1.5.1.min.js"/>
+/// <reference path="../../../Scripts/Libraries/angular-1.3.13.min.js"/>
+/// <reference path="../angular-mocks.js"/>
+/// <reference path="../../../Scripts/angular/dependencies.js"/>
+/// <reference path="../../../Scripts/angular/devices/ColorSwitchButtonGenerator.js"/>
+
+describe('roomie.devices.ColorSwitchButtonGenerator', function () {
+  var ColorSwitchButtonGenerator;
+  var colors;
+
+  beforeEach(angular.mock.module('roomie.devices', function ($provide) {
+    $provide.factory('RainbowColorsGenerator', function () {
+      return MockRainbowColorsGenerator;
+    });
+  }));
+
+  beforeEach(angular.mock.inject(function ($injector) {
+    ColorSwitchButtonGenerator = $injector.get('ColorSwitchButtonGenerator');
+  }));
+  
+  beforeEach(function () {
+    colors = ['red', 'yellow', 'blue'];
+  });
+
+  describe('generate', function() {
+
+    it('returns a button for each color from the rainbow generator', function () {
+      var generator = new ColorSwitchButtonGenerator({});
+
+      var result = generator.generate();
+
+      expect(result.length).toEqual(3);
+    });
+
+    describe('color values for each button', function() {
+
+      it('corrosponds to each color from the rainbow generator', function() {
+        var generator = new ColorSwitchButtonGenerator({});
+
+        var result = generator.generate();
+
+        expect(result[0].color).toEqual('red');
+        expect(result[1].color).toEqual('yellow');
+        expect(result[2].color).toEqual('blue');
+      });
+
+    });
+
+    describe('label values for each button', function() {
+
+      it('are nonbreaking spaces', function() {
+        var generator = new ColorSwitchButtonGenerator({});
+
+        var result = generator.generate();
+
+        expect(result[0].label).toEqual('\xA0');
+        expect(result[1].label).toEqual('\xA0');
+        expect(result[2].label).toEqual('\xA0');
+      });
+
+    });
+
+    describe('activated values for each button', function() {
+
+      it('are always false', function () {
+        var generator = new ColorSwitchButtonGenerator({});
+
+        var result = generator.generate();
+
+        expect(result[0].activated).toEqual(false);
+        expect(result[1].activated).toEqual(false);
+        expect(result[2].activated).toEqual(false);
+      });
+
+    });
+
+  });
+  
+  function MockRainbowColorsGenerator() {
+
+    this.generate = function () {
+      return colors;
+    };
+  }
+
+});
