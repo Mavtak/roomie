b64ad5cf2e2506649aa46ee0b119514f24fc1bf4
David McGrath <github.com@davidmcgrath.com>
2015-07-04 11:56:10 -0400

    add widget-disconnected-icon directive

---
+++ Web/Website/content/Scripts/angular/common/widgetDisconnectedIcon.js
@@ -0,0 +1,22 @@
+﻿var module = angular.module('roomie.common');
+
+module.directive('widgetDisconnectedIcon', function() {
+
+  return {
+    restrict: 'E',
+    template: '' +
+      '<span class="icon disconnected">' +
+        '<span class="layer">' +
+          '<span class="pillar"></span>' +
+          '<span class="dash"></span>' +
+        '</span>' +
+        '<span class="layer">' +
+          '<span class="pillar"></span>' +
+          '<span class="slashContainer">' +
+            '<span class="slash"></span>' +
+          '</span>' +
+        '</span>' +
+      '</span>'
+  };
+
+});
