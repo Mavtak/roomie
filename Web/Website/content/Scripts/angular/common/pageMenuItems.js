778989bede062e2c4c6dd15fe88eaae3d787d8c3
David McGrath <github.com@davidmcgrath.com>
2015-07-05 17:51:42 -0400

    add pageMenuItems factory so that different directives can read and write data for the page menu

---
+++ Web/Website/content/Scripts/angular/common/pageMenuItems.js
@@ -0,0 +1,29 @@
+﻿var module = angular.module('roomie.common');
+
+module.factory('pageMenuItems', function () {
+  return new PageMenuItems();
+
+  function PageMenuItems() {
+    var _items = [];
+
+    this.any = function() {
+      return _items.length > 0;
+    };
+
+    this.list = function() {
+      return _items;
+    };
+
+    this.reset = function() {
+      this.set([]);
+    };
+
+    this.set = function(items) {
+      if (!_.isArray(items)) {
+        throw new Error('items must be array');
+      }
+
+      _items = items;
+    };
+  };
+});
