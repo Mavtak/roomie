21387a5ff91489f9b5d1b6dbc442410155b0e9eb
David McGrath <github.com@davidmcgrath.com>
2015-07-05 12:47:11 -0400

    add side-menu-set directive for containing a right and left side menu

---
+++ Web/Website/content/Scripts/angular/common/siteMenuSet.js
@@ -0,0 +1,40 @@
+﻿var module = angular.module('roomie.common');
+
+module.directive('sideMenuSet', function() {
+
+  return {
+    restrict: 'E',
+    transclude: true,
+    scope: {
+      bottom: '=bottom',
+      top: '=top',
+      width: '=width'
+    },
+    link: link,
+    template: '' +
+      '<div ' +
+        'ng-style="style" ' +
+        'ng-transclude' +
+        '>' +
+      '</div>'
+  };
+  
+  function link(scope) {
+    scope.style = {
+      position: 'fixed'
+    };
+
+    scope.$watch('top', updateStyle);
+    scope.$watch('bottom', updateStyle);
+    scope.$watch('width', updateStyle);
+
+    updateStyle();
+    
+    function updateStyle() {
+      scope.style.top = scope.top;
+      scope.style.bottom = scope.bottom;
+      scope.style.width = scope.width;
+    }
+  }
+
+});
