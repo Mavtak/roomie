c840743521944ffc0d85021afe380b35b86ea77f
David McGrath <github.com@davidmcgrath.com>
2015-07-12 12:41:17 -0400

    add and get internal users

---
+++ Web/Persistence/Repositories/UserRepositoryExtensions.cs
@@ -0,0 +1,55 @@
+﻿using Roomie.Web.Persistence.Helpers.Secrets;
+using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories
+{
+    public static class UserRepositoryExtensions
+    {
+        public static void Add(this IUserRepository repository, string username, string password)
+        {
+            var token = BuildInternalUserToken(username);
+            var secret = new PlainTextSecret(password);
+
+            var user = new UserModel
+            {
+                Alias = username,
+                Secret = secret.Format(),
+                Token = token
+            };
+
+            repository.Add(user);
+        }
+
+        public static UserModel Get(this IUserRepository repository, string username, string password)
+        {
+            var token = BuildInternalUserToken(username);
+            var result = repository.Get(token);
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            var secret = result.ParseSecret();
+
+            if (secret == null)
+            {
+                return null;
+            }
+
+            var verified = secret.Verify(password);
+
+            if (!verified)
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        private static string BuildInternalUserToken(string username)
+        {
+            return "internal:" + username;
+        }
+    }
+}
