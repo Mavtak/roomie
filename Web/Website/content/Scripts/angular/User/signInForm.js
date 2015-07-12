e7aa46a556c40bbc546b2f51b9a4db392044697e
David McGrath <github.com@davidmcgrath.com>
2015-07-12 14:06:00 -0400

    create sign-in-form directive

---
+++ Web/Website/content/Scripts/angular/User/signInForm.js
@@ -0,0 +1,20 @@
+﻿var module = angular.module('roomie.users');
+
+module.directive('signInForm', function() {
+
+  return {
+    restrict: 'E',
+    scope: {
+      password: '=password',
+      username: '=username',
+      submit: '=submit'
+    },
+    template: '' +
+      '<div>' +
+        '<div>username: <input type="text" ng-model="username"></div>' +
+        '<div>password: <input type="password" ng-model="password"></div>' +
+        '<button ng-click="submit()">Submit</button>' +
+      '</div>'
+  };
+
+});
