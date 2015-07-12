82e0908a62ce8b8b6ffcaea397b8d864a744db7b
David McGrath <github.com@davidmcgrath.com>
2015-07-12 12:43:23 -0400

    allow adding users via REST API

---
+++ Web/Website/Controllers/Api/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Roomie.Web.Persistence.Repositories;
+using Roomie.Web.Website.Helpers;
+
+namespace Roomie.Web.Website.Controllers.Api
+{
+    [AutoSave]
+    public class UserController : RoomieBaseApiController
+    {
+        public void Post(string username, string password)
+        {
+           Database.Users.Add(username, password);
+        }
+    }
+}
