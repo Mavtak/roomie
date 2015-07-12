f693be6d516ca05cbb4504a2fdc4e3df99f598aa
David McGrath <github.com@davidmcgrath.com>
2015-07-12 14:09:16 -0400

    add route for sign-in page

--- Web/Website/content/Scripts/angular/User/routing.js
+++ Web/Website/content/Scripts/angular/User/routing.js
@@ -1,4 +1,15 @@
 ﻿var module = angular.module('roomie.users');
 
 module.config(['$stateProvider', function($stateProvider) {
+  $stateProvider.state('sign-in', {
+    url: '/sign-in',
+    controller: 'SignInController',
+    template: '' +
+      '<sign-in-form ' +
+        'username="username" ' +
+        'password="password" ' +
+        'submit="submit"' +
+        '>' +
+      '</sign-in-form>'
+  });
 }]);
