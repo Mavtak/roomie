7b5ce10810476acc4c5e2105f0453c9fd1419e47
David McGrath <github.com@davidmcgrath.com>
2015-07-12 12:52:05 -0400

    implement BCryptSecret using BCrypt.Net

---
+++ Web/Persistence/Helpers/Secrets/BCryptSecret.cs
@@ -0,0 +1,45 @@
+﻿namespace Roomie.Web.Persistence.Helpers.Secrets
+{
+    public class BCryptSecret : ISecret
+    {
+        public const string Name = "bcrypt";
+
+        private string _hash;
+
+        public BCryptSecret(string hash)
+        {
+            _hash = hash;
+        }
+
+        string ISecret.Name
+        {
+            get
+            {
+                return Name;
+            }
+        }
+
+        public string StoredValue
+        {
+            get
+            {
+                return _hash;
+            }
+        }
+
+        public bool Verify(string value)
+        {
+            var result = BCrypt.Net.BCrypt.Verify(value, _hash);
+
+            return result;
+        }
+
+        public static BCryptSecret FromPassword(string password)
+        {
+            var hash = BCrypt.Net.BCrypt.HashPassword(password);
+            var result = new BCryptSecret(hash);
+
+            return result;
+        }
+    }
+}
