0d49bf16c71d28daccb616b3d87b28425f29d17f
David McGrath <github.com@davidmcgrath.com>
2015-11-28 16:36:03 -0500

    copy guests from one network to another

---
+++ Web/Persistence/Repositories/NetworkGuestRepositoryExtensions.cs
@@ -0,0 +1,20 @@
+﻿using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories
+{
+    public static class NetworkGuestRepositoryExtensions
+    {
+        public static void Copy(this INetworkGuestRepository repository, Network source, Network destination)
+        {
+            var users = repository.Get(source);
+
+            foreach (var user in users)
+            {
+                if (!repository.Check(destination, user))
+                {
+                    repository.Add(destination, user);
+                }
+            }
+        }
+    }
+}
