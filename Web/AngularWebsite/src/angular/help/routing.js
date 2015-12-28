a02abc637a0d03b87a4e711fd19d298eab830b1d
David McGrath <github.com@davidmcgrath.com>
2015-12-28 17:55:32 -0500

    add help index (which is the same content as the MVC one)

--- Web/AngularWebsite/src/angular/help/routing.js
+++ Web/AngularWebsite/src/angular/help/routing.js
@@ -1,6 +1,11 @@
 angular.module('roomie.help').config(function (
   $stateProvider
   ) {
+  $stateProvider.state('help', {
+    url: '/help',
+    templateUrl: 'help/pages/index.html'
+  });
+
   $stateProvider.state('help/about', {
     url: '/help/about',
     templateUrl: 'help/pages/about.html'
