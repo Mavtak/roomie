b54304cfffc98edcfe16ae9a60894bee208b1dd2
David McGrath <github.com@davidmcgrath.com>
2015-12-28 17:45:21 -0500

    add about page (which is the same content as the MVC homepage)

--- Web/AngularWebsite/src/angular/help/routing.js
+++ Web/AngularWebsite/src/angular/help/routing.js
@@ -1,4 +1,8 @@
 angular.module('roomie.help').config(function (
   $stateProvider
   ) {
+  $stateProvider.state('help/about', {
+    url: '/help/about',
+    templateUrl: 'help/pages/about.html'
+  });
 });
