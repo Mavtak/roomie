5bb08f2fe33c975a02e575044540db5a490588f9
David McGrath <github.com@davidmcgrath.com>
2015-12-29 13:48:10 -0500

    expose command documentation API route

---
+++ Web/Website/Controllers/Api/CommandDocumentationController.cs
@@ -0,0 +1,26 @@
+﻿using Roomie.Desktop.Engine;
+using Roomie.Web.Persistence.Models;
+using Roomie.Web.Persistence.Repositories.StaticRepositories;
+using Roomie.Web.Website.Helpers;
+
+namespace Roomie.Web.Website.Controllers.Api
+{
+    public class CommandDocumentationController : RoomieBaseApiController
+    {
+        private CommandDocumentationRepository _commandDocumentationRepository;
+
+        public CommandDocumentationController()
+        {
+            var binPath = System.Web.Hosting.HostingEnvironment.MapPath("~/bin");
+            var library = new RoomieCommandLibrary();
+            library.AddCommandsFromPluginFolder(binPath);
+
+            _commandDocumentationRepository = new CommandDocumentationRepository(library);
+        }
+
+        public Command[] Get()
+        {
+            return _commandDocumentationRepository.Get();
+        }
+    }
+}
