bfb98f536fd27aaf8a915846ce59b4cc945a7c7f
David McGrath <github.com@davidmcgrath.com>
2016-01-25 22:19:08 -0500

    add deviceTypes data

---
+++ Web/AngularWebsite/src/angular/data/deviceTypes/script.js
@@ -0,0 +1,11 @@
+angular.module('roomie.data').value('deviceTypes', [
+  'Binary Sensor',
+  'Binary Switch',
+  'Controller',
+  'Keypad',
+  'Lock',
+  'Multilevel Sensor',
+  'Multilevel Switch',
+  'Relay',
+  'Thermostat',
+]);
