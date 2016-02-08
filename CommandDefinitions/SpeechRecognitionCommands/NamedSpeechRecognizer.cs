606a860f2df24fbfd4cea1fe2bcc5531edbcfde8
David McGrath <github.com@davidmcgrath.com>
2016-02-08 08:45:22 -0500

    added NamedSpeechRecognizer which has a name, and won't recognize phrases until its name is said.  this should reduce misrecognitions

---
+++ CommandDefinitions/SpeechRecognitionCommands/NamedSpeechRecognizer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Roomie.CommandDefinitions.SpeechRecognition
+{
+    public class NamedSpeechRecognizer
+    {
+        AutomaticallyResettingSwitch _automaticallyResettingSwitch;
+        private string _name;
+        private PhraseBasedSpeechRecognizer _speechRecognizer;
+
+        public NamedSpeechRecognizer(TimeSpan attentionLength, string name)
+        {
+            _automaticallyResettingSwitch = new AutomaticallyResettingSwitch(attentionLength);
+            _name = name;
+            _speechRecognizer = new PhraseBasedSpeechRecognizer();
+            _speechRecognizer.RegisterPhrase(name);
+        }
+
+        public NamedSpeechRecognizerResponse Recognize(double confidence)
+        {
+            while (true)
+            {
+                var input = _speechRecognizer.Recognize(confidence);
+
+                return ProcessInput(input);
+            }
+        }
+
+        public void RegisterPhrase(string phrase)
+        {
+            _speechRecognizer.RegisterPhrase(phrase);
+        }
+
+        public void UnregisterPhrase(string phrase)
+        {
+            _speechRecognizer.UnregisterPhrase(phrase);
+        }
+
+        private NamedSpeechRecognizerResponse ProcessInput(string input)
+        {
+            if (input == _name)
+            {
+                _automaticallyResettingSwitch.SwitchOn();
+                return new NamedSpeechRecognizerResponse(input, NamedSpeechRecognizerResponse.StatusProperty.NameRecognized);
+            }
+
+            if (!_automaticallyResettingSwitch.IsOn)
+            {
+                return new NamedSpeechRecognizerResponse(input, NamedSpeechRecognizerResponse.StatusProperty.NameRequired);
+            }
+
+            return new NamedSpeechRecognizerResponse(input, NamedSpeechRecognizerResponse.StatusProperty.PhraseRecognized);
+        }
+    }
+}
---
+++ CommandDefinitions/SpeechRecognitionCommands/NamedSpeechRecognizerResponse.cs
@@ -0,0 +1,21 @@
+﻿namespace Roomie.CommandDefinitions.SpeechRecognition
+{
+    public class NamedSpeechRecognizerResponse
+    {
+        public string Phrase { get; private set; }
+        public StatusProperty Status { get; private set; }
+
+        public NamedSpeechRecognizerResponse(string phrase, StatusProperty status)
+        {
+            Phrase = phrase;
+            Status = status;
+        }
+
+        public enum StatusProperty
+        {
+            NameRecognized,
+            NameRequired,
+            PhraseRecognized,
+        }
+    }
+}
