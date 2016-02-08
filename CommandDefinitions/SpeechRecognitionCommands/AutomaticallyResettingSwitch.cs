ba6005c49c7dc5d7c2bcc28948427b190bc39fe0
David McGrath <github.com@davidmcgrath.com>
2016-02-08 08:44:59 -0500

    implemented AutomaticalyResettingSwitch which handles the state of automatically deactivating something after a timeout

---
+++ CommandDefinitions/SpeechRecognitionCommands/AutomaticallyResettingSwitch.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Roomie.CommandDefinitions.SpeechRecognition
+{
+    public class AutomaticallyResettingSwitch
+    {
+        private TimeSpan _resetInterval;
+        private DateTime? _lastSwitchOn;
+
+        public AutomaticallyResettingSwitch(TimeSpan resetInterval)
+        {
+            _resetInterval = resetInterval;
+        }
+
+        public void SwitchOn()
+        {
+            _lastSwitchOn = GetNow();
+        }
+
+        public bool IsOn
+        {
+            get
+            {
+                if (_lastSwitchOn == null)
+                {
+                    return false;
+                }
+
+                if (_lastSwitchOn.Value.Add(_resetInterval) < GetNow())
+                {
+                    return false;
+                }
+
+                return true;
+            }
+        }
+
+        private DateTime GetNow()
+        {
+            return DateTime.UtcNow;
+        }
+    }
+}
