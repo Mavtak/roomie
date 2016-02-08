c7a530ab8013c669c71a6114f1c22cf561a53c8c
David McGrath <github.com@davidmcgrath.com>
2016-02-08 08:42:39 -0500

    implemented PhraseBasedSpeechRecognizer with a simpler API than the built-in speech recognizer.

---
+++ CommandDefinitions/SpeechRecognitionCommands/PhraseBasedSpeechRecognizer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.Speech.Recognition;
+
+namespace Roomie.CommandDefinitions.SpeechRecognition
+{
+    public class PhraseBasedSpeechRecognizer
+    {
+        private Dictionary<string, Grammar> _grammars;
+        private SpeechRecognitionEngine _speechRecognizer;
+
+        public PhraseBasedSpeechRecognizer()
+        {
+            _grammars = new Dictionary<string, Grammar>();
+            _speechRecognizer = new SpeechRecognitionEngine();
+
+            _speechRecognizer.SetInputToDefaultAudioDevice();
+        }
+
+        public string Recognize(double confidence)
+        {
+            while (true)
+            {
+                var result = _speechRecognizer.Recognize();
+
+                if (result != null && result.Confidence >= confidence)
+                {
+                    return result.Text;
+                }
+            }
+        }
+
+        public void RegisterPhrase(string phrase)
+        {
+            var grammarBuilder = new GrammarBuilder();
+            grammarBuilder.Append(phrase);
+
+            var grammar = new Grammar(grammarBuilder);
+
+            _grammars.Add(phrase, grammar);
+            _speechRecognizer.LoadGrammar(grammar);
+
+        }
+
+        public void UnregisterPhrase(string phrase)
+        {
+            var grammar = _grammars[phrase];
+
+            _speechRecognizer.UnloadGrammar(grammar);
+            _grammars.Remove(phrase);
+        }
+    }
+}
