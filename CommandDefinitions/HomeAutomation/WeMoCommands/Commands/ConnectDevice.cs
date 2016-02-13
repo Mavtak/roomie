434afb9284ed3c274ec7389bdb408cf97f67ff9c
David McGrath <github.com@davidmcgrath.com>
2016-02-13 14:27:54 -0500

    expose WeMo-specific operations through commands

---
+++ CommandDefinitions/HomeAutomation/WeMoCommands/Commands/ConnectDevice.cs
@@ -0,0 +1,19 @@
+﻿using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.WeMoCommands.Commands
+{
+    [StringParameter("IP")]
+    [Group("WeMo")]
+    public class ConnectDevice : HomeAutomationNetworkCommand
+    {
+        protected override void Execute_HomeAutomationNetworkDefinition(HomeAutomationCommandContext context)
+        {
+            var interpreter = context.Interpreter;
+            var network = context.Network as WeMoNetwork;
+            var ip = context.ReadParameter("IP").Value;
+
+            network.Connect(ip);
+        }
+    }
+}
---
+++ CommandDefinitions/HomeAutomation/WeMoCommands/Commands/RegisterNetwork.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Roomie.CommandDefinitions.HomeAutomationCommands;
+using Roomie.Desktop.Engine.Commands;
+
+namespace Roomie.CommandDefinitions.WeMoCommands.Commands
+{
+    [Group("WeMo")]
+    public class RegisterNetwork : HomeAutomationCommand
+    {
+        protected override void Execute_HomeAutomationDefinition(HomeAutomationCommandContext context)
+        {
+            var interpreter = context.Interpreter;
+            var networks = context.Networks;
+
+            var networkContext = new HomeAutomationNetworkContext(context.Engine, context.ThreadPool);
+            var network = new WeMoNetwork(networkContext);
+
+            networks.Add(network);
+
+            interpreter.WriteEvent("Done.");
+        }
+    }
+}
