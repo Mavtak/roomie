b68dbb7c1a9dc71ab90de664fabaed326eef0deb
David McGrath <github.com@davidmcgrath.com>
2016-04-02 22:53:15 -0400

    update style

--- Web/AngularWebsite/src/angular/tasks/task-widget/test.spec.js
+++ Web/AngularWebsite/src/angular/tasks/task-widget/test.spec.js
@@ -5,9 +5,9 @@ describe('angular roomie.task task-widget (directive)', function () {
 
   beforeEach(angular.mock.module('roomie.tasks'));
 
-  beforeEach(angular.mock.inject(function (_$compile_, _$rootScope_) {
-    $compile = _$compile_;
-    $rootScope = _$rootScope_;
+  beforeEach(angular.mock.inject(function ($injector) {
+    $compile = $injector.get('$compile');
+    $rootScope = $injector.get('$rootScope');
   }));
 
   beforeEach(function () {
