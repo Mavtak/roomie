d1e839fe0e9177b0704dc4dcd246d65519db94b0
David McGrath <github.com@davidmcgrath.com>
2016-04-15 06:57:32 -0400

    add humidity sensor commands

---
+++ CommandDefinitions/HomeAutomation/HomeAutomationCommands/Commands/HomeAutomation/RegisterHumidityFallsBelowValueCommand.cs
@@ -0,0 +1,30 @@
+﻿using Roomie.Common.HomeAutomation.Events.Triggers;
+using Roomie.Common.Triggers;
+using Roomie.Desktop.Engine.Commands;
+using Roomie.Desktop.Engine.Parameters;
+
+namespace Roomie.CommandDefinitions.HomeAutomationCommands.Commands.HomeAutomation
+{
+    [HumidityParameter("Target")]
+    public class RegisterHumidityFallsBelowValueCommand : HomeAutomationSingleDeviceCommand
+    {
+        protected override void Execture_HomeAutomationSingleDeviceDefinition(HomeAutomationSingleDeviceContext context)
+        {
+            var target = context.ReadParameter("Target").ToHumidity();
+            var originalCommand = context.OriginalCommand;
+            var commands = originalCommand.InnerCommands;
+            var device = context.Device;
+            var network = device.Network;
+            var triggers = network.Context.Triggers;
+            var history = network.Context.History;
+            var threadPool = network.Context.ThreadPool;
+
+            var trigger = new WhenHumidityFallsBelowValueTrigger(device, target, history.DeviceEvents);
+            var action = new RunScriptTriggerAction(threadPool, commands);
+
+            var triggerBundle = new TriggerBundle(trigger, action);
+
+            triggers.Add(triggerBundle);
+        }
+    }
+}
