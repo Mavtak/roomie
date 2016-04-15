71b30ee9444963fd720b67924e812046ccfed2c8
David McGrath <github.com@davidmcgrath.com>
2016-04-15 06:53:10 -0400

    add triggers for changing humidity

---
+++ Common/HomeAutomation/Events/Triggers/WhenHumidityFallsBelowValueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using Roomie.Common.Measurements.Humidity;
+
+namespace Roomie.Common.HomeAutomation.Events.Triggers
+{
+    public class WhenHumidityFallsBelowValueTrigger : DeviceStateChangedTriggerBase<IHumidity>
+    {
+        private readonly IHumidity _target;
+        public WhenHumidityFallsBelowValueTrigger(IDevice device, IHumidity target, IDeviceHistory history)
+            : base(device, history)
+        {
+            _target = target;
+        }
+
+        protected override IHumidity GetMeasurement(IDeviceState state)
+        {
+            return state?.HumiditySensorState?.Value;
+        }
+
+        protected override bool IsAMatch(IHumidity previousValue, IHumidity currentValue)
+        {
+            var wasAboveTarget = previousValue.Relative.Value >= _target.Relative.Value;
+            var isBelowTarget = currentValue.Relative.Value < _target.Relative.Value;
+
+            return wasAboveTarget && isBelowTarget;
+        }
+    }
+}
---
+++ Common/HomeAutomation/Events/Triggers/WhenHumidityRisesAboveValueTrigger.cs
@@ -0,0 +1,27 @@
+﻿using Roomie.Common.Measurements.Humidity;
+
+namespace Roomie.Common.HomeAutomation.Events.Triggers
+{
+    public class WhenHumidityRisesAboveValueTrigger : DeviceStateChangedTriggerBase<IHumidity>
+    {
+        private readonly IHumidity _target;
+        public WhenHumidityRisesAboveValueTrigger(IDevice device, IHumidity target, IDeviceHistory history)
+            : base(device, history)
+        {
+            _target = target;
+        }
+
+        protected override IHumidity GetMeasurement(IDeviceState state)
+        {
+            return state?.HumiditySensorState?.Value;
+        }
+
+        protected override bool IsAMatch(IHumidity previousValue, IHumidity currentValue)
+        {
+            var wasBelowTarget = previousValue.Relative.Value <= _target.Relative.Value;
+            var isAboveTarget = currentValue.Relative.Value > _target.Relative.Value;
+
+            return wasBelowTarget && isAboveTarget;
+        }
+    }
+}
