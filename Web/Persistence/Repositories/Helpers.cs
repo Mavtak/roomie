426c90a654c065b3546c524ef313d3efe026c8d7
David McGrath <github.com@davidmcgrath.com>
2016-08-06 09:13:28 -0400

    add helper for adding new users to a house

---
+++ Web/Persistence/Repositories/Helpers.cs
@@ -0,0 +1,24 @@
+﻿using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories
+{
+    public static class Helpers
+    {
+        public static void AddUserAndCopyAccess(IUserRepository userRepository, INetworkRepository networkRepository, INetworkGuestRepository networkGuestRepository, string username, string password, string referenceUsername)
+        {
+            var user = User.Create(string.Join(":", "internal", username));
+            user.Secret = Web.Persistence.Helpers.Secrets.BCryptSecret.FromPassword(password);
+
+            userRepository.Add(user);
+
+            var referenceUser = userRepository.Get(string.Join(":", "internal", referenceUsername));
+
+            var networks = networkRepository.Get(referenceUser);
+
+            foreach (var network in networks)
+            {
+                networkGuestRepository.Add(network, user);
+            }
+        }
+    }
+}
