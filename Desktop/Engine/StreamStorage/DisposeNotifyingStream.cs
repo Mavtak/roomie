1b46048e193bcb751c13e2be338bf47ad30dbfac
David McGrath <github.com@davidmcgrath.com>
2016-09-10 10:25:18 -0400

    add stream store that writes to temp files first

---
+++ Desktop/Engine/StreamStorage/DisposeNotifyingStream.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.IO;
+
+namespace Roomie.Desktop.Engine.StreamStorage
+{
+    public class DisposeNotifyingStream : Stream
+    {
+        private Action _disposeAction;
+        private bool _notifiedOfDispose;
+        private Stream _stream;
+
+        public DisposeNotifyingStream(Stream stream, Action disposeAction)
+        {
+            _disposeAction = disposeAction;
+            _stream = stream;
+        }
+
+        public override bool CanRead => _stream.CanRead;
+
+        public override bool CanSeek => _stream.CanSeek;
+
+        public override bool CanWrite => _stream.CanWrite;
+
+        public override long Length => _stream.Length;
+
+        public override long Position
+        {
+            get
+            {
+                return _stream.Position;
+            }
+
+            set
+            {
+                _stream.Position = value;
+            }
+        }
+
+        public override void Flush()
+        {
+            _stream.Flush();
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _stream.Read(buffer, offset, count);
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            _stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _stream.Write(buffer, offset, count);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            _stream.Dispose();
+
+            if (!_notifiedOfDispose)
+            {
+                _notifiedOfDispose = true;
+                _disposeAction();
+            }
+        }
+    }
+}
---
+++ Desktop/Engine/StreamStorage/SafeWritingStreamStore.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+namespace Roomie.Desktop.Engine.StreamStorage
+{
+    public class SafeWritingStreamStore : IStreamStore
+    {
+        IStreamStore _streamStore;
+
+        public SafeWritingStreamStore(IStreamStore streamStore)
+        {
+            _streamStore = streamStore;
+        }
+
+        public Stream OpenRead(string name)
+        {
+            return _streamStore.OpenRead(name);
+        }
+
+        public Stream OpenWrite(string name)
+        {
+            var tempName = string.Join(".", new[] {
+                name,
+                Guid.NewGuid().ToString().Replace("-", "").Substring(0, 4),
+                "tmp",
+            });
+            Action rename = () => _streamStore.Rename(tempName, name);
+            var stream = _streamStore.OpenWrite(tempName);
+            var disposeNotifyingStream = new DisposeNotifyingStream(stream, rename);
+
+            return disposeNotifyingStream;
+        }
+
+        public void Rename(string oldName, string newName)
+        {
+            _streamStore.Rename(oldName, newName);
+        }
+    }
+}
