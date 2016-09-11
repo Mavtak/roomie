4749d3735607228e2b842c5233e2dd1450d8d29f
David McGrath <github.com@davidmcgrath.com>
2016-09-11 16:37:00 -0400

    add function to toggle a setpoint for example: if the core mode is "auto" (where the thermostat respects both the "heat" and "cool" setpoints), and the "cool" setpoint is toggled, then the function would return "heat". (check the unit tests for all of the inputs and outputs ;-))

---
+++ Web/AngularWebsite/src/angular/devices/getNewModeToToggleSetpoint/script.js
@@ -0,0 +1,53 @@
+angular.module('roomie.data').factory('getNewModeToToggleSetpoint', function () {
+
+  return function getNewModeToToggleSetpoint(currentMode, toggledSetpoint) {
+    if (currentMode === undefined || toggledSetpoint === undefined) {
+      return;
+    }
+
+    currentMode = currentMode.toLowerCase();
+    toggledSetpoint = toggledSetpoint.toLowerCase();
+
+    switch (currentMode) {
+      case 'off':
+        switch (toggledSetpoint) {
+          case 'heat':
+            return 'heat';
+
+          case 'cool':
+            return 'cool';
+        }
+        break;
+
+      case 'heat':
+        switch (toggledSetpoint) {
+          case 'heat':
+            return 'off';
+
+          case 'cool':
+            return 'auto';
+        }
+        break;
+
+      case 'cool':
+        switch (toggledSetpoint) {
+          case 'heat':
+            return 'auto';
+
+          case 'cool':
+            return 'off';
+        }
+        break;
+
+      case 'auto':
+        switch (toggledSetpoint) {
+          case 'heat':
+            return 'cool';
+
+          case 'cool':
+            return 'heat';
+        }
+        break;
+    }
+  };
+});
---
+++ Web/AngularWebsite/src/angular/devices/getNewModeToToggleSetpoint/test.spec.js
@@ -0,0 +1,45 @@
+fdescribe('angular roomie.devices getNewModeToToggleSetpoint (factory)', function () {
+
+  var $injector;
+
+  beforeEach(angular.mock.module('roomie.devices'));
+
+  beforeEach(angular.mock.inject(function (_$injector_) {
+    $injector = _$injector_;
+  }));
+
+  it('toggles valid values', function () {
+    var subject = $injector.get('getNewModeToToggleSetpoint');
+
+    expect(subject('off', 'cool')).toBe('cool');
+    expect(subject('off', 'heat')).toBe('heat');
+
+    expect(subject('cool', 'cool')).toBe('off');
+    expect(subject('cool', 'heat')).toBe('auto');
+
+    expect(subject('heat', 'cool')).toBe('auto');
+    expect(subject('heat', 'heat')).toBe('off');
+
+    expect(subject('auto', 'cool')).toBe('heat');
+    expect(subject('auto', 'heat')).toBe('cool');
+  });
+
+  it('returns undefined for invalid values', function () {
+    var subject = $injector.get('getNewModeToToggleSetpoint');
+
+    expect(subject()).toBeUndefined();
+
+    expect(subject('', '')).toBeUndefined();
+    expect(subject('off', '')).toBeUndefined();
+    expect(subject('', 'cool')).toBeUndefined();
+
+    expect(subject('derp', 'derp')).toBeUndefined();
+    expect(subject('off', 'derp')).toBeUndefined();
+    expect(subject('derp', 'cool')).toBeUndefined();
+
+    expect(subject(undefined, undefined)).toBeUndefined();
+    expect(subject('off', undefined)).toBeUndefined();
+    expect(subject(undefined, 'cool')).toBeUndefined();
+  });
+
+});
