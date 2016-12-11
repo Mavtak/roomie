f8fcd22e95608ed61ebc640e43725ce88f7e0ddf
David McGrath <github.com@davidmcgrath.com>
2016-12-11 17:40:27 -0500

    load networks also handling loading state, sign-in state, and page menu items

--- Web/AngularWebsite/src/angular/networks.pages/NetworkDetailController/script.js
+++ Web/AngularWebsite/src/angular/networks.pages/NetworkDetailController/script.js
@@ -1,4 +1,35 @@
 angular.module('roomie.networks.pages').controller('NetworkDetailController', function (
+  $http,
+  $state,
+  pageMenuItems,
+  signInState,
+  wholePageStatus
 ) {
 
+  var controller = this;
+
+  wholePageStatus.set('loading');
+  pageMenuItems.reset();
+
+  $http.get('/api/network/' + $state.params.id)
+    .then(function (result) {
+      controller.network = result.data;
+
+      signInState.set('signed-in');
+      wholePageStatus.set('ready');
+    }, function (result) {
+      var errors = result.data;
+
+      var signInError = _.isArray(errors) && _.some(errors, {
+        type: 'must-sign-in'
+      });
+
+      if (signInError) {
+        signInState.set('signed-out');
+      }
+
+      wholePageStatus.set('ready');
+
+      //TODO: handle other errors
+    });
 });
