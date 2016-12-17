2d469e8cd1746b3e762d98da30de91f1fec28f8d
David McGrath <github.com@davidmcgrath.com>
2016-12-17 18:26:35 -0500

    implement add and remove device in UI

--- Web/AngularWebsite/src/angular/networks/network-widget/script.js
+++ Web/AngularWebsite/src/angular/networks/network-widget/script.js
@@ -1,4 +1,6 @@
-angular.module('roomie.networks').directive('networkWidget', function () {
+angular.module('roomie.networks').directive('networkWidget', function (
+  $http
+) {
 
   return {
     restrict: 'E',
@@ -6,6 +8,20 @@ angular.module('roomie.networks').directive('networkWidget', function () {
       network: '=network',
     },
     templateUrl: 'networks/network-widget/template.html',
+    link: link,
   };
+
+  function link(scope) {
+    scope.addDevice = addDevice;
+    scope.removeDevice = removeDevice;
+
+    function addDevice() {
+      $http.post('/api/network/' + scope.network.id + '?action=AddDevice');
+    }
+
+    function removeDevice() {
+      $http.post('/api/network/' + scope.network.id + '?action=RemoveDevice');
+    }
+  }
 
 });
