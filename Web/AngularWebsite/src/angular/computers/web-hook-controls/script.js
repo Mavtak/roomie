a24ac23ef59bd89c842e9c5f2e7bd7edb6f71cb5
David McGrath <github.com@davidmcgrath.com>
2016-12-18 10:55:37 -0500

    implement web-hook-controls

---
+++ Web/AngularWebsite/src/angular/computers/web-hook-controls/script.js
@@ -0,0 +1,40 @@
+angular.module('roomie.computers').directive('webHookControls', function (
+  $http,
+  $window
+) {
+
+  return {
+    restrict: 'E',
+    scope: {
+      computer: '=computer'
+    },
+    templateUrl: 'computers/web-hook-controls/template.html',
+    link: link
+  };
+
+  function link(scope) {
+    scope.disable = disable;
+    scope.renew = renew;
+
+    Object.defineProperty(scope, 'script', {
+      get: calculateScript
+    });
+
+    function calculateScript() {
+      if (!scope.computer || !scope.computer.accessKey || !scope.computer.encryptionKey) {
+        return;
+      }
+
+      return '<WebHook.Connect ComputerName="' + scope.computer.name + '" CommunicationURL="' + $window.location.origin + '/communicator/" AccessKey="' + scope.computer.accessKey + '" EncryptionKey="' + scope.computer.encryptionKey + '" />';
+    }
+
+    function disable() {
+      $http.post('/api/computer/' + scope.computer.id + '?action=DisableWebHook');
+    }
+
+    function renew() {
+      $http.post('/api/computer/' + scope.computer.id + '?action=RenewWebHookKeys');
+    }
+  }
+
+});
