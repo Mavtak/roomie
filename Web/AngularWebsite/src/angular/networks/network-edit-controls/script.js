0e2be4a4d246a6936def686cbf2065d58eec5e8d
David McGrath <github.com@davidmcgrath.com>
2016-12-17 20:15:20 -0500

    implement network-edit-controls

---
+++ Web/AngularWebsite/src/angular/networks/network-edit-controls/script.js
@@ -0,0 +1,38 @@
+angular.module('roomie.networks').directive('networkEditControls', function (
+  $http
+) {
+
+  return {
+    restrict: 'E',
+    scope: {
+      network: '=network'
+    },
+    link: link,
+    templateUrl: 'networks/network-edit-controls/template.html'
+  };
+
+  function link(scope) {
+    scope.model = {};
+    scope.save = save;
+
+    waitUntilLoaded(scope, 'network', function () {
+      scope.model.name = scope.network.name;
+    });
+
+    function save() {
+      $http.put('/api/network/' + scope.network.id, scope.model);
+    };
+  }
+
+  function waitUntilLoaded(scope, variableName, callback) {
+    var unbindWatch = scope.$watch(variableName, function (value) {
+      if (value === undefined) {
+        return;
+      }
+
+      unbindWatch();
+      callback();
+    });
+  }
+
+});
