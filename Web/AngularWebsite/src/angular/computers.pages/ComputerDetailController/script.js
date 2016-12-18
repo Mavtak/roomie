49585f88bde3afec63d460751808b0280f0ede3a
David McGrath <github.com@davidmcgrath.com>
2016-12-17 20:59:53 -0500

    load computers also handling loading state, sign-in state, and page menu items

--- Web/AngularWebsite/src/angular/computers.pages/ComputerDetailController/script.js
+++ Web/AngularWebsite/src/angular/computers.pages/ComputerDetailController/script.js
@@ -1,4 +1,35 @@
 angular.module('roomie.computers.pages').controller('ComputerDetailController', function (
+  $http,
+  $state,
+  pageMenuItems,
+  signInState,
+  wholePageStatus
 ) {
 
+  var controller = this;
+
+  wholePageStatus.set('loading');
+  pageMenuItems.reset();
+
+  $http.get('/api/computer/' + $state.params.id)
+    .then(function (result) {
+      controller.computer = result.data;
+
+      signInState.set('signed-in');
+      wholePageStatus.set('ready');
+    }, function (result) {
+      var errors = result.data;
+
+      var signInError = _.isArray(errors) && _.some(errors, {
+        type: 'must-sign-in'
+      });
+
+      if (signInError) {
+        signInState.set('signed-out');
+      }
+
+      wholePageStatus.set('ready');
+
+      //TODO: handle other errors
+    });
 });
--- Web/AngularWebsite/src/angular/computers.pages/ComputerListController/script.js
+++ Web/AngularWebsite/src/angular/computers.pages/ComputerListController/script.js
@@ -1,4 +1,34 @@
 angular.module('roomie.computers.pages').controller('ComputerListController', function (
+  $http,
+  pageMenuItems,
+  signInState,
+  wholePageStatus
 ) {
 
+  var controller = this;
+
+  wholePageStatus.set('loading');
+  pageMenuItems.reset();
+
+  $http.get('/api/computer/')
+    .then(function (result) {
+      controller.computers = result.data;
+
+      signInState.set('signed-in');
+      wholePageStatus.set('ready');
+    }, function (result) {
+      var errors = result.data;
+
+      var signInError = _.isArray(errors) && _.some(errors, {
+        type: 'must-sign-in'
+      });
+
+      if (signInError) {
+        signInState.set('signed-out');
+      }
+
+      wholePageStatus.set('ready');
+
+      //TODO: handle other errors
+    });
 });
