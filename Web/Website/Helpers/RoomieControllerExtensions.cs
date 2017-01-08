c8440bd2c81bdbabb5e5375b78a060bbff73305e
David McGrath <github.com@davidmcgrath.com>
2017-01-08 11:15:16 -0500

    remove unused code

--- Web/Website/Helpers/RoomieControllerExtensions.cs
+++ Web/Website/Helpers/RoomieControllerExtensions.cs
@@ -5,21 +5,6 @@ namespace Roomie.Web.Website.Helpers
 {
     public static class RoomieControllerExtensions
     {
-        public static Task SelectTask(this IRoomieController controller, int id)
-        {
-            var database = controller.Database;
-            var user = controller.User;
-
-            var task = database.Tasks.Get(user, id);
-
-            if (task == null)
-            {
-                throw new HttpException(404, "Task not found");
-            }
-
-            return task;
-        }
-
         public static void AddTask(this IRoomieController controller, Computer computer, string origin, string scriptText)
         {
             var database = controller.Database;
