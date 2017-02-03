6331f527a737a360f40554a5e1270164c38828cc
David McGrath <github.com@davidmcgrath.com>
2017-02-03 08:41:09 -0500

    add helper class for building SQL

---
+++ Web/Persistence/Repositories/DapperRepositories/SqlUtilities.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories
+{
+    public static class SqlUtilities
+    {
+        public static string OrderByDirection(SortDirection sortDirection)
+        {
+            switch (sortDirection)
+            {
+                case SortDirection.Ascending:
+                    return "ASC";
+
+                case SortDirection.Descending:
+                    return "DESC";
+
+                default:
+                    throw new ArgumentException($"Unrecognized SortDirection \"{sortDirection}\".", nameof(sortDirection));
+            }
+        }
+    }
+}
