9e4e876cb1f32eea2d80bfb2f6e861b193f28b35
David McGrath <github.com@davidmcgrath.com>
2017-02-23 22:19:56 -0500

    implement WebHookSessionRepository which is one half of ISessionRepository, and interface that should be split in two

---
+++ Web/Persistence/Repositories/DapperRepositories/Models/WebHookSessionModel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories.Models
+{
+    public class WebHookSessionModel
+    {
+        public int? Computer_Id { get; set; }
+        public int Id { get; set; }
+        public DateTime? LastPing { get; set; }
+        public string Token { get; set; }
+
+        public static WebHookSessionModel FromRepositoryType(WebHookSession model)
+        {
+            var result = new WebHookSessionModel
+            {
+                Computer_Id = model.Computer.Id,
+                Id = model.Id,
+                LastPing = model.LastPing,
+                Token = model.Token,
+            };
+
+            return result;
+        }
+
+        public WebHookSession ToRepositoryType(IComputerRepository computerRepository)
+        {
+            var result = new WebHookSession(
+                computer: ComputerToRepositoryType(computerRepository),
+                id: Id,
+                lastPing: LastPing,
+                token: Token
+            );
+
+            return result;
+        }
+
+        private Computer ComputerToRepositoryType(IComputerRepository computerRepository)
+        {
+            if (Computer_Id == null)
+            {
+                return null;
+            }
+
+            if (computerRepository == null)
+            {
+                return new ComputerModel
+                {
+                    Id = Computer_Id.Value
+                }.ToRepositoryType(null, null);
+            }
+
+            return computerRepository.Get(Computer_Id.Value);
+        }
+    }
+}
---
+++ Web/Persistence/Repositories/DapperRepositories/WebHookSessionRepository.cs
@@ -0,0 +1,121 @@
+﻿using System.Data;
+using System.Linq;
+using Dapper;
+using Roomie.Web.Persistence.Models;
+using Roomie.Web.Persistence.Repositories.DapperRepositories.Models;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories
+{
+    public class WebHookSessionRepository
+    {
+        private IDbConnection _connection;
+
+        private IComputerRepository _computerRepository;
+
+        public WebHookSessionRepository(IDbConnection connection, IComputerRepository computerRepository)
+        {
+            _connection = connection;
+
+            _computerRepository = computerRepository;
+        }
+
+        public void Add(WebHookSession session)
+        {
+            var model = WebHookSessionModel.FromRepositoryType(session);
+            var sql = @"
+                INSERT INTO WebHookSessionModels
+                (
+                  Computer_Id,
+                  LastPing,
+                  Token
+                )
+                VALUES
+                (
+                  @Computer_Id,
+                  @LastPing,
+                  @Token
+                )
+
+                SELECT CAST(IDENT_CURRENT('WebHookSessionModels') as int)
+            ";
+            var parameters = new
+            {
+                model.Computer_Id,
+                model.LastPing,
+                model.Token,
+            };
+
+            var id = _connection.QuerySingle<int>(sql, parameters);
+
+            session.SetId(id);
+        }
+
+        public WebHookSession Get(string token)
+        {
+            var sql = @"
+                SELECT *
+                FROM WebHookSessionModels
+                WHERE Token = @Token
+            ";
+            var parameters = new
+            {
+                Token = token,
+            };
+
+            var model = _connection.QuerySingle<WebHookSessionModel>(sql, parameters);
+            var result = model.ToRepositoryType(_computerRepository);
+
+            return result;
+        }
+
+        public Page<WebHookSession> List(User user, ListFilter filter)
+        {
+            var model = UserModel.FromRepositoryType(user);
+            var sql = $@"
+                SELECT COUNT(*)
+                FROM WebHookSessionModels
+                JOIN ComputerModels
+                  ON ComputerModels.Id = WebHookSessionModels.Computer_Id
+                WHERE ComputerModels.Owner_Id = @User_Id
+
+                SELECT *
+                FROM WebHookSessionModels
+                JOIN ComputerModels
+                  ON ComputerModels.Id = WebHookSessionModels.Computer_Id
+                WHERE ComputerModels.Owner_Id = @User_Id
+                ORDER BY CreationTimestamp {SqlUtilities.OrderByDirection(filter.SortDirection)}
+                OFFSET @Start ROWS
+                FETCH NEXT @Count ROWS ONLY
+            ";
+            var parameters = new
+            {
+                Count = filter.Count,
+                Start = filter.Start,
+                User_Id = user.Id,
+            };
+
+            int total;
+            WebHookSessionModel[] models;
+
+            using (var multiQuery = _connection.QueryMultiple(sql, parameters))
+            {
+                total = multiQuery.ReadSingle<int>();
+                models = multiQuery.Read<WebHookSessionModel>().ToArray();
+            }
+
+            var result = new Page<WebHookSession>
+            {
+                Count = filter.Count,
+                Sort = filter.SortDirection,
+                Items = models
+                    .Select(x => x.ToRepositoryType(_computerRepository))
+                    .ToArray(),
+                Start = filter.Start,
+                Total = total,
+            };
+
+            return result;
+        }
+
+    }
+}
