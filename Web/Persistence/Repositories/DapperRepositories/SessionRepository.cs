aa21cad6e8232dd3aede3a70d2ba571bf3db8a9d
David McGrath <github.com@davidmcgrath.com>
2017-02-23 22:22:49 -0500

    implement ISessionRepository with Dapper

---
+++ Web/Persistence/Repositories/DapperRepositories/SessionRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories
+{
+    public class SessionRepository : ISessionRepository
+    {
+        private UserSessionRepository _userSessionRepository;
+        private WebHookSessionRepository _webHookSessionRepository;
+
+        public SessionRepository(UserSessionRepository userSessionRepository, WebHookSessionRepository webHookSessionRepository)
+        {
+            _userSessionRepository = userSessionRepository;
+            _webHookSessionRepository = webHookSessionRepository;
+        }
+
+        public void Add(WebHookSession session)
+        {
+            _webHookSessionRepository.Add(session);
+        }
+
+        public void Add(UserSession session)
+        {
+            _userSessionRepository.Add(session);
+        }
+
+        public UserSession GetUserSession(string token)
+        {
+            return _userSessionRepository.Get(token);
+        }
+
+        public WebHookSession GetWebHookSession(string token)
+        {
+            return _webHookSessionRepository.Get(token);
+        }
+
+        public Page<UserSession> ListUserSessions(User user, ListFilter filter)
+        {
+            return _userSessionRepository.List(user, filter);
+        }
+
+        public Page<WebHookSession> ListWebhookSessions(User user, ListFilter filter)
+        {
+            return _webHookSessionRepository.List(user, filter);
+        }
+
+        public void Update(UserSession session)
+        {
+            _userSessionRepository.Update(session);
+        }
+    }
+}
