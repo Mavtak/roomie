4db4b3e1e9d898d56004c30843534cadc9f32036
David McGrath <github.com@davidmcgrath.com>
2017-03-10 20:03:24 -0500

    implement IDeviceRepository with Dapper

---
+++ Web/Persistence/Repositories/DapperRepositories/DeviceRepository.cs
@@ -0,0 +1,165 @@
+﻿using System.Data;
+using System.Linq;
+using Dapper;
+using Roomie.Common.HomeAutomation;
+using Roomie.Web.Persistence.Models;
+using Roomie.Web.Persistence.Repositories.DapperRepositories.Models;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories
+{
+    public class DeviceRepository : IDeviceRepository
+    {
+        private IDbConnection _connection;
+
+        INetworkRepository _networkRepository;
+        IScriptRepository _scriptRepository;
+        ITaskRepository _taskRepository;
+
+        public DeviceRepository(IDbConnection connection, INetworkRepository networkRepository, IScriptRepository scriptRepository, ITaskRepository taskRepository)
+        {
+            _connection = connection;
+            _networkRepository = networkRepository;
+            _scriptRepository = scriptRepository;
+            _taskRepository = taskRepository;
+        }
+
+        public void Add(Device device)
+        {
+            var model = DeviceModel.FromRepositoryType(device);
+            var sql = @"
+                INSERT INTO DeviceModels
+                (
+                  Address,
+                  IsConnected,
+                  LastPing,
+                  Name,
+                  Network_Id,
+                  Notes,
+                  Type_Name
+                )
+                VALUES
+                (
+                  @Address,
+                  @IsConnected,
+                  @LastPing,
+                  @Name,
+                  @Network_Id,
+                  @Notes,
+                  @Type_Name
+                )
+                
+                SELECT CAST(IDENT_CURRENT('DeviceModels') as int)
+            ";
+            var parameters = new
+            {
+                Address = model.Address,
+                IsConnected = model.IsConnected,
+                LastPing = model.LastPing,
+                Name = model.LastPing,
+                Network_Id = model.Network_Id,
+                Notes = model.Notes,
+                Type_Name = model.Type_Name,
+            };
+
+            var id = _connection.QuerySingle<int>(sql, parameters);
+
+            device.SetId(id);
+        }
+
+        public Device[] Get(Network network)
+        {
+            var sql = @"
+                SELECT *
+                FROM DeviceModels
+                WHERE Network_Id = @Network_Id
+            ";
+            var parameters = new
+            {
+                Network_Id = network.Id,
+            };
+
+            var models = _connection.Query<DeviceModel>(sql, parameters).ToArray();
+            var result = models
+                .Select(x => x.ToRepositoryType(_networkRepository, _scriptRepository, _taskRepository))
+                .ToArray();
+
+            return result;
+        }
+
+        public Device Get(int id)
+        {
+            var sql = @"
+                SELECT *
+                FROM DeviceModels
+                WHERE Id = @Id
+            ";
+            var parameters = new
+            {
+                Id = id,
+            };
+
+            var model = _connection.QuerySingle<DeviceModel>(sql, parameters);
+            var result = model.ToRepositoryType(_networkRepository, _scriptRepository, _taskRepository);
+
+            return result;
+        }
+
+        public Device Get(User user, int id)
+        {
+            var result = Get(id);
+
+            if (result?.Network?.Owner?.Id != user?.Id)
+            {
+                return null;
+            }
+
+            return result;
+        }
+
+        public void Remove(Device device)
+        {
+            var model = DeviceModel.FromRepositoryType(device);
+            var sql = @"
+                DELETE
+                FROM DeviceModels
+                WHERE Id = @Id
+            ";
+            var parameters = new
+            {
+                Id = model.Id,
+            };
+
+            _connection.Execute(sql, parameters);
+        }
+
+        public void Update(Device device)
+        {
+            var model = DeviceModel.FromRepositoryType(device);
+            var sql = @"
+                UPDATE DeviceModels
+                SET
+                  Name = @Name,
+                  Notes = @Notes,
+                  Type_Name = @Type_Name
+                WHERE Id = @Id
+            ";
+            var parameters = new
+            {
+                Id = model.Id,
+                Name = model.Name,
+                Notes = model.Notes,
+                Type_Name = model.Type_Name,
+            };
+
+            _connection.Execute(sql, parameters);
+        }
+
+        public void Update(int id, IDeviceState state)
+        {
+            var device = Get(id);
+            device.Update(state, false);
+
+            Update(device);
+        }
+    }
+}
---
+++ Web/Persistence/Repositories/DapperRepositories/Models/DeviceModel.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Xml.Linq;
+using Roomie.Common.HomeAutomation;
+using Roomie.Web.Persistence.Models;
+
+namespace Roomie.Web.Persistence.Repositories.DapperRepositories.Models
+{
+    public class DeviceModel
+    {
+        public string Address { get; set; }
+        public int Id { get; set; }
+        public bool? IsConnected { get; set; }
+        public DateTime? LastPing { get; set; }
+        public string Name { get; set; }
+        public int? Network_Id { get; set; }
+        public string Notes { get; set; }
+        public string Type_Name { get; set; }
+        
+        public static DeviceModel FromRepositoryType(Device model)
+        {
+            var result = new DeviceModel
+            {
+                Address = model.Address,
+                Id = model.Id,
+                IsConnected = model.IsConnected,
+                LastPing = model.LastPing,
+                Name = model.Name,
+                Network_Id = model.Network.Id,
+                Notes = model.ToXElement().ToString(),
+                Type_Name = model.Type.Name,
+            };
+
+            return result;
+        }
+
+        public Device ToRepositoryType(INetworkRepository networkRepository, IScriptRepository scriptRepository, ITaskRepository taskRepository)
+        {
+            IDeviceState state = null;
+
+            if (!string.IsNullOrEmpty(Notes))
+            {
+                var element = XElement.Parse(Notes);
+                state = element.ToDeviceState();
+            }
+
+            var result = new Device(
+                address: Address,
+                lastPing: LastPing,
+                id: Id,
+                name: Name,
+                network: NetworkToRepositoryType(networkRepository),
+                scripts: scriptRepository,
+                state: state,
+                tasks: taskRepository,
+                type: DeviceType.GetTypeFromString(Type_Name)
+            );
+
+            return result;
+        }
+
+        private Network NetworkToRepositoryType(INetworkRepository networkRepository)
+        {
+            if (Network_Id == null)
+            {
+                return null;
+            }
+
+            if (networkRepository == null)
+            {
+                return new Network(null, null, null, Network_Id.Value, null, null, null);
+            }
+
+            return networkRepository.Get(Network_Id.Value);
+        }
+    }
+}
