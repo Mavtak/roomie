6711f10944a7246aafa12f623bbedead88bf0d1a
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:27:47 -0400

    define models for creating a session

---
+++ Roomie.Common.Api/Models/WebHookSession/CreateSession/Request.cs
@@ -0,0 +1,7 @@
+﻿namespace Roomie.Common.Api.Models.WebHookSession.CreateSession
+{
+    public class Request
+    {
+        public string AccessKey { get; set; }
+    }
+}
---
+++ Roomie.Common.Api/Models/WebHookSession/CreateSession/Response.cs
@@ -0,0 +1,7 @@
+﻿namespace Roomie.Common.Api.Models.WebHookSession.CreateSession
+{
+    public class Response
+    {
+        public string Token { get; set; }
+    }
+}
