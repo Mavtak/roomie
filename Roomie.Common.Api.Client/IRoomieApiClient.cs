7b8dab702dea448a5d9a011376cbe993a31a3e14
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:25:32 -0400

    define an implement IRoomieApiClient.cs

---
+++ Roomie.Common.Api.Client/IRoomieApiClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Roomie.Common.Api.Client
+{
+    public interface IRoomieApiClient
+    {
+        Task Send(string resource, string action, object data = null);
+        Task<TResposneData> Send<TResposneData>(string resource, string action, object data = null)
+            where TResposneData : class;
+        void SetSessionToken(string sessionToken);
+
+    }
+}
---
+++ Roomie.Common.Api.Client/RoomieApiClient.cs
@@ -0,0 +1,57 @@
+﻿using Roomie.Common.Api.Client.Http;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Roomie.Common.Api.Client
+{
+    public class RoomieApiClient : IRoomieApiClient
+    {
+        private IJsonHttpClient _jsonHttpClient;
+        private string _sessionToken;
+
+        public RoomieApiClient(IJsonHttpClient jsonHttpClient)
+        {
+            _jsonHttpClient = jsonHttpClient;
+        }
+
+        public RoomieApiClient(string apiUrl)
+        {
+            var httpClient = new HttpClient
+            {
+                BaseAddress = new Uri(apiUrl),
+            };
+
+            new JsonHttpClient(httpClient);
+        }
+
+        public async Task Send(string resource, string action, object data)
+        {
+            await Send<object>(resource, action, data);
+        }
+
+        public async Task<TResposneData> Send<TResposneData>(string resource, string action, object data)
+            where TResposneData : class
+        {
+            var response = await _jsonHttpClient.Send<Models.Response<TResposneData>>(
+                verb: "POST",
+                pathSegments: new[] { resource },
+                headers: new[] { new KeyValuePair<string, string>("x-roomie-webhook-session", _sessionToken) },
+                body: data
+            );
+
+            if (response.Error != null)
+            {
+                throw new RoomieApiClientException(response.Error);
+            }
+
+            return response.Data;
+        }
+
+        public void SetSessionToken(string sessionToken)
+        {
+            _sessionToken = sessionToken;
+        }
+    }
+}
