7c97c85a47affaefe6f3156787215b58b20d9738
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:26:11 -0400

    add extension method for creating the session

---
+++ Roomie.Common.Api.Client/RoomieApiClientExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace Roomie.Common.Api.Client
+{
+    public static class RoomieApiClientExtensions
+    {
+        public static async Task UpdateSessionToken(this IRoomieApiClient client, string apiKey)
+        {
+            var tokenResponse = await client.Send<Models.WebHookSession.CreateSession.Response>(
+                resource:  "ApiSession",
+                action:  "CreateSession",
+                data: new Models.WebHookSession.CreateSession.Request
+                {
+                    AccessKey = apiKey,
+                }
+            );
+
+            client.SetSessionToken(tokenResponse.Token);
+        }
+    }
+}
