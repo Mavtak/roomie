92ddbbb4f310c16b764f5e559e6829339b7215fe
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:15:55 -0400

    define common models

---
+++ Roomie.Common.Api/Models/Error.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Roomie.Common.Api.Models
+{
+    public class Error
+    {
+        public string[] Types { get; set; }
+        public string Message { get; set; }
+
+        public Error[] SubErrors { get; set; }
+    }
+}
---
+++ Roomie.Common.Api/Models/Response.cs
@@ -0,0 +1,9 @@
+﻿namespace Roomie.Common.Api.Models
+{
+    public class Response<TData>
+        where TData : class
+    {
+        public TData Data { get; set; }
+        public Error Error { get; set; }
+    }
+}
