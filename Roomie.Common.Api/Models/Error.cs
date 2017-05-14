086474a33bd5ef471a4e0908de04dfc108e68e04
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:26:49 -0400

    add helper method for combining errors

--- Roomie.Common.Api/Models/Error.cs
+++ Roomie.Common.Api/Models/Error.cs
@@ -8,5 +8,20 @@ namespace Roomie.Common.Api.Models
         public string Message { get; set; }
 
         public Error[] SubErrors { get; set; }
+
+        public static Error Combine(string message, params Error[] errors)
+        {
+            var result = new Error
+            {
+                Types = new[] { "combined-errors" }
+                            .Concat(errors.SelectMany(x => x.Types))
+                            .Distinct()
+                            .ToArray(),
+                Message = "An error occured while trying to re-authenticate with the server",
+                SubErrors = errors,
+            };
+
+            return result;
+        }
     }
 }
