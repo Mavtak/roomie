4d12b07bccc53d57a6275edd59ea9fdb26d180c7
David McGrath <github.com@davidmcgrath.com>
2017-05-14 11:25:06 -0400

    implement RoomieApiClientException

---
+++ Roomie.Common.Api.Client/RoomieApiClientException.cs
@@ -0,0 +1,16 @@
+﻿using Roomie.Common.Api.Models;
+using System;
+
+namespace Roomie.Common.Api.Client
+{
+    public class RoomieApiClientException : Exception
+    {
+        public Error Error { get; private set; }
+
+        public RoomieApiClientException(Error error)
+            : base(error.Message)
+        {
+            Error = error;
+        }
+    }
+}
