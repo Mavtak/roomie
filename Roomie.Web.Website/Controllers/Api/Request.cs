5bccb4a182ec582a591f612a7e2378cc38d5676f
David McGrath <github.com@davidmcgrath.com>
2017-11-09 19:36:25 -0500

    implement RpcRequestRouter

---
+++ Roomie.Web.Website/Controllers/Api/Request.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Roomie.Web.Website.Controllers.Api
+{
+    public class Request
+    {
+        public string Action { get; set; }
+        public Dictionary<string, object> Parameters { get; set; }
+
+        internal static object CreateResponse(object badRequest, Error[] error)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
