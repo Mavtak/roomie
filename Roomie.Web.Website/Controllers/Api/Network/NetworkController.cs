5660c56351d9c65938ce9553d4bd6ff35b350410
David McGrath <github.com@davidmcgrath.com>
2017-11-11 12:04:37 -0500

    remove unused code

--- Roomie.Web.Website/Controllers/Api/Network/NetworkController.cs
+++ Roomie.Web.Website/Controllers/Api/Network/NetworkController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Web;
-using System.Web.Http;
+﻿using System.Web.Http;
 using System.Web.Http.Controllers;
 using Roomie.Web.Website.Helpers;
 
@@ -25,59 +22,6 @@ namespace Roomie.Web.Website.Controllers.Api.Network
         public object Post([FromBody] Request request)
         {
             return RpcRequestRouter.Route(_rpcNetworkRepository, request);
-        }
-
-        public IEnumerable<Persistence.Models.Network> Get()
-        {
-            var result = _rpcNetworkRepository.List();
-
-            return result;
-        }
-
-        public Persistence.Models.Network Get(int id)
-        {
-            var result = _rpcNetworkRepository.Read(id);
-
-            if (result == null)
-            {
-                throw new HttpException(404, "Network not found");
-            }
-
-            return result;
-        }
-
-        public void Put(int id, [FromBody] NetworkUpdateModel update)
-        {
-            update = update ?? new NetworkUpdateModel();
-
-            _rpcNetworkRepository.Update(id, update.Name);
-        }
-
-        public void Post(int id, string action, string computerName, string sentDevicesXml)
-        {
-            switch (action)
-            {
-                case "add-device":
-                    _rpcNetworkRepository.AddDevice(id);
-                    break;
-
-                case "remove-device":
-                    _rpcNetworkRepository.RemoveDevice(id);
-                    break;
-
-                case "sync-whole-network":
-                    _rpcNetworkRepository.SyncWholeNetwork(id, computerName, sentDevicesXml);
-                    break;
-
-                default:
-                    throw new NotImplementedException();
-            }
-        }
-
-        private void Delete(int id)
-        {
-            _rpcNetworkRepository.Delete(id);
-
         }
     }
 }
--- Roomie.Web.Website/Controllers/Api/Network/NetworkUpdateModel.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Roomie.Web.Website.Controllers.Api.Network
-{
-    public class NetworkUpdateModel
-    {
-        public string Name { get; set; }
-    }
-}
