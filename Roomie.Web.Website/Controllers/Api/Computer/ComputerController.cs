91c5797b5f1db2efedb4de34582cee5bb25b7779
David McGrath <github.com@davidmcgrath.com>
2017-11-11 11:22:51 -0500

    simplify code

--- Roomie.Web.Website/Controllers/Api/Computer/ComputerController.cs
+++ Roomie.Web.Website/Controllers/Api/Computer/ComputerController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Http;
-using System.Web.Http.Controllers;
 using Roomie.Web.Website.Helpers;
 
 namespace Roomie.Web.Website.Controllers.Api.Computer
@@ -7,21 +6,14 @@ namespace Roomie.Web.Website.Controllers.Api.Computer
     [ApiRestrictedAccess]
     public class ComputerController : BaseController
     {
-        private RpcComputerRepository _rpcComputerRepository;
-
-        protected override void Initialize(HttpControllerContext controllerContext)
+        public object Post([FromBody] Request request)
         {
-            base.Initialize(controllerContext);
-
-            _rpcComputerRepository = new RpcComputerRepository(
+            var rpcRepository = new RpcComputerRepository(
                 repositoryFactory: RepositoryFactory,
                 user: User
             );
-        }
 
-        public object Post([FromBody] Request request)
-        {
-            return RpcRequestRouter.Route(_rpcComputerRepository, request);
+            return RpcRequestRouter.Route(rpcRepository, request);
         }
     }
 }
