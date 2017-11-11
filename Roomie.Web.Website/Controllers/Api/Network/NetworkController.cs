f3702a31db8092709e191264955f86a74f1cb027
David McGrath <github.com@davidmcgrath.com>
2017-11-11 11:55:51 -0500

    move code to RpcNetworkRepository

--- Roomie.Web.Website/Controllers/Api/Network/NetworkController.cs
+++ Roomie.Web.Website/Controllers/Api/Network/NetworkController.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Web;
 using System.Web.Http;
-using System.Xml.Linq;
-using Roomie.Web.Persistence.Repositories;
+using System.Web.Http.Controllers;
 using Roomie.Web.Website.Helpers;
 
 namespace Roomie.Web.Website.Controllers.Api.Network
@@ -12,35 +10,33 @@ namespace Roomie.Web.Website.Controllers.Api.Network
     [ApiRestrictedAccess]
     public class NetworkController : BaseController
     {
-        private IDeviceRepository _deviceRepository;
-        private INetworkRepository _networkRepository;
+        private RpcNetworkRepository _rpcNetworkRepository;
 
-        public NetworkController()
+        protected override void Initialize(HttpControllerContext controllerContext)
         {
-            _deviceRepository = RepositoryFactory.GetDeviceRepository();
-            _networkRepository = RepositoryFactory.GetNetworkRepository();
+            base.Initialize(controllerContext);
+
+            _rpcNetworkRepository = new RpcNetworkRepository(
+                repositoryFactory: RepositoryFactory,
+                user: User
+            );
         }
 
         public IEnumerable<Persistence.Models.Network> Get()
         {
-            var cache = new InMemoryRepositoryModelCache();
-            var networks = _networkRepository.Get(User, cache);
-
-            foreach (var network in networks)
-            {
-                network.LoadDevices(_deviceRepository);
-            }
-
-            var result = networks.Select(GetSerializableVersion);
+            var result = _rpcNetworkRepository.List();
 
             return result;
         }
 
         public Persistence.Models.Network Get(int id)
         {
-            var cache = new InMemoryRepositoryModelCache();
-            var network = SelectNetwork(id, cache);
-            var result = GetSerializableVersion(network);
+            var result = _rpcNetworkRepository.Read(id);
+
+            if (result == null)
+            {
+                throw new HttpException(404, "Network not found");
+            }
 
             return result;
         }
@@ -49,40 +45,23 @@ namespace Roomie.Web.Website.Controllers.Api.Network
         {
             update = update ?? new NetworkUpdateModel();
 
-            var cache = new InMemoryRepositoryModelCache();
-            var network = SelectNetwork(id, cache);
-
-            network.UpdateName(update.Name);
-            _networkRepository.Update(network);
+            _rpcNetworkRepository.Update(id, update.Name);
         }
 
         public void Post(int id, string action, string computerName, string sentDevicesXml)
         {
-            var cache = new InMemoryRepositoryModelCache();
-            var network = SelectNetwork(id, cache);
-
             switch (action)
             {
                 case "add-device":
-                    NetworkAction(network, "AddDevice");
+                    _rpcNetworkRepository.AddDevice(id);
                     break;
 
                 case "remove-device":
-                    NetworkAction(network, "RemoveDevice");
+                    _rpcNetworkRepository.RemoveDevice(id);
                     break;
 
                 case "sync-whole-network":
-                    var computerRepository = RepositoryFactory.GetComputerRepository();
-                    var computer = computerRepository.Get(User, computerName);
-                    var syncWholeNetwork = new Actions.SyncWholeNetwork(computerRepository, _deviceRepository, _networkRepository);
-                    var sentDevices = XElement.Parse("<devices>" + sentDevicesXml + "</devices>").Descendants();
-
-                    syncWholeNetwork.Run(
-                        computer: computer,
-                        network: network,
-                        sentDevices: sentDevices,
-                        user: User
-                    );
+                    _rpcNetworkRepository.SyncWholeNetwork(id, computerName, sentDevicesXml);
                     break;
 
                 default:
@@ -92,101 +71,8 @@ namespace Roomie.Web.Website.Controllers.Api.Network
 
         private void Delete(int id)
         {
-            var cache = new InMemoryRepositoryModelCache();
-            var network = SelectNetwork(id, cache);
+            _rpcNetworkRepository.Delete(id);
 
-            foreach (var device in _deviceRepository.Get(network))
-            {
-                _deviceRepository.Remove(device);
-            }
-
-            _networkRepository.Remove(network);
-
-        }
-
-        private void NetworkAction(Persistence.Models.Network network, string action)
-        {
-            AddTask(
-                computer: network.AttatchedComputer,
-                origin: "RoomieBot",
-                scriptText: $"<HomeAutomation.{action} Network=\"{network.Address}\" />\n<HomeAutomation.SyncWithCloud />"
-                );
-        }
-        
-        private static Persistence.Models.Network GetSerializableVersion(Persistence.Models.Network network)
-        {
-            Persistence.Models.Computer computer = null;
-
-            if (network.AttatchedComputer != null)
-            {
-                computer = new Persistence.Models.Computer(
-                    accessKey: null,
-                    address: null,
-                    encryptionKey: null,
-                    id: network.AttatchedComputer.Id,
-                    lastPing: network.AttatchedComputer.LastPing,
-                    lastScript: null,
-                    name: network.AttatchedComputer.Name,
-                    owner: null
-                );
-            }
-
-            Persistence.Models.Device[] devices = null;
-
-            if(network.Devices != null)
-            {
-                devices = network.Devices
-                    .Select(x => new Persistence.Models.Device(
-                        address: x.Address,
-                        id: x.Id,
-                        lastPing: null,
-                        name: x.Name,
-                        network: null,
-                        scripts: null,
-                        state: null,
-                        tasks: null,
-                        type: x.Type                        
-                    ))
-                    .ToArray();
-            }
-
-            Persistence.Models.User owner = null;
-
-            if (network.Owner != null)
-            {
-                owner = new Persistence.Models.User(
-                    alias: network.Owner.Alias,
-                    email: null,
-                    id: network.Owner.Id,
-                    registeredTimestamp: null,
-                    secret: null,
-                    token: null
-                );
-            }
-
-            return new Persistence.Models.Network(
-                address: network.Address,
-                attatchedComputer: computer,
-                devices: devices,
-                id: network.Id,
-                lastPing: network.LastPing,
-                name: network.Name,
-                owner: owner
-            );
-        }
-
-        private Persistence.Models.Network SelectNetwork(int id, IRepositoryModelCache cache)
-        {
-            var network = _networkRepository.Get(User, id, cache);
-
-            if (network == null)
-            {
-                throw new HttpException(404, "Network not found");
-            }
-
-            network.LoadDevices(_deviceRepository);
-
-            return network;
         }
     }
 }
---
+++ Roomie.Web.Website/Controllers/Api/Network/RpcNetworkRepository.cs
@@ -0,0 +1,187 @@
+﻿using System.Linq;
+using System.Xml.Linq;
+using Roomie.Web.Persistence.Repositories;
+
+namespace Roomie.Web.Website.Controllers.Api.Network
+{
+    public class RpcNetworkRepository
+    {
+        private INetworkRepository _networkRepository;
+        private IRepositoryFactory _repositoryFactory;
+        private Persistence.Models.User _user;
+
+        public RpcNetworkRepository(IRepositoryFactory repositoryFactory, Persistence.Models.User user)
+        {
+            _repositoryFactory = repositoryFactory;
+            _user = user;
+
+            _networkRepository = _repositoryFactory.GetNetworkRepository();
+        }
+
+        public Persistence.Models.Network[] List()
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var networks = _networkRepository.Get(_user, cache);
+
+            var deviceRepository = _repositoryFactory.GetDeviceRepository();
+
+            foreach (var network in networks)
+            {
+                network.LoadDevices(deviceRepository);
+            }
+
+            var result = networks.Select(GetSerializableVersion)
+                .ToArray();
+
+            return result;
+        }
+
+        public Persistence.Models.Network Read(int id)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+            var result = GetSerializableVersion(network);
+
+            return result;
+        }
+
+        public void Update(int id, string name)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+
+            network.UpdateName(name);
+            _networkRepository.Update(network);
+        }
+
+        public void AddDevice(int id)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+
+            NetworkAction(network, "AddDevice");
+        }
+        public void RemoveDevice(int id)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+
+            NetworkAction(network, "RemoveDevice");
+        }
+
+        public void SyncWholeNetwork(int id, string computerName, string sentDevicesXml)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+
+            var computerRepository = _repositoryFactory.GetComputerRepository();
+            var computer = computerRepository.Get(_user, computerName, cache);
+
+            var deviceRepository = _repositoryFactory.GetDeviceRepository();
+            var syncWholeNetwork = new Actions.SyncWholeNetwork(computerRepository, deviceRepository, _networkRepository);
+
+            var sentDevices = XElement.Parse("<devices>" + sentDevicesXml + "</devices>").Descendants();
+
+            syncWholeNetwork.Run(
+                computer: computer,
+                network: network,
+                sentDevices: sentDevices,
+                user: _user
+            );
+        }
+
+        public void Delete(int id)
+        {
+            var cache = new InMemoryRepositoryModelCache();
+            var network = _networkRepository.Get(_user, id, cache);
+
+            var deviceRepository = _repositoryFactory.GetDeviceRepository();
+
+            foreach (var device in deviceRepository.Get(network, cache))
+            {
+                deviceRepository.Remove(device);
+            }
+
+            _networkRepository.Remove(network);
+
+        }
+
+        private void NetworkAction(Persistence.Models.Network network, string action)
+        {
+            var computerRepository = _repositoryFactory.GetComputerRepository();
+            var scriptRepository = _repositoryFactory.GetScriptRepository();
+            var taskRepository = _repositoryFactory.GetTaskRepository();
+            var runScript = new Computer.Actions.RunScript(computerRepository, scriptRepository, taskRepository);
+
+            runScript.Run(
+                computer: network.AttatchedComputer,
+                scriptText: $"<HomeAutomation.{action} Network=\"{network.Address}\" />\n<HomeAutomation.SyncWithCloud />",
+                source: "RoomieBot",
+                updateLastRunScript: false,
+                user: _user
+            );
+        }
+
+        private static Persistence.Models.Network GetSerializableVersion(Persistence.Models.Network network)
+        {
+            Persistence.Models.Computer computer = null;
+
+            if (network.AttatchedComputer != null)
+            {
+                computer = new Persistence.Models.Computer(
+                    accessKey: null,
+                    address: null,
+                    encryptionKey: null,
+                    id: network.AttatchedComputer.Id,
+                    lastPing: network.AttatchedComputer.LastPing,
+                    lastScript: null,
+                    name: network.AttatchedComputer.Name,
+                    owner: null
+                );
+            }
+
+            Persistence.Models.Device[] devices = null;
+
+            if (network.Devices != null)
+            {
+                devices = network.Devices
+                    .Select(x => new Persistence.Models.Device(
+                        address: x.Address,
+                        id: x.Id,
+                        lastPing: null,
+                        name: x.Name,
+                        network: null,
+                        scripts: null,
+                        state: null,
+                        tasks: null,
+                        type: x.Type
+                    ))
+                    .ToArray();
+            }
+
+            Persistence.Models.User owner = null;
+
+            if (network.Owner != null)
+            {
+                owner = new Persistence.Models.User(
+                    alias: network.Owner.Alias,
+                    email: null,
+                    id: network.Owner.Id,
+                    registeredTimestamp: null,
+                    secret: null,
+                    token: null
+                );
+            }
+
+            return new Persistence.Models.Network(
+                address: network.Address,
+                attatchedComputer: computer,
+                devices: devices,
+                id: network.Id,
+                lastPing: network.LastPing,
+                name: network.Name,
+                owner: owner
+            );
+        }
+    }
+}
