9098f794e334909d4170974b36608a5c3a64d025
David McGrath <github.com@davidmcgrath.com>
2017-11-11 12:54:38 -0500

    expose web hook session code via RPC

---
+++ Roomie.Web.Website/Controllers/Api/WebHookSession/RpcWebHookSessionRepository.cs
@@ -0,0 +1,31 @@
+﻿using Roomie.Web.Persistence.Repositories;
+
+namespace Roomie.Web.Website.Controllers.Api.WebHookSession
+{
+    public class RpcWebHookSessionRepository
+    {
+        private IRepositoryFactory _repositoryFactory;
+        private ISessionRepository _sessionRepository;
+        private Persistence.Models.User _user;
+
+        public RpcWebHookSessionRepository(IRepositoryFactory repositoryFactory, Persistence.Models.User user)
+        {
+            _repositoryFactory = repositoryFactory;
+            _user = user;
+
+            _sessionRepository = _repositoryFactory.GetSessionRepository();
+        }
+
+        public string Create(string accessKey)
+        {
+            var computerRepository = _repositoryFactory.GetComputerRepository();
+            var computer = computerRepository.Get(accessKey);
+
+            var session = Persistence.Models.WebHookSession.Create(computer);
+
+            _sessionRepository.Add(session);
+
+            return session.Token;
+        }
+    }
+}
--- Roomie.Web.Website/Controllers/Api/WebHookSession/WebHookSessionController.cs
+++ Roomie.Web.Website/Controllers/Api/WebHookSession/WebHookSessionController.cs
@@ -1,43 +1,17 @@
-﻿using ApiModels = Roomie.Common.Api.Models;
+﻿using System.Web.Http;
 
 namespace Roomie.Web.Website.Controllers.Api.WebHookSession
 {
     public class WebHookSessionController : BaseController
     {
-        public object Post(string action, ApiModels.WebHookSession.CreateSession.Request requestData)
+        public object Post([FromBody] Request request)
         {
-            object result;
+            var rpcRepository = new RpcWebHookSessionRepository(
+                repositoryFactory: RepositoryFactory,
+                user: User
+            );
 
-            switch(action)
-            {
-                case "create":
-                    var computerRepository = RepositoryFactory.GetComputerRepository();
-                    var sessionRepository = RepositoryFactory.GetSessionRepository();
-                    var create = new Actions.Create(computerRepository, sessionRepository);
-
-                    result = create.Run(requestData);
-
-                    break;
-
-                default:
-                    result = Error($"Invalid action \"{action}\" for resource \"WebHookSession\"","programming-error", "invalid-action");
-
-                    break;
-            }
-
-            return result;
-        }
-
-        private static ApiModels.Response<object> Error(string message, params string[] types)
-        {
-            return new ApiModels.Response<object>
-            {
-                Error = new Common.Api.Models.Error
-                {
-                    Message = message,
-                    Types = types,
-                }
-            };
+            return RpcRequestRouter.Route(rpcRepository, request);
         }
     }
 }
