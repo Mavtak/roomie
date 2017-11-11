0d946d95cf3127de51b5fb3f2346446444e5a185
David McGrath <github.com@davidmcgrath.com>
2017-11-11 12:26:45 -0500

    simplify code

--- Roomie.Web.Website/Controllers/Api/CommandDocumentation/CommandDocumentationController.cs
+++ Roomie.Web.Website/Controllers/Api/CommandDocumentation/CommandDocumentationController.cs
@@ -4,16 +4,11 @@ namespace Roomie.Web.Website.Controllers.Api.CommandDocumentation
 {
     public class CommandDocumentationController : BaseController
     {
-        private RpcCommandDocumentationRepository _rpcCommandDocumentationRepository;
-
-        public CommandDocumentationController()
-        {
-            _rpcCommandDocumentationRepository = new RpcCommandDocumentationRepository();
-        }
-
         public object Post([FromBody] Request request)
         {
-            return RpcRequestRouter.Route(_rpcCommandDocumentationRepository, request);
+            var rpcRepository = new RpcCommandDocumentationRepository();
+
+            return RpcRequestRouter.Route(rpcRepository, request);
         }
     }
 }
