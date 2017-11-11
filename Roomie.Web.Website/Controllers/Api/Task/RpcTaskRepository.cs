9cddc7da87bbba87cfd98c6ed5df39bb9f184b7e
David McGrath <github.com@davidmcgrath.com>
2017-11-11 11:25:35 -0500

    fix default sort direction

--- Roomie.Web.Website/Controllers/Api/Task/RpcTaskRepository.cs
+++ Roomie.Web.Website/Controllers/Api/Task/RpcTaskRepository.cs
@@ -30,7 +30,7 @@ namespace Roomie.Web.Website.Controllers.Api.Task
             return result;
         }
 
-        public Page<Persistence.Models.Task> List(int count = 50, string sortDirection = "Ascending", int start = 0)
+        public Page<Persistence.Models.Task> List(int count = 50, string sortDirection = "Descending", int start = 0)
         {
             var filter = new ListFilter
             {
