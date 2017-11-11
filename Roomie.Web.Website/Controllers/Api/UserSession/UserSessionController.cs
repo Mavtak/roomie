4e96610d670a64d26cd375e106017f0f7844159d
David McGrath <github.com@davidmcgrath.com>
2017-11-11 12:53:54 -0500

    expose user session code via RPC

--- Roomie.Web.Website/Controllers/Api/UserSession/UserSessionController.cs
+++ Roomie.Web.Website/Controllers/Api/UserSession/UserSessionController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Web.Http;
-using Roomie.Web.Persistence.Repositories;
 using Roomie.Web.Website.Helpers;
 
 namespace Roomie.Web.Website.Controllers.Api.UserSession
@@ -7,42 +6,14 @@ namespace Roomie.Web.Website.Controllers.Api.UserSession
     [ApiRestrictedAccess]
     public class UserSessionController : BaseController
     {
-        private ISessionRepository _sessionRepository;
-
-        public UserSessionController()
+        public object Post([FromBody] Request request)
         {
-            _sessionRepository = RepositoryFactory.GetSessionRepository();
-        }
+            var rpcRepository = new RpcUserSessionRepository(
+                repositoryFactory: RepositoryFactory,
+                user: User
+            );
 
-        public Persistence.Models.UserSession Get(string token)
-        {
-            var cache = new InMemoryRepositoryModelCache();
-            var result = _sessionRepository.GetUserSession(token, cache);
-
-            return result;
-        }
-
-        public Page<object> Get([FromUri] ListFilter filter)
-        {
-            var cache = new InMemoryRepositoryModelCache();
-            var sessions = _sessionRepository.ListUserSessions(User, filter, cache);
-
-            var result = sessions.Transform(Transform);
-
-            return result;
-        }
-
-        private static object Transform(Persistence.Models.UserSession model)
-        {
-            var result = new
-                {
-                    Id = model.Id,
-                    CreationTimeStamp = model.CreationTimeStamp,
-                    User = model.User.Id,
-                    LastContactTimeStamp = model.LastContactTimeStamp,
-                };
-
-            return result;
+            return RpcRequestRouter.Route(rpcRepository, request);
         }
     }
 }
