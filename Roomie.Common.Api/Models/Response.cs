b8f37bed5ecca002e82e5a841f2c159d1381663f
David McGrath <github.com@davidmcgrath.com>
2017-11-17 18:49:30 -0500

    add helper to create Response objects

--- Roomie.Common.Api/Models/Response.cs
+++ Roomie.Common.Api/Models/Response.cs
@@ -6,4 +6,16 @@
         public TData Data { get; set; }
         public Error Error { get; set; }
     }
+
+    public class Response
+    {
+        public static Response<TData> Create<TData>(TData data)
+          where TData : class
+        {
+            return new Response<TData>
+            {
+                Data = data,
+            };
+        }
+    }
 }
