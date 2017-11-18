fe34a4684fab013a8c3d5a518979d127dc971c07
David McGrath <github.com@davidmcgrath.com>
2017-11-18 13:04:44 -0500

    add typed "not found" error helpers
    for returning errors from RPC repository methods that return data

--- Roomie.Web.Website/Controllers/Api/Computer/RpcComputerRepositoryHelpers.cs
+++ Roomie.Web.Website/Controllers/Api/Computer/RpcComputerRepositoryHelpers.cs
@@ -4,9 +4,19 @@ namespace Roomie.Web.Website.Controllers.Api.Computer
 {
     public static class RpcComputerRepositoryHelpers
     {
+        private static string NotFoundErrorMessage => "Computer not found";
+        private static string[] NotFoundErrorTypes => new[] { "not-found", "invalid-request" };
+
+
         public static Response CreateNotFoundError()
         {
-            return Response.CreateError("Computer not found", "not-found", "invalid-request");
+            return Response.CreateError(NotFoundErrorMessage, NotFoundErrorTypes);
+        }
+
+        public static Response<TData> CreateNotFoundError<TData>()
+            where TData : class
+        {
+            return Response.CreateError<TData>(NotFoundErrorMessage, NotFoundErrorTypes);
         }
     }
 }
--- Roomie.Web.Website/Controllers/Api/Device/RpcDeviceRepositoryHelpers.cs
+++ Roomie.Web.Website/Controllers/Api/Device/RpcDeviceRepositoryHelpers.cs
@@ -4,9 +4,19 @@ namespace Roomie.Web.Website.Controllers.Api.Device
 {
     public static class RpcDeviceRepositoryHelpers
     {
+        private static string NotFoundErrorMessage => "Device not found";
+        private static string[] NotFoundErrorTypes => new[] { "not-found", "invalid-request" };
+
+
         public static Response CreateNotFoundError()
         {
-            return Response.CreateError("Device not found", "not-found", "invalid-request");
+            return Response.CreateError(NotFoundErrorMessage, NotFoundErrorTypes);
+        }
+
+        public static Response<TData> CreateNotFoundError<TData>()
+            where TData : class
+        {
+            return Response.CreateError<TData>(NotFoundErrorMessage, NotFoundErrorTypes);
         }
     }
 }
